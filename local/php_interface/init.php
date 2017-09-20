c899e765b749431cb27674d977c432a9511c49bb
Sergey Gripinskiy <gripinskiy@adv.ru>
2017-09-20 17:20:36 +0300

    Каталог

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,8 @@
 <?php
 
+use Adv\Bitrixtools\IBlockPropertyType\YesNoPropertyType;
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/vendor/autoload.php';
+
+YesNoPropertyType::init();
+
