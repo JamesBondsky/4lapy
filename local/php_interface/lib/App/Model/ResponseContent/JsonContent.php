aed04e2c180ea3deb32a90ef1b0aaa70c38065da
Sergey Gripinskiy <gripinskiy@adv.ru>
2017-10-16 11:41:43 +0300

    JsonContent для общего использования

---
+++ local/php_interface/lib/App/Model/ResponseContent/JsonContent.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace FourPaws\App\Model\ResponseContent;
+
+use JsonSerializable;
+
+class JsonContent implements JsonSerializable
+{
+    /**
+     * @var int
+     */
+    private $success;
+
+    /**
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    public function __construct(string $message = '', bool $success = true, $data = null)
+    {
+        $this->message = $message;
+        $this->success = (int)$success;
+        $this->data = $data;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSuccess(): bool
+    {
+        return (bool)$this->success;
+    }
+
+    /**
+     * @param bool $success
+     *
+     * @return JsonContent
+     */
+    public function withSuccess(bool $success): JsonContent
+    {
+        $this->success = (int)$success;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return JsonContent
+     */
+    public function withData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return JsonContent
+     */
+    public function withMessage(string $message): JsonContent
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize() : array
+    {
+        return get_object_vars($this);
+    }
+}
