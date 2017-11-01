791645875bb09b14dff77c1352815e143cad7a27
Dmitry Panychev <panychev@adv.ru>
2017-11-01 20:09:53 +0300

    Базовый класс template

--- local/templates/4paws/header.php
+++ local/templates/4paws/header.php
@@ -2,7 +2,14 @@
 
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
-} ?><!doctype html>
+}
+
+use Bitrix\Main\Application;
+use FourPaws\App\Template;
+
+$template = Template::getInstance(Application::getInstance()->getContext());
+
+?><!doctype html>
 <html>
 <head>
     <?php $APPLICATION->ShowHead(); ?>
