518a12de011ef7d65fc85345b11a8ad159599acd
Sergey Gripinskiy <gripinskiy@adv.ru>
2017-11-03 17:32:47 +0300

    Каталог
    Отправка событий об изменении инфоблоков каталога;
    Обработчик событий пока заглушка;

--- local/php_interface/lib/App/EventInitializer.php
+++ local/php_interface/lib/App/EventInitializer.php
@@ -3,6 +3,7 @@
 namespace FourPaws\App;
 
 use Bitrix\Main\EventManager;
+use FourPaws\Catalog\Event as CatalogEvent;
 use FourPaws\ProductAutoSort\Event as ProductAutoSortEvent;
 use FourPaws\User\UserServiceHandlers;
 
@@ -17,9 +18,10 @@ final class EventInitializer
 {
     const SERVICE_HANDLER_CLASSES = [
         UserServiceHandlers::class,
-        ProductAutoSortEvent::class
+        ProductAutoSortEvent::class,
+        CatalogEvent::class,
     ];
-    
+
     /**
      * Исполняем хендлеры
      *
@@ -34,7 +36,7 @@ final class EventInitializer
             $class::initHandlers($eventManager);
         }
     }
-    
+
     /**
      * @return \Generator
      *
@@ -49,7 +51,7 @@ final class EventInitializer
             if (!in_array(ServiceHandlerInterface::class, $interfaces, true)) {
                 throw new \RuntimeException('Handler class must be an instance of ServiceHandlerInterface');
             }
-            
+
             yield $serviceHandlerClass;
         }
     }
---
+++ local/php_interface/lib/Catalog/Consumer/CatalogSyncConsumer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace FourPaws\Catalog\Consumer;
+
+use Adv\Bitrixtools\Tools\Log\LoggerFactory;
+use FourPaws\Catalog\Consumer\Message\CatalogSyncMsg;
+use JMS\Serializer\Serializer;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class CatalogSyncConsumer implements ConsumerInterface
+{
+    /**
+     * @var Serializer
+     */
+    protected $serializer;
+
+    public function __construct(Serializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function execute(AMQPMessage $msg)
+    {
+
+        $cagalogSyncMessage = $this->serializer->deserialize(
+            $msg->getBody(),
+            CatalogSyncMsg::class,
+            'json'
+        );
+
+        LoggerFactory::create('CatalogConsumer')->debug(
+            sprintf('Message is %s', var_export($cagalogSyncMessage, true))
+        );
+
+    }
+
+}
