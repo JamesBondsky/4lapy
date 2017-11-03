03e23225689513ad76116eee2c2e17481e8b40b2
Dmitry Panychev <panychev@adv.ru>
2017-11-03 13:10:54 +0300

    Заглушки и описание методов интеграции с ML в сервисе

--- local/php_interface/lib/External/ManzanaService.php
+++ local/php_interface/lib/External/ManzanaService.php
@@ -55,13 +55,151 @@ class ManzanaService implements LoggerAwareInterface
     }
     
     /**
+     * Отправка телефона
+     *
+     * - после верификации номера телефона
+     * - заказ в один клик
+     * - регистрация бонусной карты в ЛК магазина
+     */
+    public function sendPhone(string $phone)
+    {
+    
+    }
+    
+    /**
+     * Обновление/создание контакта
+     *
+     * - после регистрации (для существующего и нового относительно ML)
+     * - после оформления заказа (зарегистрированный/незарегистрированный)
+     * - после оформления заказа в 1 клик (зарегистрированный/незарегистрированный)
+     * - назначение адреса доставки основным
+     * - изменение основного адреса доставки
+     * - изменение профиля
+     * - сохранение карточки питомца (передача типа питомца)
+     * - удаление типа питомца (изменение типа питомца/удаление питомца в случае, если больше нет питомцев такого типа)
+     * - замена виртуальной карты на физическую в ЛК
+     *
+     * @param array $data
+     */
+    public function updateContact(array $data)
+    {
+    
+    }
+    
+    /**
+     * Получение данных пользователя
+     *
+     * - при регистрации после ввода номера телефона
+     * - заказ в один клик
+     * - заказ
+     *
+     * @param string $phone
+     *
+     * @return array
+     */
+    public function getUserDataByPhone(string $phone) : array
+    {
+    
+    }
+    
+    /**
+     * Получение виртуальной бонусной карты
+     *
+     * - заказ в один клик
+     * - заказ
+     */
+    public function getCard()
+    {
+    
+    }
+    
+    /**
+     * Получение покупок и заказов пользователя из ML
+     *
+     * - ЛК покупателя, переход в список последних заказов
+     * - ЛК магазина, просмотр истории по карте
+     */
+    public function getOrderList()
+    {
+    
+    }
+    
+    /**
+     * Получение детальной информации о заказе
+     *
+     * - ЛК покупателя, переход в карточку заказа
+     * - ЛК магазина, просмотр истории по карте, детализация чека
+     */
+    public function getOrderDetail()
+    {
+    
+    }
+    
+    /**
+     * Получение данных о количестве активных бонусов и размере бонуса (% от стоимости товара, который возвращается
+     * баллами на бонусную карту пользователя)
+     *
+     * - после авторизации (?!)
+     *
+     */
+    public function getActiveBonus()
+    {
+    
+    }
+    
+    /**
+     * Получение данных о расширенном балансе бонусной карты пользователя
+     *
+     * - переход в раздел "Бонусы" в ЛК
+     * - ЛК магазина, просмотр истории по карте
+     */
+    public function getAdvancedBalance()
+    {
+    
+    }
+    
+    /**
+     * Передача номера бонусной карты реферала для получения Contact_ID реферала
+     *
+     * - первый шаг заполнения формы добавления реферала
+     */
+    public function addReferralByBonusCard() : string
+    {
+    
+    }
+    
+    /**
+     * Получение информации о реферале по Contact_ID
+     */
+    public function getReferralByContactId()
+    {
+    
+    }
+    
+    /**
+     * Получение данных о рефералах заводчика
+     *
+     * - переход в раздел «Реферальная программа» в ЛК покупателя
+     */
+    public function getUserReferralList()
+    {
+    
+    }
+    
+    /**
+     * Передача номера бонусной карты для проверки валидности
+     * Контракт в ML: card_validate
+     *
+     * - форма замены бонусной карты
+     * - регистрация бонусной карты сотрудником магазина в ЛК магазина
+     *
      * @param string $cardNumber
      *
      * @return bool
      *
-     * @throws \FourPaws\External\Exception\ManzanaServiceException
+     * @throws ManzanaServiceException
      */
-    public function isCardValidByCardNumber(string $cardNumber) : bool
+    public function validateCardByNumber(string $cardNumber) : bool
     {
         $parameters = [
             [
@@ -71,8 +209,30 @@ class ManzanaService implements LoggerAwareInterface
         ];
         
         $result = $this->execute(self::CONTRACT_CARD_VALIDATE, $parameters);
-
+        
         return $result->cardid->__toString() !== '';
+    }
+    
+    /**
+     * Получение данных о держателе бонусной карты
+     *
+     * @param string $cardNumber
+     *
+     * -
+     * - ЛК магазина, просмотр истории по карте
+     */
+    public function searchCardByNumber(string $cardNumber)
+    {
+    
+    }
+    
+    /**
+     * Получение контакта по Contact_ID
+     *
+     * @param $contactId
+     */
+    public function getContactByContactId($contactId) {
+    
     }
     
     /**
@@ -80,7 +240,7 @@ class ManzanaService implements LoggerAwareInterface
      * @param array  $parameters
      *
      * @return \SimpleXMLElement
-     * @throws \FourPaws\External\Exception\ManzanaServiceException
+     * @throws ManzanaServiceException
      */
     protected function execute(string $contract, array $parameters) : \SimpleXMLElement
     {
@@ -97,7 +257,8 @@ class ManzanaService implements LoggerAwareInterface
         return $result;
     }
     
-    public function getUserByPhoneNumber() {
+    public function getUserByPhoneNumber()
+    {
     
     }
 }
