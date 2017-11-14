a8fdbbf883cacdcbd625597fa4046fe6fe60503c
Dmitry Panychev <panychev@adv.ru>
2017-11-14 19:53:24 +0300

    Главная страница

---
+++ _temp_brands.php
@@ -0,0 +1,99 @@
+<section class="b-common-section">
+    <div class="b-common-section__title-box b-common-section__title-box--popular-brand">
+        <h2 class="b-title b-title--popular-brand">Популярные бренды</h2>
+        <a class="b-link b-link--title" href="javascript:void(0)" title="Показать все">
+            <span class="b-link__text b-link__text--title">Показать все</span>
+            <span class="b-link__mobile b-link__mobile--title">Все</span>
+            <span class="b-icon">
+                <svg class="b-icon__svg" viewBox="0 0 6 10 " width="6px" height="10px">
+                    <use class="b-icon__use" xlink:href="/static/build/icons.svg#icon-arrow-right"></use>
+                </svg>
+            </span>
+        </a>
+        <div class="b-common-section__link-block"></div>
+    </div>
+    <div class="b-common-section__content b-common-section__content--popular-brand">
+        <div class="b-popular-brand">
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Hill's">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/hills.jpg"
+                         alt="Hill's"
+                         title="Hill's" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Perfect fit">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/perfect-fit.jpg"
+                         alt="Perfect fit"
+                         title="Perfect fit" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Purina">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/purina.jpg"
+                         alt="Purina"
+                         title="Purina" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Whiskas">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/whiskas.jpg"
+                         alt="Whiskas"
+                         title="Whiskas" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Felix">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/felix.jpg"
+                         alt="Felix"
+                         title="Felix" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Royal Canin">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/royal-canin.jpg"
+                         alt="Royal Canin"
+                         title="Royal Canin" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Brit">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/brit.jpg"
+                         alt="Brit"
+                         title="Brit" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Bozita">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/bozita.jpg"
+                         alt="Bozita"
+                         title="Bozita" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Eukanuba">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/eukanuba.jpg"
+                         alt="Eukanuba"
+                         title="Eukanuba" />
+                </a>
+            </div>
+            <div class="b-popular-brand-item">
+                <a class="b-popular-brand-item__link" href="javascript:void(0);" title="Acana">
+                    <img class="b-popular-brand-item__image"
+                         src="/static/build/images/content/acana.jpg"
+                         alt="Acana"
+                         title="Acana" />
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
