3badb6eeeea81392a322cd3c1c33fe47cf611287
Igor Pinchuk <pinchuk@adv.ru>
2017-11-16 13:17:38 +0300

    change project signature

---
+++ common/local/templates/4paws/temp_subscription.php
@@ -0,0 +1,15 @@
+<div class="b-feedback js-feedback-permutation">
+    <h4 class="b-feedback__header">Получайте рекомендации и выгодные предложения на почту</h4>
+    <div class="b-form-inline b-form-inline--feedback">
+        <form class="b-form-inline__form">
+            <input class="b-input" type="text" id="feedback" placeholder="Адрес эл. почты" />
+            <button class="b-button b-button--form-inline b-button--feedback"><span class="b-icon"><svg
+                        class="b-icon__svg"
+                        viewBox="0 0 16 16 "
+                        width="16px"
+                        height="16px"><use class="b-icon__use"
+                                           xlink:href="/static/build/icons.svg#icon-check"></use></svg></span>
+            </button>
+        </form>
+    </div>
+</div>
