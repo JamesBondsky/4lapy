2617987cfb1147e3336bd1a544c033921fc40577
Igor Pinchuk <pinchuk@adv.ru>
2017-11-28 00:50:28 +0300

    change name to right way

--- src/AppBundle/DependencyInjection/FourPawsAppExtension.php
+++ src/AppBundle/DependencyInjection/FourPawsAppExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class FourPawsAppBundleExtension extends Extension
+class FourPawsAppExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
