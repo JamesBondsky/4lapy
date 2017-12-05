2104df6c3e949eab47d82d69b7f89f12a92f23d9
Igor Pinchuk <pinchuk@adv.ru>
2017-12-05 14:22:30 +0300

    filter_list

--- src/MobileApiBundle/Controller/ProductController.php
+++ src/MobileApiBundle/Controller/ProductController.php
@@ -4,8 +4,10 @@ namespace FourPaws\MobileApiBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FourPaws\MobileApiBundle\Dto\Request\FilterListRequest;
 use FourPaws\MobileApiBundle\Dto\Request\GoodsListRequest;
 use FourPaws\MobileApiBundle\Dto\Request\SpecialOffersRequest;
+use FourPaws\MobileApiBundle\Dto\Response\FilterListResponse;
 use FourPaws\MobileApiBundle\Dto\Response\GoodsListResponse;
 use FourPaws\MobileApiBundle\Dto\Response\SpecialOffersResponse;
 
@@ -34,6 +36,8 @@ class ProductController extends FOSRestController
 
     /**
      * @Rest\Get("/filter_list")
+     * @see FilterListRequest
+     * @see FilterListResponse
      */
     public function getFilterListAction()
     {
