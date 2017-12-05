4d65b16c0b7b1718bce9eb82bd113a9aeb636404
Igor Pinchuk <pinchuk@adv.ru>
2017-12-05 13:33:44 +0300

    goods_list

--- src/MobileApiBundle/Controller/ProductController.php
+++ src/MobileApiBundle/Controller/ProductController.php
@@ -4,13 +4,15 @@ namespace FourPaws\MobileApiBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FourPaws\MobileApiBundle\Dto\Request\GoodsListRequest;
 use FourPaws\MobileApiBundle\Dto\Request\SpecialOffersRequest;
+use FourPaws\MobileApiBundle\Dto\Response\GoodsListResponse;
 use FourPaws\MobileApiBundle\Dto\Response\SpecialOffersResponse;
 
 class ProductController extends FOSRestController
 {
     /**
-     * @Rest\Get(path="special_offers")
+     * @Rest\Get(path="/special_offers")
      * @see SpecialOffersRequest
      * @see SpecialOffersResponse
      */
@@ -19,5 +21,14 @@ class ProductController extends FOSRestController
         /**
          * @todo кеширование
          */
+    }
+
+    /**
+     * @Rest\Get("/goods_list")
+     * @see GoodsListRequest
+     * @see GoodsListResponse
+     */
+    public function getGoodsList()
+    {
     }
 }
