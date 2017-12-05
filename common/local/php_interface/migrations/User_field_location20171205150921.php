5901906a0b06444cecb74c6252da49039c6c93ab
Соловов Дмитрий <solovov@adv.ru>
2017-12-05 15:39:59 +0300

    Миграция по добавлению юзерам поля UF_LOCATION

---
+++ common/local/php_interface/migrations/User_field_location20171205150921.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sprint\Migration;
+
+use Adv\Bitrixtools\Migration\SprintMigrationBase;
+
+class User_field_location20171205150921 extends SprintMigrationBase
+{
+    protected $description = 'Добавление юзерам свойства UF_LOCATION';
+
+    const ENTITY_ID = 'USER';
+
+    const FIELD_NAME = 'UF_LOCATION';
+
+    public function up()
+    {
+        /** @var \Sprint\Migration\Helpers\UserTypeEntityHelper $userTypeEntityHelper */
+        $userTypeEntityHelper = $this->getHelper()->UserTypeEntity();
+
+        if ($userTypeEntityHelper->addUserTypeEntityIfNotExists(
+            static::ENTITY_ID,
+            static::FIELD_NAME,
+            [
+                'USER_TYPE_ID'      => 'sale_location',
+                'XML_ID'            => static::FIELD_NAME,
+                'SORT'              => 500,
+                'MULTIPLE'          => 'N',
+                'MANDATORY'         => 'N',
+                'SHOW_FILTER'       => 'N',
+                'SHOW_IN_LIST'      => 'Y',
+                'EDIT_IN_LIST'      => 'N',
+                'IS_SEARCHABLE'     => 'N',
+            ]
+        )) {
+            $this->log()->info('Пользовательское свойство ' . static::FIELD_NAME . ' создано');
+        } else {
+            $this->log()->error('Ошибка при создании пользовательского свойства ' . static::FIELD_NAME);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function down()
+    {
+        /** @var \Sprint\Migration\Helpers\UserTypeEntityHelper $userTypeEntityHelper */
+        $userTypeEntityHelper = $this->getHelper()->UserTypeEntity();
+
+        if ($userTypeEntityHelper->deleteUserTypeEntityIfExists(static::ENTITY_ID, static::FIELD_NAME)) {
+            $this->log()->info('Пользовательское свойство ' . static::FIELD_NAME . ' удалено');
+        } else {
+            $this->log()->error('Ошибка при удалении пользовательского свойства ' . static::FIELD_NAME);
+
+            return false;
+        }
+
+        return true;
+    }
+}
