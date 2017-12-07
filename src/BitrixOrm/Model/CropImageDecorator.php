80a15bf1c0db90315bba12c8f28f5479db0373d6
Dmitry Panychev <panychev@adv.ru>
2017-12-07 12:14:21 +0300

    Добавлены модели для файла, картинки, декораторы для size, resize, rotate, crop

---
+++ src/BitrixOrm/Model/CropImageDecorator.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace FourPaws\BitrixOrm\Model;
+
+use FourPaws\BitrixOrm\Model\Interfaces\CropImageInterface;
+
+/**
+ * Class CropImageDecorator
+ *
+ * @package FourPaws\BitrixOrm\Model
+ */
+class CropImageDecorator extends Image implements CropImageInterface
+{
+    /**
+     * @var string
+     */
+    protected $cropWidth = '-';
+    
+    /**
+     * @var string
+     */
+    protected $cropHeight = '-';
+    
+    /**
+     * @inheritDoc
+     */
+    public function setCropWidth(int $cropWidth) : self
+    {
+        $this->cropWidth = $cropWidth > 0 ? $cropWidth : '-';
+        
+        return $this;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function setCropHeight(int $cropHeight) : self
+    {
+        $this->cropHeight = $cropHeight > 0 ? $cropHeight : '-';
+        
+        return $this;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getCropWidth() : int
+    {
+        return (int)$this->cropWidth;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getCropHeight() : int
+    {
+        return (int)$this->cropHeight;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getSrc() : string
+    {
+        return sprintf('/crop/%sx%s%s', $this->getCropWidth() ?: '-', $this->getCropHeight() ?: '-', parent::getSrc());
+    }
+}
---
+++ src/BitrixOrm/Model/Exceptions/WrongRotateAngleException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FourPaws\BitrixOrm\Model\Exceptions;
+
+/**
+ * Class WrongRotateAngleException
+ *
+ * @package FourPaws\BitrixOrm\Model\Exceptions
+ */
+class WrongRotateAngleException extends ModelException
+{
+    
+}
