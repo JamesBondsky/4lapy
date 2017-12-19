f45a7294ca64ceb49ca23ca89ffd76731b81db95
Соловов Дмитрий <solovov@adv.ru>
2017-12-19 15:27:25 +0300

    Миграция и хелпер для вывода цен

---
+++ common/local/php_interface/migrations/Currency_rub_template20171219151247.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Sprint\Migration;
+
+use Adv\Bitrixtools\Migration\SprintMigrationBase;
+use Bitrix\Currency\CurrencyLangTable;
+
+class Currency_rub_template20171219151247 extends SprintMigrationBase
+{
+    protected $description = 'Задание дефолтного шаблона валюты в соответствии с дизайном';
+
+    const CURRENCY_CODE = 'RUB';
+
+    protected $languageIds = ['ru', 'en'];
+
+    public function up()
+    {
+        foreach ($this->languageIds as $lid) {
+            $updateResult = CurrencyLangTable::update(
+                [
+                    'CURRENCY' => self::CURRENCY_CODE,
+                    'LID'      => $lid,
+                ],
+                [
+                    'FORMAT_STRING' => '# ₽',
+                ]
+            );
+
+            if ($updateResult->isSuccess()) {
+                $this->log()->info('Задан формат вывода валюты для ' . $lid);
+            } else {
+                $this->log()->error('Не удалось задать формат вывода валюты для ' . $lid);
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function down()
+    {
+        foreach ($this->languageIds as $lid) {
+            switch ($lid) {
+                case 'ru':
+                    $format = '# руб.';
+                    break;
+                case 'en':
+                    $format = '# rub.';
+                    break;
+            }
+            $updateResult = CurrencyLangTable::update(
+                [
+                    'CURRENCY' => self::CURRENCY_CODE,
+                    'LID'      => $lid,
+                ],
+                [
+                    'FORMAT_STRING' => '# ₽',
+                ]
+            );
+
+            if ($updateResult->isSuccess()) {
+                $this->log()->info('Задан формат вывода валюты для ' . $lid);
+            } else {
+                $this->log()->error('Не удалось задать формат вывода валюты для ' . $lid);
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
