667f290885ea3db713013d42793b774f9a0827e8
Sergey Frolov <frolov@adv.ru>
2017-12-22 13:10:41 +0300

    fix названия компонента

--- common/local/components/fourpaws/shop.list/lang/ru/.description.php
+++ common/local/components/fourpaws/shop.list/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?php
-$MESS['COMPONENT_SHOP_LIST_NAME']        = 'Полный список элементов из разных инфоблоков';
-$MESS['COMPONENT_SHOP_LIST_DESCRIPTION'] = 'Полный список элементов из разных инфоблоков';
+$MESS['COMPONENT_SHOP_LIST_NAME']        = 'Список магазинов';
+$MESS['COMPONENT_SHOP_LIST_DESCRIPTION'] = 'Список магазинов';
 $MESS['MAIN_SHOP_LIST_GROUP_NAME']       = 'Магазины';
