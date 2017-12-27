5ad21b6df373663f7df994ecd371427ef5922046
Igor Pinchuk <pinchuk@adv.ru>
2017-12-27 19:04:01 +0300

    pictures now not lazy

--- src/BitrixOrm/Model/File.php
+++ src/BitrixOrm/Model/File.php
@@ -2,6 +2,8 @@
 
 namespace FourPaws\BitrixOrm\Model;
 
+use Bitrix\Main\ArgumentNullException;
+use Bitrix\Main\ArgumentOutOfRangeException;
 use Bitrix\Main\Config\Option;
 use Bitrix\Main\FileTable;
 use FourPaws\BitrixOrm\Model\Exceptions\FileNotFoundException;
@@ -18,12 +20,12 @@ class File implements FileInterface
      * @var array
      */
     protected $fields;
-    
+
     /**
      * @var string
      */
     protected $src;
-    
+
     /**
      * File constructor.
      *
@@ -33,94 +35,110 @@ class File implements FileInterface
     {
         $this->fields = $fields;
     }
-    
+
+    /**
+     * @param string $primary
+     *
+     * @throws FileNotFoundException
+     * @return static
+     *
+     */
+    public static function createFromPrimary(string $primary)
+    {
+        $fields = FileTable::getById($primary)->fetch();
+
+        if (!$fields) {
+            throw new FileNotFoundException(sprintf('File with id %s is not found', $primary));
+        }
+
+        return new static($fields);
+    }
+
     /**
      * @return string
      */
-    public function getSrc() : string
+    public function getSrc(): string
     {
         if ($this->src === null) {
-            $src = sprintf('/%s/%s/%s',
-                           Option::get('main', 'upload_dir', 'upload'),
-                           $this->getSubDir(),
-                           $this->getFileName());
-            $this->setSrc($src);
+            try {
+                $src = sprintf(
+                    '/%s/%s/%s',
+                    Option::get('main', 'upload_dir', 'upload'),
+                    $this->getSubDir(),
+                    $this->getFileName()
+                );
+                $this->setSrc($src);
+            } catch (ArgumentNullException $e) {
+            } catch (ArgumentOutOfRangeException $e) {
+            }
+            $this->src = '';
         }
-        
+
         return $this->src;
     }
-    
+
     /**
      * @param string $src
      *
      * @return static
      */
-    public function setSrc(string $src) : self
+    public function setSrc(string $src): self
     {
         $this->src = $src;
-        
+
         return $this;
     }
-    
+
     /**
      * @return string
      */
-    public function getSubDir() : string
+    public function getSubDir(): string
     {
         return (string)$this->fields['SUBDIR'];
     }
-    
+
     /**
      * @return string
      */
-    public function getFileName() : string
+    public function getFileName(): string
     {
         return (string)$this->fields['FILE_NAME'];
     }
-    
+
     /**
      * @return int
      */
-    public function getId() : int
+    public function getId(): int
     {
         return (int)$this->fields['ID'];
     }
-    
+
     /**
      * @param int $id
      *
      * @return static
      */
-    public function setId(int $id) : self
+    public function setId(int $id): self
     {
         $this->fields['ID'] = $id;
-        
+
         return $this;
     }
-    
+
     /**
      * @return string
      */
-    public function __toString() : string
+    public function __toString(): string
     {
         return $this->getSrc();
     }
-    
+
     /**
-     * @param string $primary
-     *
-     * @return static
-     *
-     * @throws FileNotFoundException
+     * @todo move to interface
+     * @return array
      */
-    public static function createFromPrimary(string $primary)
+    public function getFields(): array
     {
-        $fields = FileTable::getById($primary)->fetch();
-        
-        if (!$fields) {
-            throw new FileNotFoundException(sprintf('File with id %s is not found', $primary));
-        }
-        
-        return new static($fields);
+        return $this->fields;
     }
 }
