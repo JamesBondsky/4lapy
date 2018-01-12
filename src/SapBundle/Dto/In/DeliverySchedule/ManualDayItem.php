c8758ab0c83980471527f9ba2ed75ce3bac445cd
leshchenko <prevedgreat@gmail.com>
2018-01-12 15:48:05 +0200

    dto "График поставок товаров от поставщика и со склада"

---
+++ src/SapBundle/Dto/In/DeliverySchedule/ManualDayItem.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace FourPaws\SapBundle\Dto\In\DeliverySchedule;
+
+/**
+ * Class ManualDayItem
+ *
+ * @package FourPaws\SapBundle\Dto\In
+ * @Serializer\XmlRoot("manualdays")
+  */
+class ManualDayItem
+{
+    /**
+     * Номер по порядку.
+     * Содержит порядковый номер поставки.
+     *
+     * @Serializer\Type("int")
+     * @Serializer\SerializedName("num")
+     *
+     * @var int
+     */
+    protected $num = 0;
+
+    /**
+     * Дата поставки.
+     * Содержит дату поставки, формат: ГГГГММДД.
+     *
+     * @Serializer\Type()
+     * @Serializer\SerializedName("dlvdate")
+     *
+     * @var \DateTime
+     */
+    protected $date = null;
+
+    /**
+     * @return int
+     */
+    public function getNum(): int
+    {
+        return $this->num;
+    }
+
+    /**
+     * @param int $num
+     * @return ManualDayItem
+     */
+    public function setNum(int $num): ManualDayItem
+    {
+        $this->num = $num;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return ManualDayItem
+     */
+    public function setDate(\DateTime $date): ManualDayItem
+    {
+        $this->date = $date;
+        return $this;
+    }
+}
