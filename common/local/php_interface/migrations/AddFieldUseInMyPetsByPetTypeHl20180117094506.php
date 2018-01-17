a022816e5e7eafa742f5c6539dd0ca19a0109fd8
Sergey Frolov <frolov@adv.ru>
2018-01-17 10:28:58 +0300

    down in migrator

--- common/local/php_interface/migrations/AddFieldUseInMyPetsByPetTypeHl20180117094506.php
+++ common/local/php_interface/migrations/AddFieldUseInMyPetsByPetTypeHl20180117094506.php
@@ -7,7 +7,9 @@ use Adv\Bitrixtools\Tools\HLBlock\HLBlockFactory;
 class AddFieldUseInMyPetsByPetTypeHl20180117094506 extends \Adv\Bitrixtools\Migration\SprintMigrationBase
 {
     
-    const HL_NAME = 'ForWho';
+    const HL_NAME    = 'ForWho';
+    
+    const FIELD_NAME = 'UF_USE_BY_PET';
     
     protected $description = 'Добавление поля "использовать в моих питомцах" в highload тип питомца';
     
@@ -15,54 +17,54 @@ class AddFieldUseInMyPetsByPetTypeHl20180117094506 extends \Adv\Bitrixtools\Migr
     {
         $helper = new HelperManager();
         
-        $hlblockId = $helper->Hlblock()->getHlblockId(static::HL_NAME);
-        $entityId  = 'HLBLOCK_' . $hlblockId;
-        $helper->UserTypeEntity()->addUserTypeEntityIfNotExists(
-            $entityId,
-            'UF_USE_BY_PET',
-            [
-                'FIELD_NAME'        => 'UF_USE_BY_PET',
-                'USER_TYPE_ID'      => 'boolean',
-                'XML_ID'            => '',
-                'SORT'              => '1000',
-                'MULTIPLE'          => 'N',
-                'MANDATORY'         => 'N',
-                'SHOW_FILTER'       => 'N',
-                'SHOW_IN_LIST'      => 'Y',
-                'EDIT_IN_LIST'      => 'Y',
-                'IS_SEARCHABLE'     => 'N',
-                'SETTINGS'          => [
-                    'DEFAULT_VALUE'  => '1',
-                    'DISPLAY'        => 'CHECKBOX',
-                    'LABEL'          => [
-                        0 => '',
-                        1 => '',
+        $hlblockId = (int)$helper->Hlblock()->getHlblockId(static::HL_NAME);
+        if ($hlblockId > 0) {
+            $entityId = 'HLBLOCK_' . $hlblockId;
+            $helper->UserTypeEntity()->addUserTypeEntityIfNotExists(
+                $entityId,
+                static::FIELD_NAME,
+                [
+                    'FIELD_NAME'        => 'UF_USE_BY_PET',
+                    'USER_TYPE_ID'      => 'boolean',
+                    'XML_ID'            => '',
+                    'SORT'              => '1000',
+                    'MULTIPLE'          => 'N',
+                    'MANDATORY'         => 'N',
+                    'SHOW_FILTER'       => 'N',
+                    'SHOW_IN_LIST'      => 'Y',
+                    'EDIT_IN_LIST'      => 'Y',
+                    'IS_SEARCHABLE'     => 'N',
+                    'SETTINGS'          => [
+                        'DEFAULT_VALUE'  => '1',
+                        'DISPLAY'        => 'CHECKBOX',
+                        'LABEL'          => [
+                            0 => '',
+                            1 => '',
+                        ],
+                        'LABEL_CHECKBOX' => '',
                     ],
-                    'LABEL_CHECKBOX' => '',
-                ],
-                'EDIT_FORM_LABEL'   => [
-                    'ru' => 'Использовать для моих питомцев',
-                ],
-                'LIST_COLUMN_LABEL' => [
-                    'ru' => 'Использовать для моих питомцев',
-                ],
-                'LIST_FILTER_LABEL' => [
-                    'ru' => 'Использовать для моих питомцев',
-                ],
-                'ERROR_MESSAGE'     => [
-                    'ru' => '',
-                ],
-                'HELP_MESSAGE'      => [
-                    'ru' => 'Использовать для моих питомцев',
-                ],
-            ]
-        );
-        
-        /** @var \Bitrix\Main\Entity\DataManager $dataManager */
-        try {
-            $dataManager = HLBlockFactory::createTableObject(static::HL_NAME);
-            $res         =
-                $dataManager::query()->setSelect(['ID'])->setFilter(
+                    'EDIT_FORM_LABEL'   => [
+                        'ru' => 'Использовать для моих питомцев',
+                    ],
+                    'LIST_COLUMN_LABEL' => [
+                        'ru' => 'Использовать для моих питомцев',
+                    ],
+                    'LIST_FILTER_LABEL' => [
+                        'ru' => 'Использовать для моих питомцев',
+                    ],
+                    'ERROR_MESSAGE'     => [
+                        'ru' => '',
+                    ],
+                    'HELP_MESSAGE'      => [
+                        'ru' => 'Использовать для моих питомцев',
+                    ],
+                ]
+            );
+            
+            /** @var \Bitrix\Main\Entity\DataManager $dataManager */
+            try {
+                $dataManager = HLBlockFactory::createTableObject(static::HL_NAME);
+                $res         = $dataManager::query()->setSelect(['ID'])->setFilter(
                     [
                         'UF_CODE' => [
                             'koshki',
@@ -74,15 +76,24 @@ class AddFieldUseInMyPetsByPetTypeHl20180117094506 extends \Adv\Bitrixtools\Migr
                         ],
                     ]
                 )->exec();
-            while ($item = $res->fetch()) {
-                $dataManager::update($item['ID'], ['UF_USE_BY_PET' => 'Y']);
+                while ($item = $res->fetch()) {
+                    $dataManager::update($item['ID'], ['UF_USE_BY_PET' => 'Y']);
+                }
+            } catch (\Exception $e) {
             }
-        } catch (\Exception $e) {
         }
     }
     
     public function down()
     {
+        $helper = new HelperManager();
+        
+        $hlblockId = (int)$helper->Hlblock()->getHlblockId(static::HL_NAME);
+        if ($hlblockId > 0) {
+            $entityId = 'HLBLOCK_' . $hlblockId;
+            
+            $helper->UserTypeEntity()->deleteUserTypeEntityIfExists($entityId, static::FIELD_NAME);
+        }
     }
     
 }
