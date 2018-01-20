038db24f909e423aeba3ea8584ee995ad5371ae3
pandmitr <thor_work@yahoo.com>
2018-01-20 13:32:05 +0300

    Fix static links on modal

---
+++ common/local/templates/4paws/blocks/footer/modal_popup.php
@@ -0,0 +1,16 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+} ?>
+
+<section class="b-popup-wrapper__wrapper-modal js-popup-section" data-popup="alert-popup" style="display: none;">
+    <div class="b-registration b-registration--popup js-popup-alert-title success" data-popup="alert-popup">
+        <a class="b-registration__close js-close-popup" href="javascript:void(0)" title="закрыть"></a>
+        <header class="b-registration__header">
+            <span class="b-title b-title--h1 b-title--popup-success">Успех</span>
+            <span class="b-title b-title--h1 b-title--popup-error">Ошибка</span>
+        </header>
+        <div class="b-registration__content b-registration__content--simple-text">
+            <div class="b-registration__text-instruction js-popup-simple-text">Код выслан повторно</div>
+        </div>
+    </div>
+</section>
