a0a7b9ead169df8dbd12a3f54c27b57fc7fbb0d5
pandmitr <thor_work@yahoo.com>
2018-01-22 04:17:31 +0300

    Source?

---
+++ src/SapBundle/Source/SourceMessage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FourPaws\SapBundle\Source;
+
+use FourPaws\SapBundle\Model\SourceMessageInterface;
+
+class SourceMessage implements SourceMessageInterface
+{
+    private $id;
+    private $type;
+    private $data;
+    
+    /**
+     * @return mixed
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getType() : string {
+        return $this->type;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getData() : string {
+        return $this->data;
+    }
+    
+    /**
+     * Create source message
+     *
+     * @param        $id
+     * @param string $type
+     * @param string $data
+     */
+    public function __construct($id, string $type, string $data) {
+        $this->id   = $id;
+        $this->type = $type;
+        $this->data = $data;
+    }
+}
