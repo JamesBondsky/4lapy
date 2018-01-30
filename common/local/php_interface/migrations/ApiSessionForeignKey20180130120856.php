114331ef336f38758d9bc14864eb0925df8ff4be
Igor Pinchuk <pinchuk@adv.ru>
2018-01-30 16:00:23 +0300

    update date_update

--- common/local/php_interface/migrations/ApiSessionForeignKey20180130120856.php
+++ common/local/php_interface/migrations/ApiSessionForeignKey20180130120856.php
@@ -4,14 +4,13 @@ namespace Sprint\Migration;
 
 use Adv\Bitrixtools\Migration\SprintMigrationBase;
 use Bitrix\Main\Application;
-use FourPaws\MobileApiBundle\Tables\ApiUserSessionTable;
 
 class ApiSessionForeignKey20180130120856 extends SprintMigrationBase
 {
     public function __construct()
     {
         parent::__construct();
-        $this->description = 'Add Foreign key to Api Session';
+        $this->description = 'Modify fields api session table and add foreign keys';
     }
 
 
@@ -23,6 +22,10 @@ class ApiSessionForeignKey20180130120856 extends SprintMigrationBase
     {
         Application::getConnection()
             ->query('ALTER TABLE api_user_session ADD FOREIGN KEY (FUSER_ID) REFERENCES b_sale_fuser(ID) ON DELETE CASCADE');
+        Application::getConnection()
+            ->query('ALTER TABLE api_user_session MODIFY DATE_UPDATE DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
+        Application::getConnection()
+            ->query('ALTER TABLE api_user_session MODIFY DATE_INSERT DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
         return parent::up();
     }
 
