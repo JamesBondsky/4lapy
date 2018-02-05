d03bcf48d6399bd271517fa07813ebb54c273c2b
Igor Pinchuk <pinchuk@adv.ru>
2018-02-05 13:49:26 +0300

    static social data

---
+++ src/MobileApiBundle/Controller/InfoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FourPaws\MobileApiBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FourPaws\MobileApiBundle\Dto\Response;
+
+class InfoController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/social")
+     */
+    public function getSocialsAction()
+    {
+        $response = new Response();
+        $response->setData([
+            'vkontakte'     =>
+                [
+                    'web'     => 'http://vk.com/4lapy_ru',
+                    'ios'     => 'vk://vk.com/4lapy_ru',
+                    'android' => '',
+                ],
+            'facebook'      =>
+                [
+                    'web'     => 'https://www.facebook.com/4laps',
+                    'ios'     => 'fb://profile/137001486387927',
+                    'android' => '',
+                ],
+            'instagram'     =>
+                [
+                    'web'     => 'https://www.instagram.com/4lapy.ru/',
+                    'ios'     => 'instagram://user?username=4lapy.ru',
+                    'android' => 'link',
+                ],
+            'odnoklassniki' =>
+                [
+                    'web'     => 'https://ok.ru/chetyre.lapy',
+                    'ios'     => 'odnoklassniki://ok.ru/group/51483118272694',
+                    'android' => '',
+                ],
+        ]);
+        return $this->view($response);
+    }
+}
