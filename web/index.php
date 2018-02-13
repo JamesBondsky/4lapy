66a056fef54b2224bead0cc419bcb88ad4fcca86
Sergey Frolov <frolov@adv.ru>
2018-02-13 11:21:51 +0300

    add root sections menu by main page

--- web/index.php
+++ web/index.php
@@ -11,74 +11,94 @@ $APPLICATION->SetPageProperty('NOT_SHOW_NAV_CHAIN', 'Y');
 $APPLICATION->SetTitle('Главная страница');
 
 $APPLICATION->IncludeComponent('bitrix:news.list',
-                               'index.slider',
-                               [
-                                   'COMPONENT_TEMPLATE' => 'index.slider',
-                                   'IBLOCK_TYPE'        => IblockType::PUBLICATION,
-                                   'IBLOCK_ID'          => IblockUtils::getIblockId(IblockType::PUBLICATION,
-                                                                                    IblockCode::BANNERS),
-                                   //не проставлен символьный код
-                                   'NEWS_COUNT'         => '7',
-                                   'SORT_BY1'           => 'SORT',
-                                   'SORT_ORDER1'        => 'ASC',
-                                   'SORT_BY2'           => 'ACTIVE_FROM',
-                                   'SORT_ORDER2'        => 'DESC',
-                                   'FILTER_NAME'                     => '',
-                                   'FIELD_CODE'                      => [
-                                       0 => 'NAME',
-                                       1 => 'PREVIEW_PICTURE',
-                                       2 => 'DETAIL_PICTURE',
-                                       3 => '',
-                                   ],
-                                   'PROPERTY_CODE'                   => [
-                                       0 => 'LINK',
-                                       1 => 'IMG_TABLET',
-                                       2 => 'BACKGROUND',
-                                   ],
-                                   'CHECK_DATES'                     => 'Y',
-                                   'DETAIL_URL'                      => '',
-                                   'AJAX_MODE'                       => 'N',
-                                   'AJAX_OPTION_JUMP'                => 'N',
-                                   'AJAX_OPTION_STYLE'               => 'N',
-                                   'AJAX_OPTION_HISTORY'             => 'N',
-                                   'AJAX_OPTION_ADDITIONAL'          => '',
-                                   'CACHE_TYPE'                      => 'A',
-                                   'CACHE_TIME'                      => '36000000',
-                                   'CACHE_FILTER'                    => 'Y',
-                                   'CACHE_GROUPS'                    => 'N',
-                                   'PREVIEW_TRUNCATE_LEN'            => '',
-                                   'ACTIVE_DATE_FORMAT'              => '',
-                                   'SET_TITLE'                       => 'N',
-                                   'SET_BROWSER_TITLE'               => 'N',
-                                   'SET_META_KEYWORDS'               => 'N',
-                                   'SET_META_DESCRIPTION'            => 'N',
-                                   'SET_LAST_MODIFIED'               => 'N',
-                                   'INCLUDE_IBLOCK_INTO_CHAIN'       => 'N',
-                                   'ADD_SECTIONS_CHAIN'              => 'N',
-                                   'HIDE_LINK_WHEN_NO_DETAIL'        => 'N',
-                                   'PARENT_SECTION'                  => '',
-                                   'PARENT_SECTION_CODE'             => '',
-                                   'INCLUDE_SUBSECTIONS'             => 'N',
-                                   'STRICT_SECTION_CHECK'            => 'N',
-                                   'DISPLAY_DATE'                    => 'N',
-                                   'DISPLAY_NAME'                    => 'N',
-                                   'DISPLAY_PICTURE'                 => 'N',
-                                   'DISPLAY_PREVIEW_TEXT'            => 'N',
-                                   'PAGER_TEMPLATE'                  => '',
-                                   'DISPLAY_TOP_PAGER'               => 'N',
-                                   'DISPLAY_BOTTOM_PAGER'            => 'N',
-                                   'PAGER_TITLE'                     => '',
-                                   'PAGER_SHOW_ALWAYS'               => 'N',
-                                   'PAGER_DESC_NUMBERING'            => 'N',
-                                   'PAGER_DESC_NUMBERING_CACHE_TIME' => '',
-                                   'PAGER_SHOW_ALL'                  => 'N',
-                                   'PAGER_BASE_LINK_ENABLE'          => 'N',
-                                   'SET_STATUS_404'                  => 'N',
-                                   'SHOW_404'                        => 'N',
-                                   'MESSAGE_404'                     => '',
-                               ],
-                               false,
-                               ['HIDE_ICONS' => 'Y']);
+    'index.slider',
+    [
+        'COMPONENT_TEMPLATE'              => 'index.slider',
+        'IBLOCK_TYPE'                     => IblockType::PUBLICATION,
+        'IBLOCK_ID'                       => IblockUtils::getIblockId(IblockType::PUBLICATION,
+            IblockCode::BANNERS),
+        //не проставлен символьный код
+        'NEWS_COUNT'                      => '7',
+        'SORT_BY1'                        => 'SORT',
+        'SORT_ORDER1'                     => 'ASC',
+        'SORT_BY2'                        => 'ACTIVE_FROM',
+        'SORT_ORDER2'                     => 'DESC',
+        'FILTER_NAME'                     => '',
+        'FIELD_CODE'                      => [
+            0 => 'NAME',
+            1 => 'PREVIEW_PICTURE',
+            2 => 'DETAIL_PICTURE',
+            3 => '',
+        ],
+        'PROPERTY_CODE'                   => [
+            0 => 'LINK',
+            1 => 'IMG_TABLET',
+            2 => 'BACKGROUND',
+        ],
+        'CHECK_DATES'                     => 'Y',
+        'DETAIL_URL'                      => '',
+        'AJAX_MODE'                       => 'N',
+        'AJAX_OPTION_JUMP'                => 'N',
+        'AJAX_OPTION_STYLE'               => 'N',
+        'AJAX_OPTION_HISTORY'             => 'N',
+        'AJAX_OPTION_ADDITIONAL'          => '',
+        'CACHE_TYPE'                      => 'A',
+        'CACHE_TIME'                      => '36000000',
+        'CACHE_FILTER'                    => 'Y',
+        'CACHE_GROUPS'                    => 'N',
+        'PREVIEW_TRUNCATE_LEN'            => '',
+        'ACTIVE_DATE_FORMAT'              => '',
+        'SET_TITLE'                       => 'N',
+        'SET_BROWSER_TITLE'               => 'N',
+        'SET_META_KEYWORDS'               => 'N',
+        'SET_META_DESCRIPTION'            => 'N',
+        'SET_LAST_MODIFIED'               => 'N',
+        'INCLUDE_IBLOCK_INTO_CHAIN'       => 'N',
+        'ADD_SECTIONS_CHAIN'              => 'N',
+        'HIDE_LINK_WHEN_NO_DETAIL'        => 'N',
+        'PARENT_SECTION'                  => '',
+        'PARENT_SECTION_CODE'             => '',
+        'INCLUDE_SUBSECTIONS'             => 'N',
+        'STRICT_SECTION_CHECK'            => 'N',
+        'DISPLAY_DATE'                    => 'N',
+        'DISPLAY_NAME'                    => 'N',
+        'DISPLAY_PICTURE'                 => 'N',
+        'DISPLAY_PREVIEW_TEXT'            => 'N',
+        'PAGER_TEMPLATE'                  => '',
+        'DISPLAY_TOP_PAGER'               => 'N',
+        'DISPLAY_BOTTOM_PAGER'            => 'N',
+        'PAGER_TITLE'                     => '',
+        'PAGER_SHOW_ALWAYS'               => 'N',
+        'PAGER_DESC_NUMBERING'            => 'N',
+        'PAGER_DESC_NUMBERING_CACHE_TIME' => '',
+        'PAGER_SHOW_ALL'                  => 'N',
+        'PAGER_BASE_LINK_ENABLE'          => 'N',
+        'SET_STATUS_404'                  => 'N',
+        'SHOW_404'                        => 'N',
+        'MESSAGE_404'                     => '',
+    ],
+    false,
+    ['HIDE_ICONS' => 'Y']);
+
+/** main sections by mobile */
+$APPLICATION->IncludeComponent('bitrix:menu',
+    'mobile.root_section_menu',
+    [
+        'COMPONENT_TEMPLATE'    => 'mobile.root_section_menu',
+        'ROOT_MENU_TYPE'        => 'mobile_root_section',
+        'MENU_CACHE_TYPE'       => 'A',
+        'MENU_CACHE_TIME'       => '360000',
+        'MENU_CACHE_USE_GROUPS' => 'Y',
+        'CACHE_SELECTED_ITEMS'  => 'N',
+        'TEMPLATE_NO_CACHE'     => 'N',
+        'MENU_CACHE_GET_VARS'   => [],
+        'MAX_LEVEL'             => '1',
+        'CHILD_MENU_TYPE'       => 'mobile_root_section',
+        'USE_EXT'               => 'N',
+        'DELAY'                 => 'N',
+        'ALLOW_MULTI_SELECT'    => 'N',
+    ],
+    false);
 
 /**
  * Популярные товары
@@ -88,8 +108,8 @@ $APPLICATION->IncludeComponent(
     '',
     [
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/blocks/index.popular_products.php',
-        'EDIT_TEMPLATE' => '',
+        'PATH'           => '/local/include/blocks/index.popular_products.php',
+        'EDIT_TEMPLATE'  => '',
     ],
     null,
     [
@@ -110,12 +130,12 @@ $APPLICATION->IncludeComponent(
     '',
     [
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/blocks/advantages.php',
-        'EDIT_TEMPLATE' => '',
+        'PATH'           => '/local/include/blocks/advantages.php',
+        'EDIT_TEMPLATE'  => '',
     ],
     null,
     [
-        'HIDE_ICONS' => 'N'
+        'HIDE_ICONS' => 'N',
     ]
 );
 
@@ -132,8 +152,8 @@ $APPLICATION->IncludeComponent(
     '',
     [
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/blocks/index.popular_brands.php',
-        'EDIT_TEMPLATE' => '',
+        'PATH'           => '/local/include/blocks/index.popular_brands.php',
+        'EDIT_TEMPLATE'  => '',
     ],
     null,
     [
@@ -142,55 +162,55 @@ $APPLICATION->IncludeComponent(
 );
 
 $APPLICATION->IncludeComponent('bitrix:main.include',
-                               'index.pet_block',
-                               [
-                                   'COMPONENT_TEMPLATE' => '.default',
-                                   'AREA_FILE_SHOW'     => 'file',
-                                   'PATH'               => '/local/include/blocks/index.pet_block.php',
-                                   'EDIT_TEMPLATE'      => '',
-                               ],
-                               false);
+    'index.pet_block',
+    [
+        'COMPONENT_TEMPLATE' => '.default',
+        'AREA_FILE_SHOW'     => 'file',
+        'PATH'               => '/local/include/blocks/index.pet_block.php',
+        'EDIT_TEMPLATE'      => '',
+    ],
+    false);
 /**
  * @todo Новости и события. Заменить компонентом и удалить файл.
  */
 $APPLICATION->IncludeComponent('fourpaws:items.list',
-                               '',
-                               [
-                                   'ACTIVE_DATE_FORMAT'     => 'j F Y',
-                                   'AJAX_MODE'              => 'N',
-                                   'AJAX_OPTION_ADDITIONAL' => '',
-                                   'AJAX_OPTION_HISTORY'    => 'N',
-                                   'AJAX_OPTION_JUMP'       => 'N',
-                                   'AJAX_OPTION_STYLE'      => 'Y',
-                                   'CACHE_FILTER'           => 'N',
-                                   'CACHE_GROUPS'           => 'Y',
-                                   'CACHE_TIME'             => '36000000',
-                                   'CACHE_TYPE'             => 'A',
-                                   'CHECK_DATES'            => 'Y',
-                                   'FIELD_CODE'             => [
-                                       '',
-                                   ],
-                                   'FILTER_NAME'            => '',
-                                   'IBLOCK_ID'              => [
-                                       IblockUtils::getIblockId(IblockType::PUBLICATION, IblockCode::NEWS),
-                                       IblockUtils::getIblockId(IblockType::PUBLICATION, IblockCode::ARTICLES),
-                                       //IblockUtils::getIblockId(IblockType::PUBLICATION, 'cloubs_and_nurderis'),//Раскоментить когда добавится инфоблок
-                                   ],
-                                   'IBLOCK_TYPE'            => IblockType::PUBLICATION,
-                                   'NEWS_COUNT'             => '7',
-                                   'PREVIEW_TRUNCATE_LEN'   => '',
-                                   'PROPERTY_CODE'          => [
-                                       'PUBLICATION_TYPE',
-                                       'VIDEO',
-                                   ],
-                                   'SET_LAST_MODIFIED'      => 'N',
-                                   'SORT_BY1'               => 'ACTIVE_FROM',
-                                   'SORT_BY2'               => 'SORT',
-                                   'SORT_ORDER1'            => 'DESC',
-                                   'SORT_ORDER2'            => 'ASC',
-                               ],
-                               false,
-                               ['HIDE_ICONS' => 'Y']);
+    '',
+    [
+        'ACTIVE_DATE_FORMAT'     => 'j F Y',
+        'AJAX_MODE'              => 'N',
+        'AJAX_OPTION_ADDITIONAL' => '',
+        'AJAX_OPTION_HISTORY'    => 'N',
+        'AJAX_OPTION_JUMP'       => 'N',
+        'AJAX_OPTION_STYLE'      => 'Y',
+        'CACHE_FILTER'           => 'N',
+        'CACHE_GROUPS'           => 'Y',
+        'CACHE_TIME'             => '36000000',
+        'CACHE_TYPE'             => 'A',
+        'CHECK_DATES'            => 'Y',
+        'FIELD_CODE'             => [
+            '',
+        ],
+        'FILTER_NAME'            => '',
+        'IBLOCK_ID'              => [
+            IblockUtils::getIblockId(IblockType::PUBLICATION, IblockCode::NEWS),
+            IblockUtils::getIblockId(IblockType::PUBLICATION, IblockCode::ARTICLES),
+            //IblockUtils::getIblockId(IblockType::PUBLICATION, 'cloubs_and_nurderis'),//Раскоментить когда добавится инфоблок
+        ],
+        'IBLOCK_TYPE'            => IblockType::PUBLICATION,
+        'NEWS_COUNT'             => '7',
+        'PREVIEW_TRUNCATE_LEN'   => '',
+        'PROPERTY_CODE'          => [
+            'PUBLICATION_TYPE',
+            'VIDEO',
+        ],
+        'SET_LAST_MODIFIED'      => 'N',
+        'SORT_BY1'               => 'ACTIVE_FROM',
+        'SORT_BY2'               => 'SORT',
+        'SORT_ORDER1'            => 'DESC',
+        'SORT_ORDER2'            => 'ASC',
+    ],
+    false,
+    ['HIDE_ICONS' => 'Y']);
 
 /**
  * Просмотренные товары
@@ -200,8 +220,8 @@ $APPLICATION->IncludeComponent(
     '',
     [
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/blocks/index.viewed_products.php',
-        'EDIT_TEMPLATE' => '',
+        'PATH'           => '/local/include/blocks/index.viewed_products.php',
+        'EDIT_TEMPLATE'  => '',
     ],
     null,
     [
@@ -217,8 +237,8 @@ $APPLICATION->IncludeComponent(
     '',
     [
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/blocks/index.seo_text.php',
-        'EDIT_TEMPLATE' => '',
+        'PATH'           => '/local/include/blocks/index.seo_text.php',
+        'EDIT_TEMPLATE'  => '',
     ],
     null,
     [
