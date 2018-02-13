d9d9510c7a683021b918bfc5fba9db7486ee1f6d
Sergey Frolov <frolov@adv.ru>
2018-02-13 15:41:36 +0300

    remove repeat items by another items

--- common/local/components/fourpaws/catalog.food.selection/templates/.default/include/food_consistence.php
+++ common/local/components/fourpaws/catalog.food.selection/templates/.default/include/food_consistence.php
@@ -14,19 +14,6 @@ if (!\is_array($sections) || empty($sections)) {
 } ?>
 <div class="b-quest b-quest--step-<?= $nextStep?> js-quest js-quest--step-<?=$nextStep?>" style="display: block">
     <h4 class="b-quest__subtitle">Тип</h4>
-    <div class="b-radio b-radio--q-food">
-        <input class="b-radio__input"
-               name="food_consistence"
-               id="id-quest-type-food-default"
-               data-radio="<?= ++$_SESSION['RADIO_NUMBER'] ?>"
-               type="radio"
-               value="0"
-               data-url="<?=$nextUrl?>"
-        >
-        <label class="b-radio__label b-radio__label--q-food" for="id-quest-type-food-default">
-            <span class="b-radio__text-label">Любой</span>
-        </label>
-    </div>
     <?php /** @var IblockSect $item */
     foreach ($sections as $key => $item) {
         ?>
