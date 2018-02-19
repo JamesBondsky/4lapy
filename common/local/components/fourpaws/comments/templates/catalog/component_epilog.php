ff193ee92e8b5d8414d272e8d4b5d9f91fe3f0e0
Sergey Frolov <frolov@adv.ru>
2018-02-19 16:51:53 +0300

    reformat code

--- common/local/components/fourpaws/comments/templates/catalog/component_epilog.php
+++ common/local/components/fourpaws/comments/templates/catalog/component_epilog.php
@@ -7,11 +7,11 @@ $request = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
 
 $uri = new \Bitrix\Main\Web\Uri($request->getRequestUri());
 $fragment = $uri->getFragment();
-if($fragment === 'new-review'){?>
-<script>
-    $(function(){
-        $('div.b-tab-title__list a[data-tab=reviews]').click();
-        $('div.b-tab-content div[data-tab-content=reviews] button.js-add-review').click();
-    });
-</script>
-<?}
+if ($fragment === 'new-review') { ?>
+    <script>
+        $(function () {
+            $('div.b-tab-title__list a[data-tab=reviews]').click();
+            $('div.b-tab-content div[data-tab-content=reviews] button.js-add-review').click();
+        });
+    </script>
+<?php }
