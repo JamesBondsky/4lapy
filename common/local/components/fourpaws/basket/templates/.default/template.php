e55125b0326c07c54b4b8836ebd5b0c43f01fb41
Sergey Frolov <frolov@adv.ru>
2018-03-13 22:12:26 +0300

    fix LP03-188

--- common/local/components/fourpaws/basket/templates/.default/template.php
+++ common/local/components/fourpaws/basket/templates/.default/template.php
@@ -8,10 +8,13 @@
  * @copyright   ADV/web-engineering co.
  */
 
-/** @global \FourPaws\Components\BasketComponent $component */
+/** @global BasketComponent $component */
 
+use Bitrix\Sale\Basket;
 use Bitrix\Sale\BasketItem;
-use FourPaws\Catalog\Model\Offer;
+use Bitrix\Sale\Order;
+use Doctrine\Common\Collections\ArrayCollection;
+use FourPaws\Components\BasketComponent;
 use FourPaws\Decorators\SvgDecorator;
 use FourPaws\Helpers\WordHelper;
 use FourPaws\SaleBundle\Entity\UserAccount;
@@ -22,11 +25,14 @@ $user = $arResult['USER'];
 /** @var UserAccount $userAccount */
 $userAccount = $arResult['USER_ACCOUNT'];
 
-/** @var \Bitrix\Sale\Basket $basket */
+/** @var Basket $basket */
 $basket = $arResult['BASKET'];
 $orderableBasket = $basket->getOrderableItems();
 
-/** @var \Bitrix\Sale\Order $order */
+/** @var ArrayCollection $notAlowedItems */
+$notAlowedItems = $arResult['NOT_ALOWED_ITEMS'];
+
+/** @var Order $order */
 $order = $basket->getOrder();
 
 if (!isset($arParams['IS_AJAX']) || $arParams['IS_AJAX'] !== true) {
@@ -137,138 +143,31 @@ if (!isset($arParams['IS_AJAX']) || $arParams['IS_AJAX'] !== true) {
                 </section>
                 <?php
             }
-            ?>
-            <section class="b-stock b-stock--shopping-cart b-stock--shopping-product js-section-remove-stock">
-                <h3 class="b-title b-title--h2-cart b-title--shopping-product">Ваш заказ</h3>
-                <?php
-                /** @var BasketItem $basketItem */
-                foreach ($orderableBasket as $basketItem) {
-                    if (isset($basketItem->getPropertyCollection()->getPropertyValues()['IS_GIFT'])) {
-                        continue;
-                    }
-                    $image = $component->getImage($basketItem->getProductId());
-                    $offer = $component->getOffer((int)$basketItem->getProductId());
-                    $useOffer = $offer instanceof Offer && $offer->getId() > 0; ?>
-                    <div class="b-item-shopping">
-                        <div class="b-common-item b-common-item--shopping-cart b-common-item--shopping">
-                        <span class="b-common-item__image-wrap b-common-item__image-wrap--shopping-cart">
-                            <?php
-                            if (null !== $image) {
-                                ?>
-                                <img class="b-common-item__image b-common-item__image--shopping-cart"
-                                     src="<?= $image; ?>"
-                                     alt="<?= $basketItem->getField('NAME') ?>" title=""/>
-                                <?php
-                            }
-                            ?>
-                        </span>
-                            <div class="b-common-item__info-center-block b-common-item__info-center-block--shopping-cart b-common-item__info-center-block--shopping">
-                                <a class="b-common-item__description-wrap b-common-item__description-wrap--shopping"
-                                   href="<?= $basketItem->getField('DETAIL_PAGE_URL'); ?>" title="">
-                                    <span class="b-clipped-text b-clipped-text--shopping-cart">
-                                        <span>
-                                            <?php if ($useOffer) { ?>
-                                                <strong><?= $offer->getProduct()->getBrandName() ?>  </strong>
-                                            <?php } ?>
-                                            <?= $basketItem->getField('NAME') ?>
-                                        </span>
-                                    </span>
-                                    <span class="b-common-item__variant b-common-item__variant--shopping-cart b-common-item__variant--shopping">
-                                         <span class="b-common-item__name-value">Вес: </span>
-                                         <span><?= WordHelper::showWeight($basketItem->getWeight(), true) ?></span>
-                                    </span>
-                                    <?php if ($useOffer) {
-                                        $color = $offer->getColor();
-                                        if ($color !== null) { ?>
-                                            <span class="b-common-item__variant b-common-item__variant--shopping-cart b-common-item__variant--shopping">
-                                                <span class="b-common-item__name-value">Цвет: </span>
-                                                <span><?= $color->getName() ?></span>
-                                            </span>
-                                        <?php }
-                                        $article = $offer->getXmlId();
-                                        if (!empty($article)) { ?>
-                                            <span class="b-common-item__variant b-common-item__variant--shopping-cart b-common-item__variant--shopping">
-                                                <span class="b-common-item__name-value">Артикул: </span>
-                                                <span class="b-common-item__name-value b-common-item__name-value--shopping-mobile">, Арт. </span><span><?= $article ?></span>
-                                            </span>
-                                        <?php }
-                                    } ?>
-                                </a>
-                                <?php if ($useOffer) {
-                                    $bonus = $component->getItemBonus($offer);
-                                    if ($bonus > 0) {
-                                        $bonus = floor($bonus); ?>
-                                        <span class="b-common-item__rank-text b-common-item__rank-text--red b-common-item__rank-text--shopping">+ <?= WordHelper::numberFormat($bonus,
-                                                0) ?>
-                                            <?= WordHelper::declension($bonus,
-                                                ['бонус', 'бонуса', 'бонусов']) ?> </span>
-                                    <?php }
-                                } ?>
-                            </div>
-                        </div>
-                        <div class="b-item-shopping__operation">
-                            <?php $offer = $component->getOffer($basketItem->getProductId());
-                            $maxQuantity = 1000;
-                            if ($useOffer) {
-                                $maxQuantity = $offer->getQuantity();
-                            } ?>
-                            <div class="b-plus-minus b-plus-minus--half-mobile b-plus-minus--shopping js-plus-minus-cont">
-                                <a class="b-plus-minus__minus js-minus" data-url="/ajax/sale/basket/update/"
-                                   href="javascript:void(0);"></a>
-
-                                <input title="" class="b-plus-minus__count js-plus-minus-count"
-                                       value="<?= WordHelper::numberFormat($basketItem->getQuantity(), 0) ?>"
-                                       data-one-price="<?= $basketItem->getPrice() ?>"
-                                       data-cont-max="<?= $maxQuantity ?>"
-                                       data-basketid="<?= $basketItem->getId(); ?>" type="text"/>
-
-                                <a class="b-plus-minus__plus js-plus" data-url="/ajax/sale/basket/update/"
-                                   href="javascript:void(0);"></a>
-                            </div>
-                            <div class="b-select b-select--shopping-cart">
-                                <?php /** @todo mobile max quantity */
-                                $maxMobileQuantity = 100;
-                                if ($maxQuantity < $maxMobileQuantity) {
-                                    $maxMobileQuantity = $maxMobileQuantity;
-                                } ?>
-                                <select title="" class="b-select__block b-select__block--shopping-cart"
-                                        name="shopping-cart">
-                                    <option value="" disabled="disabled" selected="selected">выберите</option>
-                                    <?php
-                                    for ($i = 0; $i < $maxMobileQuantity; $i++) { ?>
-                                        <option value="one-click-<?= $i ?>"><?= $i + 1 ?></option>
-                                        <?php
-                                    } ?>
-                                </select>
-                            </div>
-                            <div class="b-price">
-                                <span class="b-price__current"><?= WordHelper::numberFormat($basketItem->getPrice()
-                                        * $basketItem->getQuantity()) ?>  </span>
-                                <span class="b-ruble">₽</span>
-                                <?php
-                                if ($basketItem->getDiscountPrice() > 0) {
-                                    ?>
-                                    <span class="b-old-price b-old-price--crossed-out">
-                                        <span class="b-old-price__old"><?= WordHelper::numberFormat($basketItem->getBasePrice()
-                                                * $basketItem->getQuantity()) ?>  </span>
-                                        <span class="b-ruble b-ruble--old-weight-price">₽</span>
-                                    </span>
-                                    <?php
-                                }
-                                ?>
-                            </div>
-                            <a class="b-item-shopping__delete js-cart-delete-item" href="javascript:void(0);" title=""
-                               data-url="/ajax/sale/basket/delete/" data-basketId="<?= $basketItem->getId(); ?>">
-                            <span class="b-icon b-icon--delete b-icon--shopping">
-                                <?= new SvgDecorator('icon-delete-cart-product', 12, 14); ?>
-                            </span>
-                            </a>
-                        </div>
-                    </div>
+            if (!$orderableBasket->isEmpty()) { ?>
+                <section class="b-stock b-stock--shopping-cart b-stock--shopping-product js-section-remove-stock">
+                    <h3 class="b-title b-title--h2-cart b-title--shopping-product">Ваш заказ</h3>
                     <?php
-                }
-                ?>
-            </section>
+                    /** @var BasketItem $basketItem */
+                    foreach ($orderableBasket as $basketItem) {
+                        if (isset($basketItem->getPropertyCollection()->getPropertyValues()['IS_GIFT'])) {
+                            continue;
+                        }
+                        require __DIR__ . '/basketItem.php';
+                    }
+                    ?>
+                </section>
+            <?php }
+            if (!$notAlowedItems->isEmpty()) { ?>
+                <section class="b-stock b-stock--shopping-cart b-stock--shopping-product js-section-remove-stock">
+                    <h3 class="b-title b-title--h2-cart b-title--shopping-product">Под заказ</h3>
+                    <?php foreach ($notAlowedItems as $basketItem) {
+                        if (isset($basketItem->getPropertyCollection()->getPropertyValues()['IS_GIFT'])) {
+                            continue;
+                        }
+                        require __DIR__ . '/basketItem.php';
+                    } ?>
+                </section>
+            <?php } ?>
         </main>
 
         <aside class="b-shopping-cart__aside">
@@ -279,7 +178,7 @@ if (!isset($arParams['IS_AJAX']) || $arParams['IS_AJAX'] !== true) {
                             <span class="b-information-order__pay-points">
                                 <span class="b-information-order__name"><?= $user->getName() ?>, </span>
                                 вы можете оплатить этот заказ баллами (до <?= WordHelper::numberFormat($userAccount->getCurrentBudget()) ?>
-                                    ).
+                                ).
                             </span>
                         <?php }
                     } else { ?>
@@ -362,13 +261,14 @@ if (!isset($arParams['IS_AJAX']) || $arParams['IS_AJAX'] !== true) {
                             </span><span class="b-ruble">₽</span>
                         </div>
                     </div>
-                    <a class="b-button b-button--start-order" href="/sale/order/" title="Начать оформление">
+                    <a class="b-button b-button--start-order" href="/sale/order/"
+                       title="Начать оформление" <?= (int)$arResult['TOTAL_PRICE'] === 0 ? ' disabled' : '' ?>>
                         Начать оформление
                     </a>
                     <div class="b-information-order__one-click">
                         <a class="b-link b-link--one-click js-open-popup js-open-popup--one-click js-open-popup"
                            href="javascript:void(0)" title="Купить в 1 клик" data-popup-id="buy-one-click"
-                           data-url="/ajax/sale/fast_order/load/">
+                           data-url="/ajax/sale/fast_order/load/" <?= (int)$arResult['TOTAL_PRICE'] === 0 ? ' disabled' : '' ?>>
                             <span class="b-link__text b-link__text--one-click js-open-popup">Купить в 1 клик</span>
                         </a>
                     </div>
