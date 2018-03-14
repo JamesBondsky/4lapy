6d25ebcf6200b7390e55b011874c2be7e3f28d96
Sergey Frolov <frolov@adv.ru>
2018-03-14 23:54:36 +0300

    правки

--- common/local/components/fourpaws/basket/templates/.default/component_epilog.php
+++ common/local/components/fourpaws/basket/templates/.default/component_epilog.php
@@ -16,7 +16,7 @@ if(\is_array($offers) && !empty($offers)){
         /** @var Offer $offer */
         $offer = $templateData['currentOffer'];
         try {
-            $bonuses = $offer->getBonuses($component->getCurrentUserService()->getDiscount(), $quantity);
+            $bonuses = $offer->getBonuses($component->getCurrentUserService()->getCurrentUser()->getDiscount(), $quantity);
         } catch (\Exception $e) {
             $bonuses = 0;
         }
