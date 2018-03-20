c8b002298533e84fbd59a045486c61c2c0677279
Dmitry Panychev <panychev@adv.ru>
2018-03-20 20:06:33 +0300

    Small fix

--- src/StoreBundle/Entity/DeliverySchedule.php
+++ src/StoreBundle/Entity/DeliverySchedule.php
@@ -634,7 +634,8 @@ class DeliverySchedule extends Base
         return $result;
     }
 
-    /**
+    /** @noinspection PhpMissingParentCallCommonInspection
+     *
      * @return string
      */
     public function serialize(): string
@@ -655,7 +656,8 @@ class DeliverySchedule extends Base
         ]);
     }
 
-    /**
+    /** @noinspection PhpMissingParentCallCommonInspection
+     *
      * @param string $serialized
      * @throws ApplicationCreateException
      */
@@ -674,7 +676,8 @@ class DeliverySchedule extends Base
             $this->deliveryNumber,
             $this->deliveryDates,
             $this->type
-        ] = unserialize($serialized, ['allowed_classes' => true]);
+        ] = \unserialize($serialized, ['allowed_classes' => true]);
+
         $this->__construct();
     }
 }
