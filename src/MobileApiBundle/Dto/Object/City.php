7e83413ff251c6f7ef3135b7af0c7e4f99b7bd00
Igor Pinchuk <pinchuk@adv.ru>
2018-03-20 18:12:37 +0300

    GET /city_search/

--- src/MobileApiBundle/Dto/Object/City.php
+++ src/MobileApiBundle/Dto/Object/City.php
@@ -1,11 +1,14 @@
 <?php
 
+/*
+ * @copyright Copyright (c) ADV/web-engineering co
+ */
+
 namespace FourPaws\MobileApiBundle\Dto\Object;
 
 use JMS\Serializer\Annotation as Serializer;
 
 /**
- * @todo assert
  * ОбъектГород
  * Class City
  * @package FourPaws\MobileApiBundle\Dto\Object
@@ -20,7 +23,7 @@ class City
     protected $id = '';
 
     /**
-     * @Serializer\SerializedName("id")
+     * @Serializer\SerializedName("title")
      * @Serializer\Type("string")
      * @var string
      */
@@ -39,4 +42,76 @@ class City
      * @var array
      */
     protected $path = [];
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return City
+     */
+    public function setId(string $id): City
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return City
+     */
+    public function setTitle(string $title): City
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHasMetro(): bool
+    {
+        return $this->hasMetro;
+    }
+
+    /**
+     * @param bool $hasMetro
+     * @return City
+     */
+    public function setHasMetro(bool $hasMetro): City
+    {
+        $this->hasMetro = $hasMetro;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPath(): array
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param array $path
+     * @return City
+     */
+    public function setPath(array $path): City
+    {
+        $this->path = $path;
+        return $this;
+    }
 }
