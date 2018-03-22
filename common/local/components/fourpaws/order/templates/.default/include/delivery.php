fc42769fc90c487b882fc73235a90e27d6833d67
Соловов Дмитрий <solovov@adv.ru>
2018-03-22 16:25:02 +0300

    Исправлен переход на 3й шаг заказа

--- common/local/components/fourpaws/order/templates/.default/include/delivery.php
+++ common/local/components/fourpaws/order/templates/.default/include/delivery.php
@@ -77,7 +77,7 @@ if (!$addresses || $addresses->isEmpty()) {
         </label>
     </div>
 </div>
-<div class="b-radio-tab__new-address js-form-new-address" <?= $showNewAddressForm ? 'style="display:block"' : '' ?>>
+<div class="b-radio-tab__new-address js-form-new-address js-hidden-valid-fields active" <?= $showNewAddressForm ? 'style="display:block"' : '' ?>>
     <div class="b-input-line b-input-line--new-address">
         <div class="b-input-line__label-wrapper b-input-line__label-wrapper--back-arrow">
             <?php if ($showNewAddressFormHeader) {
