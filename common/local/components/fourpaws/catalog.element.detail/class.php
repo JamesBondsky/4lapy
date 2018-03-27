88f4c8a7885f8729da85a97110d44551ec166947
Sergey Frolov <frolov@adv.ru>
2018-03-27 22:29:06 +0300

    проверка id оффера на 0

--- common/local/components/fourpaws/catalog.element.detail/class.php
+++ common/local/components/fourpaws/catalog.element.detail/class.php
@@ -378,7 +378,7 @@ class CatalogElementDetailComponent extends \CBitrixComponent
         $offerId = (int)$this->arParams['OFFER_ID'];
 
         $offers = $product->getOffers();
-        if ($offerId) {
+        if ($offerId > 0) {
             foreach ($offers as $offer) {
                 if ($offer->getId() === $offerId) {
                     return $offer;
