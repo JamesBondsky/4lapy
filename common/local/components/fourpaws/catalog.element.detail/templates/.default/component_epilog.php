df9a9a56bcd60c57961f48a6abf01f68d4b79b4d
Sergey Frolov <frolov@adv.ru>
2018-03-27 17:26:38 +0300

    проверка на наличие итема

--- common/local/components/fourpaws/catalog.element.detail/templates/.default/component_epilog.php
+++ common/local/components/fourpaws/catalog.element.detail/templates/.default/component_epilog.php
@@ -32,8 +32,10 @@ foreach ($basket->getBasketItems() as $basketItem) { ?>
     <script type="text/javascript">
         $(function () {
             var $offerInCart = $('.js-offer-in-cart-<?=$basketItem->getProductId()?>');
-            $offerInCart.find('.b-weight-container__number').html('<?=$basketItem->getQuantity()?>');
-            $offerInCart.css('display', 'inline-block');
+            if($offerInCart.length > 0) {
+                $offerInCart.find('.b-weight-container__number').html('<?=$basketItem->getQuantity()?>');
+                $offerInCart.css('display', 'inline-block');
+            }
         });
     </script>
 <?php }
@@ -43,24 +45,27 @@ $offers = $product->getOffers();
 /** @var Offer $offer */
 foreach ($offers as $offer) {
     /** установка цен, скидочных цен, акции, нет в наличии */ ?>
-
     <script type="text/javascript">
         $(function () {
             var $offerLink = $('.js-offer-link-<?=$offer->getId()?>');
-            $offerLink.find('.b-weight-container__price').html('<?= WordHelper::numberFormat($offer->getPrice(), 0) ?> <span class="b-ruble b-ruble--weight">₽</span>');
-            $offerLink.data('price', '<?=WordHelper::numberFormat($offer->getPrice(), 0)?>');
-            <?php if(!$offer->isAvailable()){ ?>
-                $offerLink.addClass('unavailable-link');
-                $offerLink.find('.b-weight-container__not').html('Нет в наличии').css('display','inline-block');
-            <?php }
-            else{ ?>
-                <?php if($offer->isShare()){ ?>
-                    $offerLink.find('.js-offer-action').html('Акция').css('display', 'inline-block');
+            if($offerLink.length > 0) {
+                $offerLink.find('.b-weight-container__price').html('<?= WordHelper::numberFormat($offer->getPrice(),
+                    0) ?> <span class="b-ruble b-ruble--weight">₽</span>');
+                $offerLink.data('price', '<?=WordHelper::numberFormat($offer->getPrice(), 0)?>');
+                <?php if(!$offer->isAvailable()) { ?>
+                    $offerLink.addClass('unavailable-link');
+                    $offerLink.find('.b-weight-container__not').html('Нет в наличии').css('display', 'inline-block');
                 <?php }
-                if($offer->getOldPrice() !== $offer->getPrice()){?>
-                    $offerLink.find('.b-weight-container__old-price--big').html('<?=WordHelper::numberFormat($offer->getOldPrice(), 0)?> <span class="b-ruble b-ruble--old-weight-price">₽</span>').css('display', 'inline-block');
-                <?php }
-            }?>
+                else { ?>
+                    <?php if($offer->isShare()) { ?>
+                        $offerLink.find('.js-offer-action').html('Акция').css('display', 'inline-block');
+                    <?php }
+                    if($offer->getOldPrice() !== $offer->getPrice()) {?>
+                        $offerLink.find('.b-weight-container__old-price--big').html('<?=WordHelper::numberFormat($offer->getOldPrice(),
+                            0)?> <span class="b-ruble b-ruble--old-weight-price">₽</span>').css('display', 'inline-block');
+                    <?php }
+                }?>
+            }
         });
     </script>
 <?php }
