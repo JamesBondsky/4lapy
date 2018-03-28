a8c91fef81665dbab512ed5aa64e85d641405312
Соловов Дмитрий <solovov@adv.ru>
2018-03-28 16:12:58 +0300

    Исправлено отображение ограничения по доставкам для оплаты

--- src/SaleBundle/Restrictions/PaymentByDeliveryRestriction.php
+++ src/SaleBundle/Restrictions/PaymentByDeliveryRestriction.php
@@ -6,25 +6,48 @@
 
 namespace FourPaws\SaleBundle\Restrictions;
 
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
 use Bitrix\Sale\Delivery\Restrictions;
 use Bitrix\Sale\Internals\Entity;
 use Bitrix\Sale\Payment;
 use FourPaws\App\Application;
+use FourPaws\App\Exceptions\ApplicationCreateException;
+use FourPaws\DeliveryBundle\Exception\NotFoundException;
 use FourPaws\DeliveryBundle\Service\DeliveryService;
 use FourPaws\SaleBundle\Service\OrderStorageService;
 
 class PaymentByDeliveryRestriction extends Restrictions\Base
 {
+    /**
+     * @return string
+     */
     public static function getClassTitle(): string
     {
         return 'Ограничение по службам доставки, которое работает';
     }
 
+    /**
+     * @return string
+     */
     public static function getClassDescription(): string
     {
-        return 'способ оплаты будет доступна для указанных доставок';
+        return 'способ оплаты будет доступен для указанных доставок';
     }
 
+    /**
+     * @param $sum
+     * @param array $restrictionParams
+     * @param int $paySystemId
+     *
+     * @throws ArgumentException
+     * @throws ObjectPropertyException
+     * @throws SystemException
+     * @throws ApplicationCreateException
+     * @throws NotFoundException
+     * @return bool
+     */
     public static function check($sum, array $restrictionParams, $paySystemId = 0): bool
     {
         if (!$paySystemId) {
@@ -52,6 +75,11 @@ class PaymentByDeliveryRestriction extends Restrictions\Base
         return false;
     }
 
+    /**
+     * @param Entity $payment
+     *
+     * @return mixed|null|string
+     */
     protected static function extractParams(Entity $payment)
     {
         if (!$payment instanceof Payment) {
@@ -61,16 +89,27 @@ class PaymentByDeliveryRestriction extends Restrictions\Base
         return $payment->getField('SUM');
     }
 
+    /**
+     * @param int $entityId
+     *
+     * @throws ApplicationCreateException
+     * @throws ArgumentException
+     * @throws NotFoundException
+     * @throws ObjectPropertyException
+     * @throws SystemException
+     * @return array
+     */
     public static function getParamsStructure($entityId = 0): array
     {
         /** @var DeliveryService $deliveryService */
         $deliveryService = Application::getInstance()->getContainer()->get('delivery.service');
         $result = [];
 
-        foreach (DeliveryService::DELIVERY_CODES as $code) {
+        $deliveryCodes = \array_merge(DeliveryService::DELIVERY_CODES, DeliveryService::PICKUP_CODES);
+        foreach ($deliveryCodes as $code) {
             $delivery = $deliveryService->getDeliveryByCode($code);
             $result[$code] = [
-                'TYPE'  => 'Y/N',
+                'TYPE' => 'Y/N',
                 'VALUE' => 'N',
                 'LABEL' => 'Разрешить для доставки ' . $delivery['NAME'],
             ];
