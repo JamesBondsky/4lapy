41770cbb642a389b27f83ca5f3df97d83b1fc7d4
pandmitr <thor_work@yahoo.com>
2018-04-01 18:50:22 +0300

    Composite remove

--- common/local/components/fourpaws/city.selector/templates/.default/template.php
+++ common/local/components/fourpaws/city.selector/templates/.default/template.php
@@ -19,11 +19,7 @@
  * @global CDatabase              $DB
  */
 
-use FourPaws\Decorators\SvgDecorator;
-
-$this->setFrameMode(true);
-
-$frame = $this->createFrame()->begin() ?>
+use FourPaws\Decorators\SvgDecorator; ?>
 <div class="b-header__wrapper-for-popover js-city-permutation">
     <a class="b-combobox b-combobox--header js-open-popover"
        href="javascript:void(0);"
@@ -51,32 +47,3 @@ $frame = $this->createFrame()->begin() ?>
            data-popup-id="pick-city">Нет, выбрать другой</a>
     </div>
 </div>
-<?php $frame->beginStub() ?>
-<div class="b-header__wrapper-for-popover js-city-permutation">
-    <a class="b-combobox b-combobox--header js-open-popover"
-       href="javascript:void(0);"
-       title="<?= $arResult['DEFAULT_CITY']['NAME'] ?>">
-        <span class="b-icon b-icon--location">
-            <?= new SvgDecorator('icon-delivery-header', 14, 16) ?>
-        </span>
-        <span class="b-combobox__name-city"><?= $arResult['DEFAULT_CITY']['NAME'] ?></span>
-        <span class="b-icon b-icon--delivery-arrow">
-            <?= new SvgDecorator('icon-arrow-down', 13, 13) ?>
-        </span>
-    </a>
-    <div class="b-popover b-popover--blue-arrow b-popover--city js-your-city js-popover">
-        <p class="b-popover__text">Ваш город&nbsp;&mdash;
-            <span><?= $arResult['DEFAULT_CITY']['NAME'] ?></span>?
-        </p>
-        <a class="b-popover__link js-set-my-city js-get-loction"
-           href="javascript:void(0)"
-           title=""
-           data-url="<?= $arResult['CITY_SET_URL'] ?>"
-           data-code="<?= $arResult['SELECTED_CITY']['CODE'] ?>">Да</a>
-        <a class="b-popover__link b-popover__link--last js-open-popup"
-           href="javascript:void(0)"
-           title=""
-           data-popup-id="pick-city">Нет, выбрать другой</a>
-    </div>
-</div>
-<?php $frame->end() ?>
