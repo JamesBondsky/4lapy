5f2647975e6c049f55528de66fa2c7ad6ae5da42
Sergey Frolov <frolov@adv.ru>
2018-04-05 11:26:10 +0300

    добавление в миграцию создание свойства

--- common/local/php_interface/migrations/ReferralAddEventMessage20180405092223.php
+++ common/local/php_interface/migrations/ReferralAddEventMessage20180405092223.php
@@ -5,7 +5,8 @@ namespace Sprint\Migration;
 
 use Adv\Bitrixtools\Migration\SprintMigrationBase;
 
-class ReferralAddEventMessage20180405092223 extends SprintMigrationBase {
+class ReferralAddEventMessage20180405092223 extends SprintMigrationBase
+{
 
     protected $description = 'Добавление письма по отмене модерации рефералом';
 
@@ -13,7 +14,8 @@ class ReferralAddEventMessage20180405092223 extends SprintMigrationBase {
      * @return bool|void
      * @throws Exceptions\HelperException
      */
-    public function up(){
+    public function up()
+    {
         $helper = new HelperManager();
 
         $siteId = 's1';
@@ -32,19 +34,19 @@ class ReferralAddEventMessage20180405092223 extends SprintMigrationBase {
         $helper->Event()->addEventMessageIfNotExists(
             'ReferralModeratedCancel',
             [
-                'LID'      => $siteId,
-                'LANGUAGE_ID'      => $lang,
-                'EMAIL_TO' => '#EMAIL#',
-                'SUBJECT'  => 'Отмена модерации реферала',
-                'MESSAGE'  => 'Результат модерации реферала с номером карты #CARD# отрицательный. Вы можете уточнить подробности по телефону 8 (800) 770-00-22',
+                'LID'         => $siteId,
+                'LANGUAGE_ID' => $lang,
+                'EMAIL_TO'    => '#EMAIL#',
+                'SUBJECT'     => 'Отмена модерации реферала',
+                'MESSAGE'     => 'Результат модерации реферала с номером карты #CARD# отрицательный. Вы можете уточнить подробности по телефону 8 (800) 770-00-22',
             ]
         );
 
         /** пересоздание письма */
         $by = '';
         $order = '';
-        $eventId = (int)\CEventMessage::GetList($by, $order, ['TYPE_ID'=>'ReferralAdd'])->Fetch()['ID'];
-        if($eventId > 0) {
+        $eventId = (int)\CEventMessage::GetList($by, $order, ['TYPE_ID' => 'ReferralAdd'])->Fetch()['ID'];
+        if ($eventId > 0) {
             \CEventMessage::Delete($eventId);
         }
         \CEventType::Delete('ReferralAdd');
@@ -61,13 +63,57 @@ class ReferralAddEventMessage20180405092223 extends SprintMigrationBase {
         $helper->Event()->addEventMessageIfNotExists(
             'ReferralAdd',
             [
-                'LID'      => $siteId,
-                'LANGUAGE_ID'      => $lang,
-                'EMAIL_TO' => '#DEFAULT_EMAIL_FROM#',
-                'SUBJECT'  => 'Новый реферал',
-                'MESSAGE'  => 'Добавлен новый реферал с номером карты #CARD#. Необходимо произвести модерацию',
+                'LID'         => $siteId,
+                'LANGUAGE_ID' => $lang,
+                'EMAIL_TO'    => '#DEFAULT_EMAIL_FROM#',
+                'SUBJECT'     => 'Новый реферал',
+                'MESSAGE'     => 'Добавлен новый реферал с номером карты #CARD#. Необходимо произвести модерацию',
             ]
         );
 
+        /** добавление свойства отмены модерации */
+        $hlblockId = $helper->Hlblock()->getHlblockId('Referral');
+        $entityId = 'HLBLOCK_' . $hlblockId;
+
+        $helper->UserTypeEntity()->addUserTypeEntityIfNotExists(
+            $entityId,
+            'UF_CANCEL_MODERATE',
+            [
+                'FIELD_NAME'        => 'UF_CANCEL_MODERATE',
+                'USER_TYPE_ID'      => 'boolean',
+                'XML_ID'            => '',
+                'SORT'              => '150',
+                'MULTIPLE'          => 'N',
+                'MANDATORY'         => 'N',
+                'SHOW_FILTER'       => 'N',
+                'SHOW_IN_LIST'      => 'Y',
+                'EDIT_IN_LIST'      => 'Y',
+                'IS_SEARCHABLE'     => 'N',
+                'SETTINGS'          => [
+                    'DEFAULT_VALUE'  => '0',
+                    'DISPLAY'        => 'CHECKBOX',
+                    'LABEL'          => [
+                        0 => '',
+                        1 => '',
+                    ],
+                    'LABEL_CHECKBOX' => '',
+                ],
+                'EDIT_FORM_LABEL'   => [
+                    'ru' => 'Модерация отменена',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'ru' => 'Модерация отменена',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'ru' => 'Модерация отменена',
+                ],
+                'ERROR_MESSAGE'     => [
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE'      => [
+                    'ru' => 'Модерация отменена',
+                ],
+            ]
+        );
     }
 }
