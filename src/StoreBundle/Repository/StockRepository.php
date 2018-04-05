7e436fa95133171788be86f4d2589da91b649ee8
Соловов Дмитрий <solovov@adv.ru>
2018-04-05 16:28:37 +0300

    Исправлено получение остатков. Теперь по умолчанию только с активных складов

--- src/StoreBundle/Repository/StockRepository.php
+++ src/StoreBundle/Repository/StockRepository.php
@@ -3,6 +3,11 @@
 namespace FourPaws\StoreBundle\Repository;
 
 use Bitrix\Catalog\StoreProductTable;
+use Bitrix\Catalog\StoreTable;
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\Entity\Query;
+use Bitrix\Main\Entity\ReferenceField;
+use Bitrix\Main\SystemException;
 use FourPaws\StoreBundle\Collection\StockCollection;
 use FourPaws\StoreBundle\Entity\Stock;
 
@@ -25,11 +30,33 @@ class StockRepository extends BaseRepository
 
     protected function getDefaultFilter(): array
     {
-        return ['>AMOUNT' => 0];
+        return [
+            '>AMOUNT' => 0,
+            'STORE.ACTIVE' => 'Y'
+        ];
     }
 
     protected function getDefaultOrder(): array
     {
         return ['ID' => 'ASC'];
+    }
+
+    /**
+     * @param Query $query
+     * @return Query
+     * @throws ArgumentException
+     * @throws SystemException
+     */
+    protected function modifyQuery(Query $query): Query
+    {
+        $query->registerRuntimeField(
+            new ReferenceField(
+                'STORE',
+                StoreTable::class,
+                ['=this.STORE_ID' => 'ref.ID'],
+                ['join_type' => 'INNER'])
+        );
+
+        return parent::modifyQuery($query);
     }
 }
