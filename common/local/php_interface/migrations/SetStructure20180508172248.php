1566c9b68a5dfa431cd771e9a3e14e7ff045bf80
Sergey Frolov <frolov@adv.ru>
2018-05-08 17:28:44 +0300

    save

---
+++ common/local/php_interface/migrations/SetStructure20180508172248.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class SetStructure20180508172248 extends \Adv\Bitrixtools\Migration\SprintMigrationBase {
+
+    protected $description = "Структура для комплектов";
+
+    public function up(){
+        $helper = new HelperManager();
+
+        //your code ...
+
+    }
+
+    public function down(){
+        $helper = new HelperManager();
+
+        //your code ...
+
+    }
+
+}
