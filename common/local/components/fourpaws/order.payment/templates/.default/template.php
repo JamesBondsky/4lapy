17b3987b5907ea7ecbf9829c807345b230224a6c
Соловов Дмитрий <solovov@adv.ru>
2018-05-11 13:44:40 +0300

    Фикс отображения суммы заказа на странице оплаты

--- common/local/components/fourpaws/order.payment/templates/.default/template.php
+++ common/local/components/fourpaws/order.payment/templates/.default/template.php
@@ -49,6 +49,8 @@ $declension = new Declension('товар', 'товара', 'товаров');
         $relatedOrderWeight = $relatedOrder->getBasket()->getWeight();
         $relatedOrderQuantity = array_sum($relatedOrder->getBasket()->getQuantityList());
     }
+
+    var_dump($order->getPrice());
     ?>
     <div class="b-container">
         <h1 class="b-title b-title--h1 b-title--order">Оплата заказа
@@ -66,7 +68,7 @@ $declension = new Declension('товар', 'товара', 'товаров');
                     <div class="b-order__text-block b-order__text-block--gotopay">
                         <p><?= $orderQuantity ?> <?= $declension->get($orderQuantity) ?>
                             (<?= WordHelper::showWeight($orderWeight) ?>) на
-                            сумму <?= CurrencyHelper::formatPrice($order->getPrice() + $order->getDeliveryPrice()) ?></p>
+                            сумму <?= CurrencyHelper::formatPrice($order->getPrice()) ?></p>
                     </div>
                     <?php if ($order->isPaid()) { ?>
                         <button class="b-button b-button--order-step-3 b-button--next" disabled="disabled">
@@ -94,7 +96,7 @@ $declension = new Declension('товар', 'товара', 'товаров');
                             <p><?= $relatedOrderQuantity ?>  <?= $declension->get($relatedOrderQuantity) ?>
                                 (<?= WordHelper::showWeight($relatedOrderWeight) ?>)
                                 на
-                                сумму <?= CurrencyHelper::formatPrice($relatedOrder->getPrice() + $relatedOrder->getDeliveryPrice()) ?>
+                                сумму <?= CurrencyHelper::formatPrice($relatedOrder->getPrice()) ?>
                             </p>
                         </div>
                         <?php if ($relatedOrder->isPaid()) { ?>
