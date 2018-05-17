a8e096d61925a67542402fe270df66c83cc2be84
Соловов Дмитрий <solovov@adv.ru>
2018-05-17 13:22:16 +0300

    Задание региона и района складам при изменении местоположения

---
+++ src/StoreBundle/Enum/StoreFields.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * @copyright Copyright (c) ADV/web-engineering co.
+ */
+
+namespace FourPaws\StoreBundle\Enum;
+
+class StoreFields
+{
+    public const DELIVERY_TIME = 'UF_DELIVERY_TIME';
+
+    public const IS_BASE_SHOP = 'UF_IS_BASE_SHOP';
+
+    public const IS_SHOP = 'UF_IS_SHOP';
+
+    public const IS_SUPPLIER = 'UF_IS_SUPPLIER';
+
+    public const LOCATION = 'UF_LOCATION';
+
+    public const METRO = 'UF_METRO';
+
+    public const REGION = 'UF_REGION';
+
+    public const SERVICES = 'UF_SERVICES';
+
+    public const SHIPMENT_TILL_11 = 'UF_SHIPMENT_TILL_11';
+
+    public const SHIPMENT_TILL_13 = 'UF_SHIPMENT_TILL_13';
+
+    public const SHIPMENT_TILL_18 = 'UF_SHIPMENT_TILL_18';
+
+    public const SUBREGION = 'UF_SUBREGION';
+
+    public const YANDEX_SHOP_ID = 'UF_YANDEX_SHOP_ID';
+}
