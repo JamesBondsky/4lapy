4a7b4c511516f751bd3c21e3998db58e14eef7e9
Sergey Frolov <frolov@adv.ru>
2018-05-21 12:41:20 +0300

    еще 1 меню

--- web/.personal.menu.php
+++ web/.personal.menu.php
@@ -1,73 +1,89 @@
-<?
+<?php
+
+global $optId;
+
+use Bitrix\Main\GroupTable;
+use FourPaws\App\Application;
+use FourPaws\Enum\UserGroup;
+use FourPaws\UserBundle\Service\CurrentUserProviderInterface;
+
+global $optId, $isAuth, $isAvatarAuth, $USER;
+$isAuth = $USER->IsAuthorized();
+$isAvatarAuth = Application::getInstance()->getContainer()->get(CurrentUserProviderInterface::class)->isAvatarAuthorized();
+$optId = (int)GroupTable::query()->setFilter(['STRING_ID' => UserGroup::OPT_CODE])->setLimit(1)->setSelect(['ID'])->setCacheTtl(360000)->exec()->fetch()['ID'];
+if ($optId === 0) {
+    $optId = UserGroup::OPT_ID;
+}
+
 $aMenuLinks = [
     [
         'Мои заказы',
         '/personal/orders/',
         [],
         ['icon' => 'icon-order'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Адреса доставки',
         '/personal/address/',
         [],
         ['icon' => 'icon-delivery-header'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Подписка на доставку',
         '/personal/subscribe/',
         [],
         ['icon' => 'icon-delivery-menu'],
-        "\$USER->IsAuthorized()",
+        "\$isAuth",
     ],
     [
         'Мои питомцы',
         '/personal/pets/',
         [],
         ['icon' => 'icon-pet'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Бонусы',
         '/personal/bonus/',
         [],
         ['icon' => 'icon-bonus'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Реферальная программа',
         '/personal/referral/',
         [],
         ['icon' => 'icon-menu-referal'],
-        "\$USER->IsAuthorized() && \\in_array(30, \$USER->GetUserGroupArray()) && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && \\in_array(30, \$USER->GetUserGroupArray()) && !\$isAvatarAuth",
     ],
     [
         'Профиль',
         '/personal/',
         [],
         ['icon' => 'icon-profile'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Топ 10 товаров',
         '/personal/top/',
         [],
         ['icon' => 'icon-empty-star'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Выход',
         '?logout=yes',
         [],
         ['icon' => 'icon-exit'],
-        "\$USER->IsAuthorized() && !\FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && !\$isAvatarAuth",
     ],
     [
         'Вернуться',
         '/front-office/avatar/logout.php',
         [],
         ['icon' => 'icon-exit'],
-        "\$USER->IsAuthorized() && \FourPaws\App\Application::getInstance()->getContainer()->get(\FourPaws\UserBundle\Service\CurrentUserProviderInterface::class)->isAvatarAuthorized()",
+        "\$isAuth && \$isAvatarAuth",
     ],
 ];
--- web/personal/.personal_cab.menu.php
+++ web/personal/.personal_cab.menu.php
@@ -8,7 +8,7 @@
 use Bitrix\Main\GroupTable;
 use FourPaws\Enum\UserGroup;
 
-global $optId;
+global $optId, $USER;
 $optId = (int)GroupTable::query()->setFilter(['STRING_ID' => UserGroup::OPT_CODE])->setLimit(1)->setSelect(['ID'])->setCacheTtl(360000)->exec()->fetch()['ID'];
 if($optId === 0){
     $optId = UserGroup::OPT_ID;
