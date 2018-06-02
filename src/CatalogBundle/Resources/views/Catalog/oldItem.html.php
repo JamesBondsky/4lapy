826b7255478500c671a4f3b7ba6db112c9ac7866
Sergey Frolov <frolov@adv.ru>
2018-06-02 15:10:55 +0300

    удаление логирования в случае редиректа

--- src/CatalogBundle/Resources/views/Catalog/oldItem.html.php
+++ src/CatalogBundle/Resources/views/Catalog/oldItem.html.php
@@ -1,10 +1,4 @@
-<?php use Adv\Bitrixtools\Tools\Log\LoggerFactory;
-use Bitrix\Iblock\Component\Tools;
-
+<?php use Bitrix\Iblock\Component\Tools;
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
-
-$logger = LoggerFactory::create('productDetail');
-
 Tools::process404([], true, true, true);
-$logger->error('Нет итема');
 return;
