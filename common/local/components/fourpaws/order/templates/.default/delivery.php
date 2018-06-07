7f56787de3f50ea6a500a9008a52fc25cb2e094a
Соловов Дмитрий <solovov@adv.ru>
2018-06-07 16:23:04 +0300

    правка верстки 2го шага заказа

--- common/local/components/fourpaws/order/templates/.default/delivery.php
+++ common/local/components/fourpaws/order/templates/.default/delivery.php
@@ -52,7 +52,7 @@ if ($pickup && $selectedDelivery->getDeliveryCode() === $pickup->getDeliveryCode
     <h1 class="b-title b-title--h1 b-title--order">
         <?php $APPLICATION->ShowTitle() ?>
     </h1>
-    <div class="b-order js-order-whole-block">
+    <div class="b-order js-order-whole-block js-order-step2">
         <div class="b-tab-list">
             <ul class="b-tab-list__list js-scroll-order">
                 <li class="b-tab-list__item completed">
