aa35ec6dd905a4adcb9ae687fa13ce2eba96b520
Dmitry Panychev <panychev@adv.ru>
2018-06-15 12:14:35 +0300

    LP03-700 - add counters

---
+++ common/local/include/blocks/counters_header.php
@@ -0,0 +1,17 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-30730607-1" data-skip-moving="true"></script>
+<script data-skip-moving="true">
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-30730607-1');
+</script>
