852aad74f13cde2d417e1704fa4a197b02c8f659
pandmitr <thor_work@yahoo.com>
2018-06-22 00:20:26 +0200

    Check events

--- common/local/php_interface/cron_events.php
+++ common/local/php_interface/cron_events.php
@@ -13,6 +13,7 @@ define('NO_KEEP_STATISTIC', true);
 define('NOT_CHECK_PERMISSIONS', true);
 define('BX_CRONTAB', true);
 define('BX_NO_ACCELERATOR_RESET', true);
+define('CHK_EVENT', true);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
