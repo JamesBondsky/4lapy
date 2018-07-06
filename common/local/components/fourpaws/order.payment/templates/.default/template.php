81cf9b17e4ede798169094162f2eff43a0464b17
Соловов Дмитрий <solovov@adv.ru>
2018-07-06 13:15:18 +0300

    Исправлено отображение номера второго заказа на странице оплаты при разделении

--- common/local/components/fourpaws/order.payment/templates/.default/template.php
+++ common/local/components/fourpaws/order.payment/templates/.default/template.php
@@ -92,7 +92,7 @@ $declension = new Declension('товар', 'товара', 'товаров');
                     <?php } ?>
                     <input type="hidden" name="PAY" value="Y">
                     <div class="b-order__ord-wrapper">
-                        <h2 class="b-title b-title--order-heading b-title--block">Заказ №<?= $relatedOrder->getId() ?>
+                        <h2 class="b-title b-title--order-heading b-title--block">Заказ №<?= $relatedOrder->getField('ACCOUNT_NUMBER') ?>
                         </h2>
                         <div class="b-order__text-block b-order__text-block--gotopay">
                             <p><?= $relatedOrderQuantity ?>  <?= $declension->get($relatedOrderQuantity) ?>
