ca21f5d5f9de3086d90d295168bf1cad3a97f25e
leshchenko <prevedgreat@gmail.com>
2018-07-09 13:16:59 +0300

    Обновлен csv для импорта. Изменения в логике импорта.

--- common/local/php_interface/migrations/CatalogReviewsImport20180706200000.php
+++ common/local/php_interface/migrations/CatalogReviewsImport20180706200000.php
@@ -8,6 +8,7 @@ use Adv\Bitrixtools\Tools\Iblock\IblockUtils;
 use FourPaws\App\Application;
 use FourPaws\Enum\IblockCode;
 use FourPaws\Enum\IblockType;
+use FourPaws\Migrator\Entity\MapTable;
 
 class CatalogReviewsImport20180706200000 extends SprintMigrationBase
 {
@@ -25,6 +26,7 @@ class CatalogReviewsImport20180706200000 extends SprintMigrationBase
      */
     public function up()
     {
+        $this->createFields();
         $this->importReviews();
 
         return true;
@@ -35,6 +37,57 @@ class CatalogReviewsImport20180706200000 extends SprintMigrationBase
 
     }
 
+    protected function createFields()
+    {
+        $helper = new HelperManager();
+
+        $hlblockId = $helper->Hlblock()->getHlblockId(
+            $this->commentsHighloadBlockName
+        );
+        $entityId  = 'HLBLOCK_' . $hlblockId;
+
+        $fieldCode = 'UF_XML_ID';
+        $helper->UserTypeEntity()->addUserTypeEntityIfNotExists(
+            $entityId,
+            $fieldCode,
+            [
+                'FIELD_NAME' => $fieldCode,
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => $fieldCode,
+                'SORT' => '1000',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'N',
+                'SHOW_FILTER' => 'I',
+                'SHOW_IN_LIST' => 'Y',
+                'EDIT_IN_LIST' => 'Y',
+                'IS_SEARCHABLE' => 'N',
+                'SETTINGS' => [
+                    'SIZE' => 10,
+                    'ROWS' => 0,
+                    'REGEXP' => '',
+                    'MIN_LENGTH' => 0,
+                    'MAX_LENGTH' => 0,
+                    'DEFAULT_VALUE' => '',
+                ],
+                'EDIT_FORM_LABEL' => [
+                    'ru' => 'XML_ID',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'ru' => 'XML_ID',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'ru' => 'XML_ID',
+                ],
+                'ERROR_MESSAGE' => [
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE' => [
+                    'ru' => 'XML_ID',
+                ],
+            ]
+        );
+    }
+
     /**
      * @throws \Adv\Bitrixtools\Exception\IblockNotFoundException
      * @throws \Bitrix\Main\ArgumentException
@@ -42,7 +95,7 @@ class CatalogReviewsImport20180706200000 extends SprintMigrationBase
      * @throws \Bitrix\Main\SystemException
      * @throws \Exception
      */
-    public function importReviews()
+    protected function importReviews()
     {
         $filePathAbs = Application::getAbsolutePath($this->scvFilePath);
 
@@ -57,9 +110,6 @@ class CatalogReviewsImport20180706200000 extends SprintMigrationBase
         }
         $rowsMap = [];
         $firstRow = true;
-$a = 0;
-$b = 0;
-$c = 0;
         while ($row = fgetcsv($rs, null, ';', '"')) {
             if ($firstRow) {
                 $firstRow = false;
@@ -71,14 +121,8 @@ $c = 0;
 
             $tmpKey = $rowsMap['DETAIL_TEXT'];
             $text = trim($row[$tmpKey]);
-if ($text === '') {
-    ++$a;
-}
             $tmpKey = $rowsMap['DETAIL_TEXT_TYPE'];
             $textType = trim($row[$tmpKey]);
-/**
- * @todo Уточнить, можно ли html передавать
-*/
             if (mb_strtolower($textType) === 'html') {
                 $text = HTMLToTxt($text, '', [], false);
             }
@@ -86,9 +130,11 @@ if ($text === '') {
             // текст - обязательное поле
 
             if ($text === '') {
-++$b;
                 continue;
             }
+
+            $tmpKey = $rowsMap['ID'];
+            $xmlId = trim($row[$tmpKey]);
 
             $tmpKey = $rowsMap['DATE_CREATE'];
             $dateCreate = trim($row[$tmpKey]);
@@ -105,18 +151,21 @@ if ($text === '') {
             $tmpKey = $rowsMap['PROPERTY_REV_PRODUCT'];
             $externalProductOfferId = trim($row[$tmpKey]);
 
-            $internalUserId = (int)$this->getInternalUserId($externalUserId);
             $internalProductOfferId = (int)$this->getInternalProductId($externalProductOfferId);
             $internalProductId = (int)$this->getProductIdByOfferId($internalProductOfferId);
-/**
- * @todo Уточнить, нужно ли сохранять, если товар не найден
-*/
+            // не сохраняем отзывы без связи с товаром
+            if (!$internalProductId) {
+                continue;
+            }
+
+            $internalUserId = (int)$this->getInternalUserId($externalUserId);
 
             // в комментариях дата без времени
             $date = (new \DateTime($dateCreate))->format('d.m.Y');
 
-            $newId = $this->addComment(
+            $this->addComment(
                 [
+                    'UF_XML_ID' => $xmlId,
                     'UF_DATE' => $date,
                     'UF_TYPE' => 'catalog',
                     'UF_OBJECT_ID' => $internalProductId,
@@ -126,13 +175,9 @@ if ($text === '') {
                     'UF_USER_ID' => (int)$internalUserId,
                 ]
             );
-if ($newId) {
-    $c++;
-}
         }
 
         fclose($rs);
-//_log_array([$a, $b, $c], '$a');
     }
 
     /**
@@ -144,7 +189,7 @@ if ($newId) {
     protected function getInternalProductId(string $externalValue)
     {
         if ($externalValue !== '') {
-            return \FourPaws\Migrator\Entity\MapTable::getInternalIdByExternalId($externalValue, 'catalog');
+            return MapTable::getInternalIdByExternalId($externalValue, 'catalog');
         }
 
         return '';
@@ -159,7 +204,7 @@ if ($newId) {
     protected function getInternalUserId(string $externalValue)
     {
         if ($externalValue !== '') {
-            return \FourPaws\Migrator\Entity\MapTable::getInternalIdByExternalId($externalValue, 'user');
+            return MapTable::getInternalIdByExternalId($externalValue, 'user');
         }
 
         return '';
@@ -218,35 +263,22 @@ if ($newId) {
     {
         $newId = 0;
         $dataManager = $this->getCommentsDataManager();
-        // ищем комментарий по ключевым полям
-/**
- * @todo Может быть ситуация, когда на один товар в одну дату пишет несколько анонимов - из таких комментов пройдет только один.
- * Отрубить нафиг проверку?
-*/
+        // ищем комментарий
         $item = $dataManager::getList(
             [
                 'order' => [
                     'ID' => 'asc',
                 ],
                 'filter' => [
-                    '=UF_USER_ID' => $fields['UF_USER_ID'],
-                    '=UF_OBJECT_ID' => $fields['UF_OBJECT_ID'],
-                    '=UF_TYPE' => $fields['UF_TYPE'],
-                    '=UF_DATE' => $fields['UF_DATE'],
-                    '=UF_MARK' => $fields['UF_MARK'],
-                    // ?
-                    '=UF_ACTIVE' => $fields['UF_ACTIVE'],
+                    '=UF_XML_ID' => $fields['UF_XML_ID'],
                 ]
             ]
         )->fetch();
-        $addResult = null;
         if (!$item) {
             $addResult = $dataManager::add($fields);
             if ($addResult->isSuccess()) {
                 $newId = $addResult->getId();
             }
-        } else {
-//_log_array([$fields, $item], '$item');
         }
 
         return $newId;
