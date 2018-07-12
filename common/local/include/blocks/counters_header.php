433b3c5a1ca351f5e632985565da5e88dcb879be
Dmitry Panychev <panychev@adv.ru>
2018-07-12 17:38:11 +0300

    Add gtm into head

--- common/local/include/blocks/counters_header.php
+++ common/local/include/blocks/counters_header.php
@@ -5,6 +5,20 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-30730607-1" data-skip-moving="true"></script>
 <script data-skip-moving="true">
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-NXNPF4Z');
+
     window.dataLayer = window.dataLayer || [];
 
     function gtag() {
