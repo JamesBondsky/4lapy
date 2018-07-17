ce2eae50ba1fb0af4021883aa9ea289c7accf9f8
Соловов Дмитрий <solovov@adv.ru>
2018-07-17 14:54:26 +0300

    fix codestyle

--- src/SaleBundle/Enum/OrderPayment.php
+++ src/SaleBundle/Enum/OrderPayment.php
@@ -9,10 +9,7 @@ namespace FourPaws\SaleBundle\Enum;
 class OrderPayment
 {
     public const PAYMENT_CASH_OR_CARD = 'cash-or-card';
-
-    public const PAYMENT_CASH = 'cash';
-
-    public const PAYMENT_ONLINE = 'card-online';
-
-    public const PAYMENT_INNER = 'inner';
+    public const PAYMENT_CASH         = 'cash';
+    public const PAYMENT_ONLINE       = 'card-online';
+    public const PAYMENT_INNER        = 'inner';
 }
