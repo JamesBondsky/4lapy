d4f3e149951f92a604b7f08e61ddc679690ffeaa
Sergey Frolov <frolov@adv.ru>
2018-07-20 17:17:53 +0300

    фикс установки ключа - установка значения а не текста

--- src/PersonalBundle/Repository/OrderRepository.php
+++ src/PersonalBundle/Repository/OrderRepository.php
@@ -211,12 +211,12 @@ class OrderRepository extends BaseRepository
              * @var array $item
              */
             /** был PRODUCT_XML_ID или PRODUCT_ID, насколько я вижу ключ нигде не используется, а одинаковые товары в корзине быть могут с разделением */
-//            $key = !empty($item['PRODUCT_XML_ID']) ? $item['PRODUCT_XML_ID'] : '';
+//            $key = !empty($item['PRODUCT_XML_ID']) ? 'PRODUCT_XML_ID' : '';
 //            if (\mb_strlen($key) <= 1) {
-//                $key = $item['PRODUCT_ID'];
+//                $key = 'PRODUCT_ID';
 //            }
             $key = 'ID';
-            if (!isset($items[$key])) {
+            if (!isset($items[$item[$key]])) {
                 if (empty($item['PROPERTY_SELECTED'])) {
                     if (!empty($item['PROPERTY_SIZE'])) {
                         $res = HLBlockFactory::createTableObject('ClothingSize')::query()
@@ -316,7 +316,7 @@ class OrderRepository extends BaseRepository
                 }
                 $allSum += (float)$item['SUMMARY_PRICE'];
 
-                $items[$key] = $item;
+                $items[$item[$key]] = $item;
             }
         }
         if ($basketRes->getSelectedRowsCount() > 0) {
