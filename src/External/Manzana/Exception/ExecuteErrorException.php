48575ed92d7e28feca180ac7e2f21e53f606285e
Соловов Дмитрий <solovov@adv.ru>
2018-07-24 17:05:25 +0300

    Обработка ошибок манзаны при запросе чека

---
+++ src/External/Manzana/Exception/ExecuteErrorException.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @copyright Copyright (c) ADV/web-engineering co.
+ */
+
+namespace FourPaws\External\Manzana\Exception;
+
+
+class ExecuteErrorException extends ExecuteException
+{
+
+}
