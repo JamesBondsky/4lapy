4a8028d166706e82537e843697617c28f1d82f44
Соловов Дмитрий <solovov@adv.ru>
2018-08-02 11:45:35 +0300

    Запрос на сброс всех цен в YM после выгрузки фида

--- src/CatalogBundle/Console/CreateProductFeed.php
+++ src/CatalogBundle/Console/CreateProductFeed.php
@@ -6,25 +6,18 @@
 
 namespace FourPaws\CatalogBundle\Console;
 
-use Adv\Bitrixtools\Exception\IblockNotFoundException;
-use Adv\Bitrixtools\Tools\Iblock\IblockUtils;
 use Adv\Bitrixtools\Tools\Log\LazyLoggerAwareTrait;
-use Bitrix\Iblock\ElementTable;
-use Bitrix\Main\ArgumentException;
-use Bitrix\Main\Entity\Query;
-use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\SystemException;
-use CIBlockElement;
-use FourPaws\Enum\IblockCode;
-use FourPaws\Enum\IblockType;
+use FourPaws\App\Application;
+use FourPaws\App\Exceptions\ApplicationCreateException;
 use Psr\Log\LoggerAwareInterface;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Exception\LogicException;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -35,6 +28,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CreateProductFeed extends Command implements LoggerAwareInterface
 {
     use LazyLoggerAwareTrait;
+
+    public const ARG_PROFILE_ID = 'type';
+
+    public const OPT_FEED_TYPE = 'type';
+
+    public const FEED_TYPE_YANDEX_MARKET = 'yandex-market';
 
     /**
      * CreateProductFeed constructor.
@@ -56,33 +55,57 @@ class CreateProductFeed extends Command implements LoggerAwareInterface
         $this
             ->setName('bitrix:product:feed:create')
             ->setDescription('Run bitrix export task')
-            ->addArgument('id', InputArgument::REQUIRED, 'Bitrix feed id');
+            ->addArgument(static::ARG_PROFILE_ID, InputArgument::REQUIRED, 'Bitrix feed id')
+            ->addOption(
+                static::OPT_FEED_TYPE,
+                't',
+                InputOption::VALUE_REQUIRED,
+                'type of feed'
+            );
     }
 
     /** @noinspection PhpMissingParentCallCommonInspection
      *
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
      *
      * @throws RuntimeException
      * @throws InvalidArgumentException
      * @throws SystemException
+     * @throws ApplicationCreateException
      */
     public function execute(InputInterface $input, OutputInterface $output): void
     {
-        $id = $input->getArgument('id');
+        $id = $input->getArgument(static::ARG_PROFILE_ID);
+        $type = $input->getOption(static::OPT_FEED_TYPE);
         if (!$id) {
             throw new RuntimeException('Profile id not defined');
         }
 
-        if (! \CCatalogExport::GetByID($id)) {
+        if (!\CCatalogExport::GetByID($id)) {
             throw new RuntimeException(\sprintf('Profile with id #%s not found', $id));
         }
 
         if (!\CCatalogExport::PreGenerateExport($id)) {
             $this->log()->error(\sprintf('Failed to generate feed for profile #%s', $id));
         }
-        $this->log()->info('Task finished');
+
+        $this->runAfterExport($type);
+
+        $this->log()->info(\sprintf('Task #%s (%s) finished', $id, $type));
+    }
+
+    /**
+     * @param string $type
+     * @throws ApplicationCreateException
+     */
+    protected function runAfterExport(string $type)
+    {
+        switch ($type) {
+            case static::FEED_TYPE_YANDEX_MARKET:
+                Application::getInstance()->getContainer()->get('yandex_market.service')->deleteAllPrices();
+                break;
+        }
     }
 }
 
