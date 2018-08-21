e6108636293167de4dd5d1b9c3e3ab03d7925dd4
Dmitry Panychev <panychev@adv.ru>
2018-08-21 19:34:55 +0300

    Product cart optimize - fix cache

--- common/local/include/blocks/viewed_products.php
+++ common/local/include/blocks/viewed_products.php
@@ -1,9 +1,12 @@
-<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 /**
  * Блок просмотренные товары
  * Используется на списочных страницах каталога, результатах поиска, карточки бренда
+ *
  * @updated: 15.01.2018
  */
 
@@ -13,14 +16,14 @@
 $APPLICATION->IncludeFile(
     'blocks/components/viewed_products.php',
     [
-        'WRAP_CONTAINER_BLOCK' => isset($arParams['WRAP_CONTAINER_BLOCK']) ? $arParams['WRAP_CONTAINER_BLOCK'] : 'Y',
-        'WRAP_SECTION_BLOCK' => isset($arParams['WRAP_SECTION_BLOCK']) ? $arParams['WRAP_SECTION_BLOCK'] : 'Y',
-        'SHOW_TOP_LINE' => isset($arParams['SHOW_TOP_LINE']) ? $arParams['SHOW_TOP_LINE'] : 'Y',
-        'SHOW_BOTTOM_LINE' => isset($arParams['SHOW_BOTTOM_LINE']) ? $arParams['SHOW_BOTTOM_LINE'] : 'N',
+        'WRAP_CONTAINER_BLOCK' => $arParams['WRAP_CONTAINER_BLOCK'] ?? 'Y',
+        'WRAP_SECTION_BLOCK'   => $arParams['WRAP_SECTION_BLOCK'] ?? 'Y',
+        'SHOW_TOP_LINE'        => $arParams['SHOW_TOP_LINE'] ?? 'Y',
+        'SHOW_BOTTOM_LINE'     => $arParams['SHOW_BOTTOM_LINE'] ?? 'N',
     ],
     [
         'SHOW_BORDER' => false,
-        'NAME' => 'Блок просмотренных товаров',
-        'MODE' => 'php',
+        'NAME'        => 'Блок просмотренных товаров',
+        'MODE'        => 'php',
     ]
 );
