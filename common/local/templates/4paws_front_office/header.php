28593eb97bf816734c9c3e66b64cc47fe8900d8e
Макеев <makeev@adv.ru>
2018-08-23 16:40:30 +0300

    вернул доктайп

--- common/local/templates/4paws_front_office/header.php
+++ common/local/templates/4paws_front_office/header.php
@@ -10,7 +10,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 
 use Bitrix\Main\Page\Asset;
 
-?><!DOCTYPE html>
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="ru">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
