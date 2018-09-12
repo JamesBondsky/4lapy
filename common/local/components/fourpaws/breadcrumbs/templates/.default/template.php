a73e4727e0f16442e275c55b6b1e73b41af9130e
Dmitry Panychev <panychev@adv.ru>
2018-09-12 20:23:06 +0300

    LP23-211 - breadcrumbs

--- common/local/components/fourpaws/breadcrumbs/templates/.default/template.php
+++ common/local/components/fourpaws/breadcrumbs/templates/.default/template.php
@@ -9,30 +9,27 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 ?>
 <nav class="b-breadcrumbs <?= $arParams['ADDITIONAL_CLASS'] ?? '' ?>">
     <ul class="b-breadcrumbs__list">
-        <?php if ($arParams['IS_LANDING']) { ?>
+        <?php if ($arParams['SHOW_LINK_TO_MAIN'] === 'Y' && !$arParams['IS_LANDING']) { ?>
             <li class="b-breadcrumbs__item">
-                <a class="b-counter-basket__basket-link" style="position: static;width: auto;" href="<?= $arResult['BACK_LINK'] ?>" title="Вернуться в каталог">Вернуться в каталог</a>
+                <a class="b-breadcrumbs__link" href="/" title="Главная">Главная</a>
             </li>
-        <?php } else {
-            if ($arParams['SHOW_LINK_TO_MAIN'] === 'Y' && !$arParams['IS_LANDING']) { ?>
-                <li class="b-breadcrumbs__item">
-                    <a class="b-breadcrumbs__link" href="/" title="Главная">Главная</a>
-                </li>
-            <?php }
+        <?php }
 
-            if ($arParams['IS_CATALOG'] && !$arParams['IS_LANDING']) { ?>
-                <li class="b-breadcrumbs__item">
-                    <a class="b-breadcrumbs__link" href="/catalog/" title="Каталог">Каталог</a>
-                </li>
-            <?php }
+        if ($arParams['IS_CATALOG'] && !$arParams['IS_LANDING']) { ?>
+            <li class="b-breadcrumbs__item">
+                <a class="b-breadcrumbs__link" href="/catalog/" title="Каталог">Каталог</a>
+            </li>
+        <?php }
 
-            foreach ($arResult['SECTIONS'] as $k => $section) { ?>
-                <li class="b-breadcrumbs__item">
-                    <a class="b-breadcrumbs__link"
-                       href="<?= $section['URL'] ?>"
-                       title="<?= $section['NAME'] ?>"><?= $section['NAME'] ?></a>
-                </li>
-            <?php }
-        } ?>
+        foreach ($arResult['SECTIONS'] as $k => $section) {
+            if (!$k && $arParams['IS_LANDING']) {
+                continue;
+            } ?>
+            <li class="b-breadcrumbs__item">
+                <a class="b-breadcrumbs__link"
+                   href="<?= $section['URL'] ?>"
+                   title="<?= $section['NAME'] ?>"><?= $section['NAME'] ?></a>
+            </li>
+            <?php } ?>
     </ul>
 </nav>
