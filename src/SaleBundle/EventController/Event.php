9f207a15e317c2cc827dad0866f4fe0943e12b9e
Дмитрий Соловов <solovov@adv.ru>
2018-09-21 11:32:08 +0300

    LP23-279 добавлен обработчик на удаление товара из корзины для забытых корзин

--- src/SaleBundle/EventController/Event.php
+++ src/SaleBundle/EventController/Event.php
@@ -204,6 +204,10 @@ class Event extends BaseServiceHandler
             self::class,
             'disableForgotBasketReminder'
         ], $module);
+        static::initHandler('OnSaleBasketItemEntityDeleted', [
+            self::class,
+            'disableForgotBasketReminder'
+        ], $module);
     }
 
     public static function updateUserAccountBalance(): void
