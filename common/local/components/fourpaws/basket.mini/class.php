163d9d317f334b035471bf05f27a8632938f90cf
Дмитрий Соловов <solovov@adv.ru>
2018-09-26 20:26:40 +0300

    Рефактор компонента basket.mini - исправил проблему с сериализацией arResult при кешировании

--- common/local/components/fourpaws/basket.mini/class.php
+++ common/local/components/fourpaws/basket.mini/class.php
@@ -10,21 +10,19 @@ declare(strict_types=1);
 
 namespace FourPaws\Components;
 
+use Adv\Bitrixtools\Tools\BitrixUtils;
 use Bitrix\Main\ArgumentException;
 use Bitrix\Main\ArgumentNullException;
 use Bitrix\Main\ArgumentOutOfRangeException;
 use Bitrix\Main\SystemException;
 use Bitrix\Sale\Basket;
-use Bitrix\Sale\BasketBase;
 use Bitrix\Sale\BasketItem;
 use CBitrixComponent;
 use FourPaws\App\Application;
 use FourPaws\AppBundle\Bitrix\FourPawsComponent;
-use FourPaws\BitrixOrm\Model\ResizeImageDecorator;
-use FourPaws\Catalog\Model\Offer;
-use FourPaws\Catalog\Query\OfferQuery;
 use FourPaws\Helpers\TaggedCacheHelper;
 use FourPaws\SaleBundle\Exception\BasketUserInitializeException;
+use FourPaws\SaleBundle\Exception\InvalidArgumentException;
 use FourPaws\SaleBundle\Service\BasketService;
 use FourPaws\SaleBundle\Service\BasketUserService;
 use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
@@ -39,19 +37,15 @@ use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
  */
 class BasketMiniComponent extends FourPawsComponent
 {
-    /** @var BasketService */
-    public $basketService;
+    /**
+     * @var BasketService
+     */
+    protected $basketService;
 
-    /** @var BasketUserService */
+    /**
+     * @var BasketUserService
+     */
     protected $basketUserService;
-
-    /** @var array */
-    public $offers;
-
-    /** @var array $images */
-    private $images;
-    /** @var Basket */
-    private $basketItemsWithoutGifts;
 
     /**
      * BasketMiniComponent constructor.
@@ -88,90 +82,38 @@ class BasketMiniComponent extends FourPawsComponent
     }
 
     /**
-     * Prepare component result
+     * @throws ArgumentException
+     * @throws ArgumentNullException
+     * @throws \Exception
+     * @throws InvalidArgumentException
+     * @throws \RuntimeException
      */
     public function prepareResult(): void
     {
         /** @var Basket $basket */
         $basket = $this->basketService->getBasket(false, $this->arParams['FUSER_ID']);
+
+        $basketItems = [];
+        /** @var BasketItem $basketItem */
+        foreach ($basket as $basketItem) {
+            $basketItems[] = [
+                'ID'              => $basketItem->getId(),
+                'NAME'            => $basketItem->getField('NAME'),
+                'WEIGHT'          => $basketItem->getWeight(),
+                'PRODUCT_ID'      => $basketItem->getProductId(),
+                'PRICE'           => $basketItem->getPrice(),
+                'BASKE_PRICE'     => $basketItem->getBasePrice(),
+                'QUANTITY'        => $basketItem->getQuantity(),
+                'IS_GIFT'         => $basketItem->getPropertyCollection()->getPropertyValues()['IS_GIFT']['VALUE']
+                    ? BitrixUtils::BX_BOOL_TRUE
+                    : BitrixUtils::BX_BOOL_FALSE,
+                'DETAIL_PAGE_URL' => $basketItem->getField('DETAIL_PAGE_URL'),
+            ];
+        }
+
         TaggedCacheHelper::addManagedCacheTag('basket:' . $basket->getFUserId());
-        $this->arResult['BASKET'] = $basket;
-    }
-
-    /**
-     * @param int $offerId
-     *
-     * @return Offer|null
-     */
-    public function getOffer(int $offerId): ?Offer
-    {
-        if ($offerId <= 0) {
-            return null;
-        }
-        if (!isset($this->offers[$offerId])) {
-            $this->offers[$offerId] = OfferQuery::getById($offerId,
-                [
-                    'ID',
-                    'IBLOCK_ID',
-                    'PROPERTY_IMG',
-                    'PROPERTY_CML2_LIK',
-                ]);
-        }
-
-        return $this->offers[$offerId];
-    }
-
-    /**
-     * @param int $offerId
-     *
-     * @return ResizeImageDecorator|null
-     */
-    public function getImage(int $offerId): ?ResizeImageDecorator
-    {
-        if ($offerId <= 0) {
-            return null;
-        }
-        if (!isset($this->images[$offerId])) {
-            $offer = $this->getOffer($offerId);
-            $image = null;
-            if ($offer !== null) {
-                $images = $offer->getResizeImages(110, 110);
-                $this->images[$offerId] = $images->first();
-            }
-        }
-
-        return $this->images[$offerId];
-    }
-
-    /**
-     *
-     *
-     * @param Basket $basket
-     *
-     * @return int
-     */
-    public function getBasketCountWithoutGifts(Basket $basket): int
-    {
-        return $this->getBasketItemsWithoutGifts($basket)->count();
-    }
-
-    /**
-     * @param Basket $basket
-     *
-     * @return BasketBase
-     */
-    public function getBasketItemsWithoutGifts(Basket $basket): BasketBase
-    {
-        if ($this->basketItemsWithoutGifts === null) {
-            $this->basketItemsWithoutGifts = Basket::create($basket->getSiteId());
-            /** @var BasketItem $basketItem */
-            foreach ($basket->getBasketItems() as $basketItem) {
-                if ($this->basketService->isBasketPropEmpty($basketItem->getId(), 'IS_GIFT')) {
-                    $this->basketItemsWithoutGifts->addItem($basketItem);
-                }
-            }
-        }
-
-        return $this->basketItemsWithoutGifts;
+        $this->arResult = [
+            'BASKET' => $basketItems,
+        ];
     }
 }
