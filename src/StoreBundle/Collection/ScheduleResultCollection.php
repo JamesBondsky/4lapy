899028027a0ff026065a64e6e74c4269206bd43f
Дмитрий Соловов <solovov@adv.ru>
2018-10-01 15:42:54 +0300

    LP23-288 исправлена 500ая при расчете доставок
    Конструктор в FourPaws\StoreBundle\Entity\ScheduleResult не вызывался
    при получении из базы (при десериализации из кеша вызывался)

--- src/StoreBundle/Collection/ScheduleResultCollection.php
+++ src/StoreBundle/Collection/ScheduleResultCollection.php
@@ -5,53 +5,11 @@
 
 namespace FourPaws\StoreBundle\Collection;
 
-use Bitrix\Main\ArgumentException;
 use FourPaws\StoreBundle\Entity\ScheduleResult;
 use FourPaws\StoreBundle\Entity\Store;
-use FourPaws\StoreBundle\Exception\NotFoundException;
 
 class ScheduleResultCollection extends BaseCollection
 {
-    /**
-     * @return StoreCollection
-     * @throws ArgumentException
-     * @throws NotFoundException
-     */
-    public function getSenders(): StoreCollection
-    {
-        $result = new StoreCollection();
-        /** @var ScheduleResult $item */
-        foreach ($this->getIterator() as $item) {
-            if (isset($result[$item->getSenderCode()])) {
-                continue;
-            }
-
-            $result[$item->getSenderCode()] = $item->getSender();
-        }
-
-        return $result;
-    }
-
-    /**
-     * @return StoreCollection
-     * @throws ArgumentException
-     * @throws NotFoundException
-     */
-    public function getReceivers(): StoreCollection
-    {
-        $result = new StoreCollection();
-        /** @var ScheduleResult $item */
-        foreach ($this->getIterator() as $item) {
-            if (isset($result[$item->getReceiverCode()])) {
-                continue;
-            }
-
-            $result[$item->getReceiverCode()] = $item->getReceiver();
-        }
-
-        return $result;
-    }
-
     /**
      * @param Store $sender
      *
