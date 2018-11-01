409e0099298d075f24dbde230e9f2f5afeec6abc
Dev 4 lapy <dev@articul.ru>
2018-11-01 15:35:55 +0300

    tm-29489 из иконок соц.сетей убрала механзм модуля маркетплейса

--- common/local/templates/4paws/blocks/footer/social_links.php
+++ common/local/templates/4paws/blocks/footer/social_links.php
@@ -8,44 +8,39 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 <div class="b-social">
     <ul class="b-social__list">
         <li class="b-social__item">
-            <a class="b-social__link" href="<?= tplvar('social_link_vk') ?>" title="ВКонтакте" target="_blank">
+            <a class="b-social__link" href="http://vk.com/4lapy_ru" title="ВКонтакте" target="_blank">
                 <span class="b-icon b-icon--vk">
                     <?= new SvgDecorator('icon-vk', 22, 18) ?>
                 </span>
             </a>
-            <?= tplinvis('social_link_vk') ?>
         </li>
         <li class="b-social__item">
-            <a class="b-social__link" href="<?= tplvar('social_link_fb') ?>" title="Facebook" target="_blank">
+            <a class="b-social__link" href="https://www.facebook.com/4laps" title="Facebook" target="_blank">
                 <span class="b-icon b-icon--fb">
                     <?= new SvgDecorator('icon-fb', 9, 18) ?>
                 </span>
             </a>
-            <?= tplinvis('social_link_fb') ?>
         </li>
         <li class="b-social__item">
-            <a class="b-social__link" href="<?= tplvar('social_link_ok') ?>" title="Одноклассники" target="_blank">
+            <a class="b-social__link" href="https://ok.ru/chetyre.lapy" title="Одноклассники" target="_blank">
                 <span class="b-icon b-icon--ok">
                     <?= new SvgDecorator('icon-ok', 11, 18) ?>
                 </span>
             </a>
-            <?= tplinvis('social_link_ok') ?>
         </li>
         <li class="b-social__item">
-            <a class="b-social__link" href="<?= tplvar('social_link_youtube') ?>" title="Youtube" target="_blank">
+            <a class="b-social__link" href="https://www.youtube.com/channel/UCduvxcmOQFwTewukh9DUpvQ" title="Youtube" target="_blank">
                 <span class="b-icon b-icon--youtube">
                     <?= new SvgDecorator('icon-youtube', 23, 23) ?>
                 </span>
             </a>
-            <?= tplinvis('social_link_in') ?>
         </li>
         <li class="b-social__item">
-            <a class="b-social__link" href="<?= tplvar('social_link_inst') ?>" title="Instagram" target="_blank">
+            <a class="b-social__link" href="https://www.instagram.com/4lapy.ru/" title="Instagram" target="_blank">
                 <span class="b-icon b-icon--inst">
                     <?= new SvgDecorator('icon-inst', 23, 23) ?>
                 </span>
             </a>
-            <?= tplinvis('social_link_inst') ?>
         </li>
     </ul>
 </div>
