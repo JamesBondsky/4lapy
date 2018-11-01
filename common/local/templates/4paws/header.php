b606826c8ff67497ce8bd73cd727a06a0b567698
siplay <siplay92@gmail.com>
2018-11-01 17:03:18 +0300

    tm-29488 fix detail page title

--- common/local/templates/4paws/header.php
+++ common/local/templates/4paws/header.php
@@ -33,7 +33,7 @@ $markup = PawsApplication::markup(); ?><!DOCTYPE html>
         window.dataLayer = window.dataLayer || [];
     </script>
     <?php $APPLICATION->ShowHead(); ?>
-    <title><?php $APPLICATION->ShowTitle(false) ?></title>
+    <title><?php $APPLICATION->ShowTitle() ?></title>
     <?php
     $asset = Asset::getInstance();
     $asset->addCss($markup->getCssFile());
