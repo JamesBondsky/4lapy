dd75683fc461fb2f384dc759acf6bbd3d839b487
adutov <a.dutov@articul.ru>
2018-11-26 16:05:50 +0300

    mobile header fix styles

--- common/local/templates/4paws/header.php
+++ common/local/templates/4paws/header.php
@@ -90,14 +90,10 @@ $sViewportCookie = $_COOKIE['viewport'] ?? null;
 <header class="b-header <?= $template->getHeaderClass() ?> js-header">
     <div class="b-mobile-app hidden js-banner-mobile-app">
         <div class="b-mobile-app__banner hidden js-banner-mobile-app-android">
-            <a href="#" class="b-mobile-app__link">
-                <img src="/static/build/images/inhtml/app_googleplay.png" class="b-mobile-app__link-banner">
-            </a>
+            <a href="#" class="b-mobile-app__link" style="background-image: url('/static/build/images/inhtml/app_googleplay.png')" target="_blank"></a>
         </div>
         <div class="b-mobile-app__banner hidden js-banner-mobile-app-ios">
-            <a href="#" class="b-mobile-app__link">
-                <img src="/static/build/images/inhtml/app_appstore.png" class="b-mobile-app__link-banner">
-            </a>
+            <a href="#" class="b-mobile-app__link" style="background-image: url('/static/build/images/inhtml/app_appstore.png')" target="_blank"></a>
         </div>
         <div class="b-mobile-app__close-wrap">
             <button type="button" class="b-mobile-app__close js-banner-mobile-app-close"></button>
