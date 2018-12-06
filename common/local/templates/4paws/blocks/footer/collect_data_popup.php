b0f94afb9b57657c857d23ad3e781a4be628194d
Alexandr <a.dutov@articul.ru>
2018-12-06 10:33:20 +0000

    Revert "collect_data_popup.php fix online in bitbucket (pull request #123)"
    This reverts pull request #123.
    
    > collect_data_popup.php fix online in bitbucket

--- common/local/templates/4paws/blocks/footer/collect_data_popup.php
+++ common/local/templates/4paws/blocks/footer/collect_data_popup.php
@@ -23,15 +23,16 @@ if($USER->IsAuthorized()) {
                 $user_data = CUser::GetByID( $USER->GetID() )->Fetch();
                 if($user_data['UF_SESSION_CNTS'] % 3 == 1) // Каждая 3-я сессия
                 {
-                    if($user_data['NAME'] && $user_data['PERSONAL_PHONE'] || $modal_counts[0] > 2)
+                    if($user_data['NAME'] && $user_data['PERSONAL_PHONE'])
                     {
-                        if($user_data['LAST_NAME'] && $user_data['EMAIL'] || || $modal_counts[1] > 2)
+                        if($user_data['LAST_NAME'] && $user_data['EMAIL'])
                         {
                             $container = App::getInstance()->getContainer();
                             $pets = $container->get('pet.service');
-                            if(count($pets->getCurUserPets()) || $modal_counts[2] > 2) $user_class->Update($USER->GetID(), ['UF_MODALS_CNTS' => '3 3 3']);
+                            if(count($pets->getCurUserPets())) $user_class->Update($USER->GetID(), ['UF_MODALS_CNTS' => '3 3 3']);
                             else{
-                                $modal_number = 3;
+                                if($modal_counts[2] > 2) $user_class->Update($USER->GetID(), ['UF_MODALS_CNTS' => '3 3 3']);
+                                else $modal_number = 3;
                             }
                         }
                         else {
