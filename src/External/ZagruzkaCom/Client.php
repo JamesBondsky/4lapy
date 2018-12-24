d9380d26211f4e33e7e7495ac3267d1a304d062c
Stepan Mamontov <s.mamontov@articul.ru>
2018-12-24 19:40:55 +0300

    TM-30602 Новая служба отправки смс

---
+++ src/External/ZagruzkaCom/Client.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace FourPaws\External\ZagruzkaCom;
+
+
+class Client
+{
+
+    /**
+     * API URL
+     */
+    public const API_URL = 'http://lk.zagruzka.com:9080';
+
+    /**
+     * Login and url postfix
+     * @var string
+     */
+    protected $login;
+
+    /**
+     * Password
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Message sender
+     * @var string
+     */
+    protected $originator = '4lapy';
+
+    /**
+     * Client constructor.
+     * @param string $login Sms Traffic login
+     * @param string $password Sms Traffic Password
+     * @param string|null $originator Sms sender
+     */
+    public function __construct($login, $password, $originator = '4lapy')
+    {
+        $this->login = $login;
+        $this->password = $password;
+        $this->originator = $originator;
+    }
+
+    /**
+     * @param $sms Sms
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function send($sms) {
+
+        $query = [
+            'serviceId' => $this->login,
+            'pass' => $this->password,
+            'source' => $this->originator,
+            'message' => $sms->getMessage(),
+            'clientId' => $sms->getPhone(),
+        ];
+
+        if ($sms->getFromHour() && $sms->getToHour()) {
+            $query['sending_time'] = implode('_', [$sms->getFromHour(), $sms->getToHour()]);
+        }
+
+        $client = new \GuzzleHttp\Client();
+        $result = $client->get($this::API_URL.'/'.$this->login, [
+            'query' => $query,
+        ]);
+
+        return $result;
+    }
+
+}
