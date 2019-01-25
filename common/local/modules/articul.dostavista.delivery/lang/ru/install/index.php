117097449582e7ceeeb5a0093515a407f13b6c5e
adutov <a.dutov@articul.ru>
2019-01-25 17:08:40 +0300

    rename module

--- common/local/modules/articul.dostavista.delivery/lang/ru/install/index.php
+++ common/local/modules/articul.dostavista.delivery/lang/ru/install/index.php
@@ -1,4 +1,4 @@
 <?
-$MESS['ARTICUL_DOSTAVISTA_DELIVERY_NAME'] = 'Модуль доставки "Достависта"';
-$MESS['ARTICUL_DOSTAVISTA_DELIVERY_DESCRIPTION'] = 'Модуль доставки "Достависта"';
+$MESS['ARTICUL_DOSTAVISTA_DELIVERY_NAME'] = 'Служба доставки "Достависта"';
+$MESS['ARTICUL_DOSTAVISTA_DELIVERY_DESCRIPTION'] = 'Служба доставки "Достависта"';
 ?>
