cfd9994a6572397c88b4b197617d1dec28238427
Stepan Mamontov <s.mamontov@articul.ru>
2019-02-26 13:02:39 +0300

    TM-31341 генерация штрих кода на основе купона (base64, code_128)

--- common/local/components/fourpaws/personal.piggybank/templates/.default/template.php
+++ common/local/components/fourpaws/personal.piggybank/templates/.default/template.php
@@ -97,7 +97,11 @@ $isActiveNextType = $arResult['IS_ACTIVE_NEXT_TYPE'];
                         </div>
                         <div class="b-sale-coupon-kopilka__barcode">
                             <div class="b-sale-coupon-kopilka__barcode-img">
-                                <img src="/static/build/images/content/barcode-kopilka.png" alt="" /><? //TODO generate barcode (look github repo) ?>
+                            </div>
+                            <div class="b-sale-coupon-kopilka__barcode-img">
+                                <? $barcodeGenerator = new \Picqer\Barcode\BarcodeGeneratorPNG()?>
+                                <img src="data:image/png;base64,<?=base64_encode($barcodeGenerator->getBarcode($arResult['ACTIVE_COUPON']['COUPON_NUMBER'], \Picqer\Barcode\BarcodeGenerator::TYPE_CODE_128, 2.803149606299213, 127))?>" alt="" />
+                                <?/* <img src="/static/build/images/content/barcode-kopilka.png" alt="" /> */?>
                             </div>
                             <a href="#"class="link">Отправить мне на Email</a><? //TODO отправка ?>
                         </div>
