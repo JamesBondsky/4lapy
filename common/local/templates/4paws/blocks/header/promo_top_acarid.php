b0edf9560c734666ae05727144213207e1c9e1de
Daria <d.gritsaenko@articul.ru>
2019-03-12 15:40:07 +0300

    tm-32003 промо баннер блошинка в шапке

---
+++ common/local/templates/4paws/blocks/header/promo_top_acarid.php
@@ -0,0 +1,8 @@
+<div class="b-promo-top-acarid js-promo-top-acarid hide">
+    <div class="b-promo-top-acarid__container">
+        <img src="/static/build/images/inhtml/promo-banner_acarid.png" class="b-promo-top-acarid__img">
+        <div class="b-promo-top-acarid__title">А&nbsp;вы&nbsp;купили таблетки и&nbsp;ошейники от&nbsp;паразитов?</div>
+        <a href="#" class="b-promo-top-acarid__btn">Подробнее</a>
+        <div class="b-promo-top-acarid__close js-close-promo-top-acarid"></div>
+    </div>
+</div>
