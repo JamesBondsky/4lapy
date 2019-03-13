18188b34f68e8c756c5d0843eccc41246624a5a8
adutov <a.dutov@articul.ru>
2019-03-13 17:04:41 +0300

    tm-32024 first part

--- common/local/components/fourpaws/catalog.element.detail/class.php
+++ common/local/components/fourpaws/catalog.element.detail/class.php
@@ -157,9 +157,31 @@ class CatalogElementDetailComponent extends \CBitrixComponent
             $sectionId = (int)current($product->getSectionsIdList());
 
             $offersXmlIds = [];
-            /** @var Offer $offer */
-            foreach ($product->getOffersSorted() as $offer) {
-                $offersXmlIds[] = $offer->getXmlId();
+
+            foreach (['flavour', 'color'] as $combination) {
+                if (count($offersXmlIds) < 2) {
+                    $propVal = ($combination == 'flavour') ? $currentOffer->getFlavourCombination() : $currentOffer->getColourCombination();
+                    if ($propVal != '' && $propVal != null) {
+                        $unionOffers = $this->getOffersByUnion($combination, $propVal);
+                        if (!$unionOffers->isEmpty()) {
+                            /** @var Offer $unionOffer */
+                            foreach ($unionOffers as $unionOffer) {
+                                $offerXmlID = $unionOffer->getXmlId();
+                                if (!in_array($offerXmlID, $offersXmlIds)) {
+                                    $offersXmlIds[] = $offerXmlID;
+                                }
+                            }
+                            ksort($offersXmlIds);
+                        }
+                    }
+                }
+            }
+
+            if (count($offersXmlIds) < 2) {
+                /** @var Offer $offer */
+                foreach ($product->getOffersSorted() as $offer) {
+                    $offersXmlIds[] = $offer->getXmlId();
+                }
             }
 
             $this->arResult = [
