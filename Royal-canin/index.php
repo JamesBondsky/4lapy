88e65cc33394566464275798e3339b22ec4dc364
Daria <d.gritsaenko@articul.ru>
2019-03-18 21:01:23 +0300

    tm-32122 верстка и настройки лендинга Royal Canin

---
+++ Royal-canin/index.php
@@ -0,0 +1,92 @@
+<?php
+
+use FourPaws\Helpers\ProtectorHelper;
+
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+
+$APPLICATION->SetPageProperty('title', '');
+$APPLICATION->SetPageProperty('description', '');
+$APPLICATION->SetTitle('');
+?>
+
+<section id="registr-check" data-id-section-landing="registr-check" class="registr-check-landing registr-check-landing_white">
+    <div class="container-landing">
+
+        <?if ($USER->IsAuthorized()) {?>
+            <? $arUser = \CUser::GetById($USER->GetID())->Fetch(); ?>
+
+            <div class="registr-check-landing__form-wrap" data-wrap-form-registr-chek-landing="true">
+                <div class="landing-title landing-title_gray-dark">
+                    Регистрация чека
+                </div>
+                <div class="registr-check-landing__form-info">
+                    Все поля обязательны для заполнения
+                </div>
+                <form data-form-registr-check-landing="true" class="form-landing registr-check-landing__form js-form-validation" method="post" action="" name="" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <input type="dateDatepicker" id="DATE_REG_CHECK_CANIN" name="date" value="" placeholder="Дата чека" data-datepicker-landing="true" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input data-price-check-landing="true" type="minPriceLanding" data-min-price-landing="1800" id="SUM_REG_CHECK_CANIN" name="sum" value="" placeholder="Сумма чека (не менее 1800р)" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" id="SURNAME_REG_CHECK_CANIN" class="js-small-input" name="surname" value="<?=$arUser['LAST_NAME']?:''?>" placeholder="Фамилия" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" id="NAME_REG_CHECK_CANIN" class="js-small-input" name="name" value="<?=$arUser['NAME']?:''?>" placeholder="Имя" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="tel" id="PHONE_REG_CHECK_CANIN" name="phone" value="<?=$arUser['PERSONAL_PHONE']?:''?>" placeholder="Телефон" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="emailLanding" id="EMAIL_REG_CHECK_CANIN" name="email" value="<?=$arUser['EMAIL']?:''?>" placeholder="E-mail" >
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+
+                    <div class="read-rules">
+                        <input type="checkbox" id="READ_RULES_REG_CHECK_CANIN" name="rules" value="Y" cheform-group form-group_select js-wrap-select-form-registr-check-landingcked>
+                        <label for="READ_RULES_REG_CHECK_CANIN"><span></span> с <a href="#" target="_blank">правилами</a> акции ознакомлен</label>
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+
+                    <div class="registr-check-landing__btn-form">
+                        <button type="submit" class="landing-btn landing-btn_dark" >Отправить</button>
+                    </div>
+                </form>
+
+                <div class="registr-check-landing__response" data-response-form-landing="true"></div>
+            </div>
+
+        <?} else {?>
+
+            <div class="registr-check-landing__message">
+                <div class="landing-title landing-title_gray">
+                    Регистрируйте чеки<br/> и&nbsp;выигрывайте призы каждую неделю
+                </div>
+            </div>
+
+        <?}?>
+    </div>
+</section>
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'; ?>
---
+++ common/local/templates/4lapy_canin/components/bitrix/form.result.new/feedback/template.php
@@ -0,0 +1,105 @@
+<?php
+
+use FourPaws\App\Application as App;
+use FourPaws\App\Exceptions\ApplicationCreateException;
+use FourPaws\ReCaptchaBundle\Service\ReCaptchaService;
+
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+if (!\is_array($arResult['QUESTIONS']) || empty($arResult['QUESTIONS'])) {
+    return;
+}
+?>
+
+<form data-form-feedback-landing="true" class="form-landing feedback-landing__form js-form-validation"
+      name="<?= $arResult['arForm']['SID'] ?>" data-url="/ajax/form/add/feedback/" action="/ajax/form/add/feedback/"
+      method="post"
+      enctype="multipart/form-data">
+    <?= bitrix_sessid_post() ?>
+    <input name="WEB_FORM_ID" value="<?= $arResult['arForm']['ID'] ?>" type="hidden">
+
+    <?
+        foreach ($arResult['QUESTIONS'] as $fieldSid => $question) {
+            if ($question['STRUCTURE'][0]['FIELD_TYPE'] === 'hidden') {
+                echo $question['HTML_CODE'];
+            }
+        }
+    ?>
+
+    <?
+        foreach ($arResult['QUESTIONS'] as $fieldSid => $question) {
+            if ($question['STRUCTURE'][0]['FIELD_TYPE'] === 'dropdown') {
+                $fieldName = 'form_' . $question['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $fieldSid;
+
+                foreach ($arResult['arAnswers'][$fieldSid] as $item) {
+                    if ($item['MESSAGE'] == 'Другое') {
+                        $value = $item['ID'];
+                    }
+                }
+
+                ?>
+                <input type="hidden" name="<?=$fieldName?>" value="<?=$value?>" />
+                <?
+            }
+        }
+    ?>
+
+    <?
+        foreach ($arResult['QUESTIONS'] as $fieldSid => $question) {
+            if ($question['STRUCTURE'][0]['FIELD_TYPE'] === 'textarea') {
+                $fieldName = 'form_' . $question['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $question['STRUCTURE'][0]['ID'];
+                ?>
+                    <div class="form-group form-group_full">
+                        <textarea id="<?= $fieldName ?>" name="<?= $fieldName ?>" placeholder="напишите ваш вопрос"></textarea>
+                        <div class="b-error">
+                            <span class="js-message"></span>
+                        </div>
+                    </div>
+                <?
+            }
+        }
+    ?>
+
+    <?
+    foreach ($arResult['QUESTIONS'] as $fieldSid => $question) {
+        if (in_array($question['STRUCTURE'][0]['FIELD_TYPE'], ['text', 'email'])) {
+            $fieldName = 'form_' . $question['STRUCTURE'][0]['FIELD_TYPE'] . '_' . $question['STRUCTURE'][0]['ID'];
+            $type = 'text';
+            if ($fieldSid === 'email') {
+                $type = 'email';
+            } elseif ($fieldSid === 'phone') {
+                $type = 'tel';
+            } ?>
+            ?>
+            <div class="form-group">
+                <input type="<?= $type ?>" id="FIO_REG_FEEDBACK_GRANDIN" name="<?= $fieldName ?>" value="<?= $arResult['CUR_USER'][$fieldSid] ?>" placeholder="<?=$question['CAPTION']?>">
+                <div class="b-error">
+                    <span class="js-message"></span>
+                </div>
+            </div>
+            <?
+        }
+    }
+    ?>
+
+    <? if ($arResult['isUseCaptcha']) { ?>
+        <div class="form-group form-group_full">
+            <?php try {
+                echo App::getInstance()->getContainer()->get(ReCaptchaService::class)->getCaptcha();
+            } catch (ApplicationCreateException $e) {
+                /** ошибка - капчу не вывести */
+            } ?>
+        </div>
+    <?}?>
+
+    <div class="feedback-landing__form-info">
+        Обратите внимание, что все поля данной формы должны быть заполнены
+    </div>
+
+    <div class="feedback-landing__btn-form">
+        <button type="submit" class="btn-canin">Отправить</button>
+    </div>
+</form>
