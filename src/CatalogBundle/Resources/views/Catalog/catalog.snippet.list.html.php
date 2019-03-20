99e6849acb060c6cc69a5c4c9eb904ac7c7f8134
mmasterkov <m.masterkov@articul.ru>
2019-03-20 13:57:54 +0300

    Перенос статей в каталоге из футера к товарам

--- src/CatalogBundle/Resources/views/Catalog/catalog.snippet.list.html.php
+++ src/CatalogBundle/Resources/views/Catalog/catalog.snippet.list.html.php
@@ -17,6 +17,28 @@ global $APPLICATION;
 
 $category = $catalogRequest->getCategory();
 
+if ($catalogRequest->getCategory()->isLanding()) {
+    $getLastRowIndex = function ($rowCount) use ($collection) {
+        $lastRowIndex = ($rowCount * (intval(count($collection) / $rowCount)));
+        if (count($collection) % $rowCount == 0) {
+            $lastRowIndex -= $rowCount;
+        }
+        return $lastRowIndex;
+    };
+
+    $lastRowIndex = $getLastRowIndex(4);
+    $lastRowIndexTablet = $getLastRowIndex(3);
+
+    ob_start();
+    echo $view->render('FourPawsCatalogBundle:Catalog:old.landing.articles.php', \compact('category'));
+    $articlesHtml = ob_get_contents();
+    ob_end_clean();
+
+    $pageNum = $collection->getCdbResult()->NavPageNomer;
+    $isFiltered = $category->getFilters()->hasCheckedFilter();
+}
+
+
 foreach ($collection as $product) {
     $i++;
 
@@ -33,35 +55,52 @@ foreach ($collection as $product) {
     );
 
     if ($catalogRequest->getCategory()->isLanding()) {
+
         /**
          * Баннеры между рядами товаров
          */
-        if (!empty($catalogRequest->getCategory()->getUfLandingBanner())) {
-            if ($i === 3 || ($i === $countItems && $i < 3)) { ?>
-                <div class="b-fleas-protection-banner b-tablet">
-                    <?= htmlspecialcharsback($category->getUfLandingBanner()) ?>
-                </div>
-            <?php }
+        if(!$isFiltered){
+            if (!empty($catalogRequest->getCategory()->getUfLandingBanner())) {
+                if ($i === 3 || ($i === $countItems && $i < 3)) { ?>
+                    <div class="b-fleas-protection-banner b-tablet">
+                        <?= htmlspecialcharsback($category->getUfLandingBanner()) ?>
+                    </div>
+                <? }
 
-            if ($i === 4 || ($i === $countItems && $i < 4)) { ?>
-                <div class="b-fleas-protection-banner">
-                    <?= htmlspecialcharsback($category->getUfLandingBanner()) ?>
-                </div>
-            <?php }
+                if ($i === 4 || ($i === $countItems && $i < 4)) { ?>
+                    <div class="b-fleas-protection-banner">
+                        <?= htmlspecialcharsback($category->getUfLandingBanner()) ?>
+                    </div>
+                <? }
+            }
+
+            if (!empty($catalogRequest->getCategory()->getUfLandingBanner2())) {
+                if ($i === 9 || ($i === $countItems && $i < 9)) { ?>
+                    <div class="b-fleas-protection-banner b-tablet">
+                        <?= htmlspecialcharsback($category->getUfLandingBanner2()) ?>
+                    </div>
+                <? }
+
+                if ($i === 12 || ($i === $countItems && $i < 12)) { ?>
+                    <div class="b-fleas-protection-banner">
+                        <?= htmlspecialcharsback($category->getUfLandingBanner2()) ?>
+                    </div>
+                <? }
+            }
         }
 
-        if (!empty($catalogRequest->getCategory()->getUfLandingBanner2())) {
-            if ($i === 9 || ($i === $countItems && $i < 9)) { ?>
-                <div class="b-fleas-protection-banner b-tablet">
-                    <?= htmlspecialcharsback($category->getUfLandingBanner2()) ?>
-                </div>
-            <?php }
-            if ($i === 12 || ($i === $countItems && $i < 12)) { ?>
-                <div class="b-fleas-protection-banner">
-                    <?= htmlspecialcharsback($category->getUfLandingBanner2()) ?>
-                </div>
-            <?php }
+        /**
+         * Анонсы статей
+         */
+        if($pageNum < 2){
+            // @todo: Здесь нужен класс для моб. разрешения
+            /*if ($i === $lastRowIndexTablet || ($i === $countItems && $i < $lastRowIndexTablet)) {
+                echo '<div class="b-tablet">',$articlesHtml,'</div>';
+            }*/
+            if ($i === $lastRowIndex || ($i === $countItems && $i < $lastRowIndex)) {
+                echo $articlesHtml;
+            }
         }
+
     }
-
 }
