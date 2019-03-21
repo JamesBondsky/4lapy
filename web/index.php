60738a1b8bf86915f020dfc0d8be169659636597
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-03-21 19:18:27 +0300

    TM-32157 Настраиваемый из админки слайдер на главной+миграция

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
 <?php
 
 use Adv\Bitrixtools\Tools\Iblock\IblockUtils;
+use FourPaws\App\Application as App;
 use FourPaws\Enum\IblockCode;
 use FourPaws\Enum\IblockType;
+use FourPaws\UserBundle\Service\UserCitySelectInterface;
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
@@ -118,92 +120,78 @@ $APPLICATION->IncludeComponent(
 );
 ?>
 
-<section class="b-promo-banner">
-    <div class="b-container">
-        <div class="b-promo-banner__list js-promo-banner">
-            <?/*div class="b-promo-banner-item">
-                <div class="b-promo-banner-item__content">
-                    <div class="b-promo-banner-item__left">
-                        <div class="b-promo-banner-item__logo"></div>
-                        <div class="b-promo-banner-item__img">
-                            <img src="/static/build/images/inhtml/promo-banner_express.png" alt=""/>
-                        </div>
-                    </div>
-                    <div class="b-promo-banner-item__descr">
-                        Экспресс доставка по&nbsp;Москве! Привезем заказ через 3 часа!
-                    </div>
-                    <div class="b-promo-banner-item__link-wrap">
-                        <a class="b-promo-banner-item__link" href="#">Подробнее</a>
-                    </div>
-                </div>
-            </div*/?>
-            <div class="b-promo-banner-item">
-                <div class="b-promo-banner-item__content">
-                    <div class="b-promo-banner-item__left">
-                        <div class="b-promo-banner-item__logo"></div>
-                        <div class="b-promo-banner-item__img">
-                            <img src="/static/build/images/inhtml/promo-banner_application.png" alt=""/>
-                        </div>
-                    </div>
-                    <div class="b-promo-banner-item__descr">
-                        Удобные мобильные<br/> приложения для&nbsp;Android и&nbsp;IOS
-                    </div>
-                    <div class="b-promo-banner-item__link-wrap">
-                        <a class="b-promo-banner-item__link" href="/shares/mobilnoe_prilozhenie_chetyre_lapy.html">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="b-promo-banner-item b-promo-banner-item--dark">
-                <div class="b-promo-banner-item__content">
-                    <div class="b-promo-banner-item__left">
-                        <div class="b-promo-banner-item__logo"></div>
-                        <div class="b-promo-banner-item__img">
-                            <img src="/static/build/images/inhtml/promo-banner_acarid.png" alt=""/>
-                        </div>
-                    </div>
-                    <div class="b-promo-banner-item__descr">
-                        Бесплатная обработка<br/> от&nbsp;клещей и&nbsp;блох в&nbsp;магазинах!
-                    </div>
-                    <div class="b-promo-banner-item__link-wrap">
-                        <a class="b-promo-banner-item__link" href="/catalog/veterinarnaya-apteka/zashchita-ot-blokh-i-kleshchey/">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="b-promo-banner-item b-promo-banner-item--big-text">
-                <div class="b-promo-banner-item__content">
-                    <div class="b-promo-banner-item__left">
-                        <div class="b-promo-banner-item__logo"></div>
-                        <div class="b-promo-banner-item__img">
-                            <img src="/static/build/images/inhtml/promo-banner_pickup.png" alt=""/>
-                        </div>
-                    </div>
-                    <div class="b-promo-banner-item__descr">
-                        Бесплатный самовывоз заказа через 30 минут из&nbsp;230+ магазинов!
-                    </div>
-                    <div class="b-promo-banner-item__link-wrap">
-                        <a class="b-promo-banner-item__link" href="/shops/">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="b-promo-banner-item b-promo-banner-item--big-text">
-                <div class="b-promo-banner-item__content">
-                    <div class="b-promo-banner-item__left">
-                        <div class="b-promo-banner-item__logo"></div>
-                        <div class="b-promo-banner-item__img">
-                            <img src="/static/build/images/inhtml/promo-banner_region.png" alt=""/>
-                        </div>
-                    </div>
-                    <div class="b-promo-banner-item__descr">
-                        Бесплатная доставка по&nbsp;Москве в&nbsp;день заказа!<br/> Доставим в&nbsp;любой <nobr>4-х</nobr> часовой интервал
-                    </div>
-                    <div class="b-promo-banner-item__link-wrap">
-                        <a class="b-promo-banner-item__link" href="/payment-and-delivery/">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?
+
+$selectedCityCode = null;
+try {
+	/** @var \FourPaws\UserBundle\Service\UserService $userService */
+	$userService = App::getInstance()
+		->getContainer()
+		->get(UserCitySelectInterface::class);
+	$selectedCity = $userService->getSelectedCity(); //FIXME убрать повторные запросы внутри метода, если он уже выполнялся ранее
+	$selectedCityCode = $selectedCity['CODE'];
+} catch (\Exception $e) {}
+
+$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"modified.slider",
+	[
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "N",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => ["", ""],
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => IblockUtils::getIblockId(IblockType::PUBLICATION, IblockCode::SLIDER_CONTROLLED),
+		"IBLOCK_TYPE" => IblockType::PUBLICATION,
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Баннеры",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => [
+		    'COLOR',
+            'BIG_TEXT',
+            'LINK',
+            'LOCATION',
+        ],
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "SORT",
+		"SORT_BY2" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "DESC",
+		"STRICT_SECTION_CHECK" => "N",
+		"SELECTED_CITY_CODE" => $selectedCityCode
+	]
+);
+?>
 
 <?php
 /**
