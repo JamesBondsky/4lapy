e2c08be1c1a36e795df67ab51059a36485ae13a7
Alexandr Dutov <a.dutov@articul.ru>
2019-03-25 11:16:21 +0000

    tm-31790 dostavista consumer fix MySQL server has gone away

--- src/External/Dostavista/Consumer/DostavistaOrdersAddConsumer.php
+++ src/External/Dostavista/Consumer/DostavistaOrdersAddConsumer.php
@@ -3,9 +3,11 @@
 namespace FourPaws\External\Dostavista\Consumer;
 
 use Adv\Bitrixtools\Tools\BitrixUtils;
+use FourPaws\External\Dostavista\Exception\DostavistaOrdersAddConsumerException;
 use FourPaws\UserBundle\EventController\Event;
 use PhpAmqpLib\Message\AMQPMessage;
 use Bitrix\Sale\Order;
+use FourPaws\FrontOffice\Traits\DbTransactionTrait;
 
 /**
  * Class DostavistaOrdersAddConsumer
@@ -14,6 +16,7 @@ use Bitrix\Sale\Order;
  */
 class DostavistaOrdersAddConsumer extends DostavistaConsumerBase
 {
+    use DbTransactionTrait;
 
     /**
      * @param AMQPMessage $message
@@ -24,7 +27,9 @@ class DostavistaOrdersAddConsumer extends DostavistaConsumerBase
     {
         Event::disableEvents();
 
-        $result = static::MSG_ACK;
+        $result = static::MSG_REJECT;
+
+        $this->getDbConnection();
 
         try {
             $data = json_decode($message->getBody(), true);
@@ -35,49 +40,46 @@ class DostavistaOrdersAddConsumer extends DostavistaConsumerBase
              * Получаем битриксовый заказ
              */
             if ($bitrixOrderId === null) {
-                //айди битриксового заказа нет в данных от рэббита
-                $result = static::MSG_REJECT;
-            } else {
-                $order = $this->orderService->getOrderById($bitrixOrderId);
-                if (!$order) {
-                    //битриксовый заказ не найден
-                    $result = static::MSG_REJECT;
-                } else {
-                    /** Отправляем заказ в достависту */
-                    $response = $this->dostavistaService->addOrder($data);
-                    if ($response['connection'] === false) {
-                        //Достависта недоступна
-                        $result = static::MSG_REJECT;
-                    } else {
-                        $dostavistaOrderId = $response['order_id'];
-                        if ((is_array($dostavistaOrderId) || empty($dostavistaOrderId)) || !$response['success']) {
-                            //ошибка отправки сообщения в достависту
-                            $dostavistaOrderId = 0;
-                            $result = static::MSG_REJECT;
-                        }
-                    }
-                    /** Обновляем битриксовые свойства достависты */
-                    $deliveryId = $order->getField('DELIVERY_ID');
-                    $deliveryCode = $this->deliveryService->getDeliveryCodeById($deliveryId);
-                    $address = $this->orderService->compileOrderAddress($order)->setValid(true);
-                    $this->orderService->setOrderPropertiesByCode(
-                        $order,
-                        [
-                            'IS_EXPORTED_TO_DOSTAVISTA' => ($dostavistaOrderId) ? BitrixUtils::BX_BOOL_TRUE : BitrixUtils::BX_BOOL_FALSE,
-                            'ORDER_ID_DOSTAVISTA' => ($dostavistaOrderId) ? $dostavistaOrderId : 0
-                        ]
-                    );
-                    $this->orderService->updateCommWayPropertyEx($order, $deliveryCode, $address, ($dostavistaOrderId) ? true : false);
-                    $order->save();
-                }
+                throw new DostavistaOrdersAddConsumerException('Dostavista: bitrix order id empty in message data!', 10);
             }
-        } catch (\Exception $e) {
-            $result = static::MSG_REJECT;
-        }
 
-        if ($result === static::MSG_REJECT) {
-            if ($order) {
+            $order = $this->orderService->getOrderById($bitrixOrderId);
+            if (!$order) {
+                throw new DostavistaOrdersAddConsumerException('Dostavista: bitrix order not found!', 20);
+            }
+
+            /** Отправляем заказ в достависту */
+            $response = $this->dostavistaService->addOrder($data);
+
+            if ($response['connection'] === false) {
+                throw new DostavistaOrdersAddConsumerException('Dostavista: connection with service dostavista error!', 30);
+            }
+            $dostavistaOrderId = $response['order_id'];
+            if ((is_array($dostavistaOrderId) || empty($dostavistaOrderId)) || !$response['success']) {
+                throw new DostavistaOrdersAddConsumerException('Dostavista: dostavista service add order failed!', 40);
+            }
+            /** Обновляем битриксовые свойства достависты */
+            $deliveryId = $order->getField('DELIVERY_ID');
+            $deliveryCode = $this->deliveryService->getDeliveryCodeById($deliveryId);
+            $address = $this->orderService->compileOrderAddress($order)->setValid(true);
+            $this->startTransaction();
+            $this->orderService->setOrderPropertiesByCode(
+                $order,
+                [
+                    'IS_EXPORTED_TO_DOSTAVISTA' => ($dostavistaOrderId) ? BitrixUtils::BX_BOOL_TRUE : BitrixUtils::BX_BOOL_FALSE,
+                    'ORDER_ID_DOSTAVISTA' => ($dostavistaOrderId) ? $dostavistaOrderId : 0
+                ]
+            );
+            $this->orderService->updateCommWayPropertyEx($order, $deliveryCode, $address, ($dostavistaOrderId) ? true : false);
+            $order->save();
+            $this->commitTransaction();
+            $result = static::MSG_ACK;
+        } catch (\DostavistaOrdersAddConsumerException|\Exception $e) {
+            $this->log()->error('Dostavista error, code: ' . $e->getCode() . ' message: ' . $e->getMessage());
+            if ($order && is_array($response)) {
                 $this->dostavistaService->dostavistaOrderAddErrorSendEmail($order->getId(), $order->getField('ACCOUNT_NUMBER'), $response['message'], $response['data'], (new \Datetime)->format('d.m.Y H:i:s'));
+            } else {
+                $this->dostavistaService->dostavistaOrderAddErrorSendEmail(0, 0, $e->getMessage(), $e->getCode(), (new \Datetime)->format('d.m.Y H:i:s'));
             }
         }
 
