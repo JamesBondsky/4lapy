a6eb3363644fe9a31a531a60ba00f32e904a4fad
Dmitry Savchenkov <ds@notagency.ru>
2019-03-29 18:20:01 +0300

    migration to remove foreign key from api_user_session table

--- common/local/php_interface/migrations/ApiSessionRemoveForeignKey20190329175331.php
+++ common/local/php_interface/migrations/ApiSessionRemoveForeignKey20190329175331.php
@@ -13,8 +13,14 @@ class ApiSessionRemoveForeignKey20190329175331 extends \Adv\Bitrixtools\Migratio
      * @throws \Bitrix\Main\Db\SqlQueryException
      */
     public function up(){
-        Application::getConnection()
-            ->query('ALTER TABLE api_user_session DROP FOREIGN KEY api_user_session_ibfk_1');
+        $data = Application::getConnection()
+            ->query('SELECT CONSTRAINT_NAME FROM `information_schema`.`referential_constraints` WHERE TABLE_NAME = "api_user_session"')
+            ->fetch();
+
+        if ($data) {
+            Application::getConnection()
+                ->query('ALTER TABLE api_user_session DROP FOREIGN KEY ' . $data['CONSTRAINT_NAME']);
+        }
     }
 
     public function down(){
