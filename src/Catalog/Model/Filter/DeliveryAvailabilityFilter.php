3dcc659c0fa5409ff5f556228487429521bb6dcf
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-04-04 07:01:13 +0300

    TM-32153 Порядок пунктов в фильтре "Доступность"

--- src/Catalog/Model/Filter/DeliveryAvailabilityFilter.php
+++ src/Catalog/Model/Filter/DeliveryAvailabilityFilter.php
@@ -54,15 +54,15 @@ class DeliveryAvailabilityFilter extends FilterBase
             ->withAvailable(true)
             ->withValue($code . '_' . Product::AVAILABILITY_PICKUP));
         $result->add((new Variant())
-            ->withName('Под заказ')
-            ->withAvailable(true)
-            ->withValue($code . '_' . Product::AVAILABILITY_BY_REQUEST));
-        $result->add((new Variant())
             ->withName('Наличие в выбранном магазине')
             ->withAvailable(true)
             ->withAdditionalLinkClass('b-filter-link-list__link--shop js-open-catalog-shop-popup')
             ->withHiddenFieldName(StoreAvailabilityFilter::$filterCode)
             ->withValue(Product::AVAILABILITY_PICKUP_FROM_SELECTED_STORES));
+        $result->add((new Variant())
+            ->withName('Под заказ')
+            ->withAvailable(true)
+            ->withValue($code . '_' . Product::AVAILABILITY_BY_REQUEST));
         return $result;
     }
 }
