9d298dd6575b2c200bcd8e467ef0a6ebfd3c4610
Андрей Воробьев <a.vorobyev@articul.ru>
2019-04-04 16:26:07 +0000

    Revert "TM-32153 Скрыты способы оплаты в фильтре по магазину (pull request #447)"
    This reverts pull request #447.
    
    > TM-32153 Скрыты способы оплаты в фильтре по магазину

--- common/local/components/fourpaws/catalog.shop.list/templates/popup/include/shop.php
+++ common/local/components/fourpaws/catalog.shop.list/templates/popup/include/shop.php
@@ -53,7 +53,6 @@ use FourPaws\Decorators\SvgDecorator;
                         {{schedule}}
                     </div>
                 </div>
-                <?/*?>
                 <div class="b-input-line b-input-line--myself">
                     <div class="b-input-line__label-wrapper">
                         <span class="b-input-line__label">Оплата в магазине</span>
@@ -73,7 +72,6 @@ use FourPaws\Decorators\SvgDecorator;
                         </span>
                     </div>
                 </div>
-                <?*/?>
                 <div class="b-input-line b-input-line--pin">
                     <a class="b-link b-link--pin js-shop-link"
                        href="javascript:void(0);"
