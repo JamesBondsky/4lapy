7c2075f96477d4094bb3a5190f2bbf906a690b62
Dmitry Savchenkov <ds@notagency.ru>
2019-04-08 11:13:46 +0300

    fix migration

--- common/local/php_interface/migrations/MobileApiCreatePushEventTable20181217015731.php
+++ common/local/php_interface/migrations/MobileApiCreatePushEventTable20181217015731.php
@@ -17,7 +17,7 @@ class MobileApiCreatePushEventTable20181217015731 extends \Adv\Bitrixtools\Migra
          * Compile from d7 DataManager will return only not null table fields structure
          */
         $tableStructure = <<<SQL
-CREATE TABLE IF NOT EXISTS TABLE `$tableName`(
+CREATE TABLE `$tableName`(
   `ID`                       INT          NOT NULL AUTO_INCREMENT,
   `PLATFORM`                 CHAR(1)      NOT NULL,
   `PUSH_TOKEN`               VARCHAR(255) NOT NULL DEFAULT 0,
@@ -31,20 +31,15 @@ CREATE TABLE IF NOT EXISTS TABLE `$tableName`(
   PRIMARY KEY (`ID`)
 )
 SQL;
-
-        Application::getConnection()->startTransaction();
-        if (Application::getConnection()->isTableExists($tableName)) {
-            throw new \RuntimeException(sprintf(
-                'Table %s already exists',
-                $tableName
-            ));
-        }
-        try {
-            Application::getConnection()->query($tableStructure);
-            Application::getConnection()->commitTransaction();
-        } catch (\Exception $exception) {
-            Application::getConnection()->rollbackTransaction();
-            throw  new \RuntimeException($exception->getMessage(), $exception->getCode(), $exception);
+        if (!Application::getConnection()->isTableExists($tableName)) {
+            Application::getConnection()->startTransaction();
+            try {
+                Application::getConnection()->query($tableStructure);
+                Application::getConnection()->commitTransaction();
+            } catch (\Exception $exception) {
+                Application::getConnection()->rollbackTransaction();
+                throw  new \RuntimeException($exception->getMessage(), $exception->getCode(), $exception);
+            }
         }
     }
 
