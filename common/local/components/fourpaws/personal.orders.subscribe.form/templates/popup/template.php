a94c5ce3b053cf10b9115bc974c26f67b6a9470c
mmasterkov <m.masterkov@articul.ru>
2019-04-10 16:48:05 +0300

    Fix: контролы

--- common/local/components/fourpaws/personal.orders.subscribe.form/templates/popup/template.php
+++ common/local/components/fourpaws/personal.orders.subscribe.form/templates/popup/template.php
@@ -14,13 +14,16 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
  * @var string $componentPath
  */
 
+// контролы
+if($arResult['CURRENT_STAGE'] == 'initial'){
+    include __DIR__.'/stage.initial.php';
+    return;
+}
+
+// форма
 include __DIR__. '/header.php';
 
 switch ($arResult['CURRENT_STAGE']) {
-    case 'initial':
-        // страница с контролами
-        include __DIR__.'/stage.initial.php';
-        break;
     case 'step1':
         // страница с описанием
         include __DIR__.'/stage.step1.php';
