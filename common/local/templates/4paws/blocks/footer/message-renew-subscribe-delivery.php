c405086d29cf775f4e20626abef38fdef9fe83fb
Daria <d.gritsaenko@articul.ru>
2019-04-12 11:27:10 +0300

    tm-32503 заглушка для возобновления подписки

--- common/local/templates/4paws/blocks/footer/message-renew-subscribe-delivery.php
+++ common/local/templates/4paws/blocks/footer/message-renew-subscribe-delivery.php
@@ -1,6 +1,9 @@
 <section class="b-message-subscribe-delivery js-popup-section" data-popup="message-renew-subscribe-delivery">
-    <a class="b-message-subscribe-delivery__close js-close-popup" href="javascript:void(0);" title="Закрыть"></a>
+    <div class="b-message-subscribe-delivery__header">
+        <div class="b-message-subscribe-delivery__header-title">Возобновление подписки</div>
+        <a class="b-message-subscribe-delivery__close js-close-popup" href="javascript:void(0);" title="Закрыть"></a>
+    </div>
     <div class="b-message-subscribe-delivery__content">
-        <h1 class="b-message-subscribe-delivery__title">Ваша подписка на&nbsp;доставку успешно возобновлена</h1>
+        <h1 class="b-message-subscribe-delivery__title">Ваша подписка на доставку успешно возобновлена</h1>
     </div>
 </section>
