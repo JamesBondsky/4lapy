158fc6cfacd5ac5d95f5a825dc0e8ee67e5ebeaf
mmasterkov <m.masterkov@articul.ru>
2019-04-15 11:48:55 +0300

    Шаблон товара из подписки + фикс ключа

--- common/local/components/fourpaws/personal.orders.subscribe.form/templates/popup/stage.step1.php
+++ common/local/components/fourpaws/personal.orders.subscribe.form/templates/popup/stage.step1.php
@@ -44,6 +44,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
         <div class="b-product-subscribe-delivery__list js-list-product-subscribe-delivery">
 
             <? foreach ($arResult['BASKET'] as $id => $basketItem) {
+                $itemId = $id+1;
                 include __DIR__ . '/include/basketItem.php';
             } ?>
 
