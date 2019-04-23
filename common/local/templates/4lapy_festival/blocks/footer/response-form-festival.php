8e4d70326ddc34547fad6f8f648e3ed0c5c0c3db
Daria <d.gritsaenko@articul.ru>
2019-04-23 18:12:24 +0300

    tm-32855 попап спасибо

---
+++ common/local/templates/4lapy_festival/blocks/footer/response-form-festival.php
@@ -0,0 +1,8 @@
+<section class="popwrap2 js-popup-section" data-popup="response-form-festival">
+    <div class="formwrap">
+        <div class="formitem">
+            <p><b>ПОЗДРАВЛЯЕМ</b></p>
+            <p>Ты – участник Квеста «Хочу в Париж» и почетный гость Фестиваля. На почте уже ждет приглашение с персональным кодом участника и праздничная скидка.</p>
+        </div>
+    </div>
+</section>
