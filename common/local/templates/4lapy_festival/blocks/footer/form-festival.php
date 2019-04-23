2978072945da3a2367b4924757f8010a2de39792
Daria <d.gritsaenko@articul.ru>
2019-04-23 14:38:08 +0300

    tm-32855 фикс попапа фестиваля

--- common/local/templates/4lapy_festival/blocks/footer/form-festival.php
+++ common/local/templates/4lapy_festival/blocks/footer/form-festival.php
@@ -1,26 +1,28 @@
-<div class="popwrap js-popup-section" data-popup="form-festival">
+<section class="js-popup-section" data-popup="form-festival">
     <div class="js-close-popup"></div>
-    <div class="formwrap">
-        <div class="formitem">
-            <p><b>До фестиваля «Четыре лапы» осталось совсем чуть-чуть. Заполни короткую форму регистрации, и мы отправим на эелектронную почту приглашение с персональным кодом участника.</b></p>
-            <p>Это отличная возможность:</p>
-            <ul>
-                <li>Стать участником квеста и выиграть поездку в Париж!</li>
-                <li>Принять участие в розыгрыше более 100 призов</li>
-                <li>Воспользоваться праздничной скидкой 10% </li>
-                <li>Получить Паспорт участника на вип стойке регистрации</li>
-            </ul>
-        </div>
-        <div class="formitem">
-            <form>
-                <input type="text" placeholder="ИМЯ" />
-                <input type="text" placeholder="ФАМИЛИЯ" />
-                <input type="email" placeholder="EMAIL" />
-                <input type="phone" placeholder="ТЕЛЕФОН" />
+    <div class="popwrap">
+        <div class="formwrap">
+            <div class="formitem">
+                <p><b>До фестиваля «Четыре лапы» осталось совсем чуть-чуть. Заполни короткую форму регистрации, и мы отправим на эелектронную почту приглашение с персональным кодом участника.</b></p>
+                <p>Это отличная возможность:</p>
+                <ul>
+                    <li>Стать участником квеста и выиграть поездку в Париж!</li>
+                    <li>Принять участие в розыгрыше более 100 призов</li>
+                    <li>Воспользоваться праздничной скидкой 10% </li>
+                    <li>Получить Паспорт участника на вип стойке регистрации</li>
+                </ul>
+            </div>
+            <div class="formitem">
+                <form>
+                    <input type="text" placeholder="ИМЯ" />
+                    <input type="text" placeholder="ФАМИЛИЯ" />
+                    <input type="email" placeholder="EMAIL" />
+                    <input type="phone" placeholder="ТЕЛЕФОН" />
 
-                <input type="checkbox" id="agree" /> <label for="agree">Я принимаю условия пользовательского соглашения, правил, политики обработки персональных данных, даю согласие на обработку персональных данных.</label>
-                <button class="join_btn">я пойду!</button>
-            </form>
+                    <input type="checkbox" id="agree" /> <label for="agree">Я принимаю условия пользовательского соглашения, правил, политики обработки персональных данных, даю согласие на обработку персональных данных.</label>
+                    <button class="join_btn">я пойду!</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+</section>
