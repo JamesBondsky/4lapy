4fd138f431973bcacbfb149f1baf45ae3eb35449
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-04-24 12:51:08 +0300

    TM-32641 фестиваль: вывод результата регистрации

--- common/local/templates/4lapy_festival/blocks/footer/response-form-festival.php
+++ common/local/templates/4lapy_festival/blocks/footer/response-form-festival.php
@@ -1,7 +1,7 @@
 <section class="popwrap2 js-popup-section" data-popup="response-form-festival">
     <div class="btn-close btn-close--message-form js-close-popup"></div>
     <div class="formwrap">
-        <div class="formitem">
+        <div class="formitem" data-popup-content="true">
             <p><b>ПОЗДРАВЛЯЕМ</b></p>
             <p>Ты – участник Квеста «Хочу в Париж» и почетный гость Фестиваля. На почте уже ждет приглашение с персональным кодом участника и праздничная скидка.</p>
         </div>
