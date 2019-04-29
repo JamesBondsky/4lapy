d867ada29904e8f8af48f6cb33fd284c677e4665
adutov <a.dutov@articul.ru>
2019-04-29 15:45:15 +0300

    tm-32953 menu from catalog sections by codes with order by sort

--- web/catalog/.left.menu.php
+++ web/catalog/.left.menu.php
@@ -4,7 +4,22 @@ use FourPaws\Catalog\Model\Category;
 use FourPaws\Catalog\Query\CategoryQuery;
 
 $aMenuLinks = [];
-$sections   = (new CategoryQuery())->withFilterParameter('DEPTH_LEVEL', 1)->exec();
+$sections = (new CategoryQuery())
+    ->withFilter([
+        'CODE' => [
+            'koshki',
+            'sobaki',
+            'zashchita-ot-blokh-i-kleshchey',
+            'ryby',
+            'gryzuny-i-khorki',
+            'reptilii',
+            'ptitsy',
+            'veterinarnaya-apteka',
+        ]
+    ])
+    ->withOrder(['SORT' => 'ASC'])
+    ->exec();
+
 /** @var Category $section */
 foreach ($sections as $section) {
     $aMenuLinks[] = [
