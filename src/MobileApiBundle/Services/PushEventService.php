f29492ff14c2b71d3eed6c61f7139dae24c967d1
Dmitry Savchenkov <ds@notagency.ru>
2019-05-19 23:10:37 +0300

    fix push messages

--- src/MobileApiBundle/Services/PushEventService.php
+++ src/MobileApiBundle/Services/PushEventService.php
@@ -381,7 +381,9 @@ class PushEventService
                 '=USER_ID' => $user->getId(),
             ], ['ID' => 'DESC'], 1)[0];
 
-            if (!($userSession->getPlatform() && $userSession->getPushToken())) {
+            if (!$userSession) {
+                $this->log()->warning("PushEventService: у пользователя с номером телефона $personalPhone нет сессий в мобильном приложении");
+            } else if (!($userSession->getPlatform() && $userSession->getPushToken())) {
                 $this->log()->warning("PushEventService: у пользователя с номером телефона $personalPhone не установлено мобильное приложение");
             } else {
                 if ($this->shouldSendPushMessage($user, $typeCode)) {
