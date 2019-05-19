3cd5e968e19998c1b114714a30494b162e25bf8a
Dmitry Savchenkov <ds@notagency.ru>
2019-05-19 10:04:10 +0300

    fix catalog.product.delivery.info

--- common/local/components/fourpaws/catalog.product.delivery.info/templates/detail/include/delivery-info.php
+++ common/local/components/fourpaws/catalog.product.delivery.info/templates/detail/include/delivery-info.php
@@ -4,7 +4,6 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 }
 
 use FourPaws\Catalog\Model\Offer;
-use FourPaws\DeliveryBundle\Entity\CalculationResult\DeliveryResult;
 use FourPaws\DeliveryBundle\Helpers\DeliveryTimeHelper;
 
 /**
@@ -14,32 +13,21 @@ use FourPaws\DeliveryBundle\Helpers\DeliveryTimeHelper;
 /**
  * @var Offer $offer
  */
-/**
- * @var DeliveryResult $deliveryResult
- */
 $offer = $arParams['OFFER'];
-$deliveryResult = $delivery['RESULT'];
-$isByRequest = $offer->isByRequest();
-$text = $deliveryResult->getTextForOffer($offer->getPrice(), $isByRequest);
 ?>
 
 <li class="b-product-information__item">
-    <div class="b-product-information__title-info">Доставка
-    </div>
-    <div class="b-product-information__value b-product-information__value--link js-open-tab-link" data-tab="data">
-        <?= DeliveryTimeHelper::showByDate($delivery['DELIVERY_DATE'], 0, ['DATE_FORMAT' => 'XX']) ?>
-        <?php if ($offer->isByRequest()) { ?>
-            ближайшая
-        <?php } elseif ($delivery['FREE_FROM'] && $offer->getPrice() > $delivery['FREE_FROM']) { ?>
-            бесплатно
-        <?php } elseif ($delivery['FREE_FROM']) { ?>
-            бесплатно от <?= $delivery['FREE_FROM'] ?>
-            <span class="b-ruble b-ruble--value-information">₽</span>
-        <?php } ?>
-    <div class="b-product-information__value">
-        <?= $text ?>
-        <?php if (!$isByRequest && $delivery['FREE_FROM']) { ?>
-          <span class="b-ruble b-ruble--value-information"><?= $delivery['CURRENCY'] ?></span>
-        <?php } ?>
-    </div>
+  <div class="b-product-information__title-info">Доставка
+  </div>
+  <div class="b-product-information__value b-product-information__value--link js-open-tab-link" data-tab="data">
+      <?= DeliveryTimeHelper::showByDate($delivery['DELIVERY_DATE'], 0, ['DATE_FORMAT' => 'XX']) ?>
+      <?php if ($offer->isByRequest()) { ?>
+        ближайшая
+      <?php } elseif ($delivery['FREE_FROM'] && $offer->getPrice() > $delivery['FREE_FROM']) { ?>
+        бесплатно
+      <?php } elseif ($delivery['FREE_FROM']) { ?>
+        бесплатно от <?= $delivery['FREE_FROM'] ?>
+        <span class="b-ruble b-ruble--value-information">₽</span>
+      <?php } ?>
+  </div>
 </li>
--- common/local/components/fourpaws/catalog.product.delivery.info/templates/detail/include/pickup-info.php
+++ common/local/components/fourpaws/catalog.product.delivery.info/templates/detail/include/pickup-info.php
@@ -3,7 +3,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-use FourPaws\DeliveryBundle\Entity\CalculationResult\PickupResult;
+use Bitrix\Sale\Delivery\CalculationResult;
 use FourPaws\DeliveryBundle\Helpers\DeliveryTimeHelper;
 use FourPaws\DeliveryBundle\Service\DeliveryService;
 use FourPaws\Helpers\WordHelper;
@@ -12,78 +12,72 @@ use FourPaws\Helpers\WordHelper;
  * @var array $pickup
  */
 
-/**
- * @var PickupResult $pickupResult
- */
-$pickupResult = $pickup['RESULT'];
-
 ?>
 
 <li class="b-product-information__item">
-    <div class="b-product-information__title-info">Самовывоз</div>
-    <div class="b-product-information__value b-product-information__value--link disabled js-open-tab-link" data-tab="availability">
-        <?php if ($pickup['CODE'] === DeliveryService::INNER_PICKUP_CODE) {
-            $totalCount = $pickup['SHOP_COUNT']['TOTAL'];
-            $availableCount = $pickup['SHOP_COUNT']['AVAILABLE'];
-            $hasToday = $pickup['SHOP_COUNT']['HAS_TODAY'];
-            $unavailableCount = $pickup['SHOP_COUNT']['TOTAL'] - $pickup['SHOP_COUNT']['AVAILABLE'];
-            if ($availableCount) {
-                if ($hasToday) { ?>
-                    из <?= $availableCount . ' ' . WordHelper::declension(
-                        (int)$availableCount,
-                        [
-                            'магазина',
-                            'магазинов',
-                            'магазинов',
-                        ]
-                    ); ?>
-                    <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
-                        'DATE_FORMAT' => 'XX',
-                        'SHOW_TIME'   => $hasToday,
-                    ]);
-                } else { ?>
-                    <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
-                        'DATE_FORMAT' => 'XX',
-                        'SHOW_TIME'   => $hasToday,
-                    ]); ?> из <?= $availableCount . ' ' . WordHelper::declension(
-                        (int)$availableCount,
-                        [
-                            'магазина',
-                            'магазинов',
-                            'магазинов',
-                        ]
-                    );
-                }
-                if ($unavailableCount) { ?>
-                    <br>
-                    и из <?= $unavailableCount ?> <?= WordHelper::declension(
-                        (int)$unavailableCount,
-                        [
-                            'магазина',
-                            'магазинов',
-                            'магазинов',
-                        ]
-                    ); ?> позже
-                    <?php
-                }
-            } else { ?>
-                из <?= $totalCount . ' ' . WordHelper::declension(
-                    (int)$totalCount,
-                    [
-                        'магазина',
-                        'магазинов',
-                        'магазинов',
-                    ]
-                ); ?>
-                <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
-                    'DATE_FORMAT' => 'XX',
-                    'SHOW_TIME'   => false,
-                ]) ?>
-            <?php } ?>
-        <?php } else { ?>
-            <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, ['DATE_FORMAT' => 'XX']) ?>
-        <?php } ?>
-    <div class="b-product-information__value">
-        <?=$pickupResult->getTextForOffer()?>
-    </div>
+  <div class="b-product-information__title-info">Самовывоз</div>
+  <div class="b-product-information__value b-product-information__value--link disabled js-open-tab-link" data-tab="availability">
+      <?php if ($pickup['CODE'] === DeliveryService::INNER_PICKUP_CODE) { ?>
+          <?php
+          $totalCount = $pickup['SHOP_COUNT']['TOTAL'];
+          $availableCount = $pickup['SHOP_COUNT']['AVAILABLE'];
+          $hasToday = $pickup['SHOP_COUNT']['HAS_TODAY'];
+          $unavailableCount = $pickup['SHOP_COUNT']['TOTAL'] - $pickup['SHOP_COUNT']['AVAILABLE'];
+          if ($availableCount) {
+              if ($hasToday) { ?>
+                из <?= $availableCount . ' ' . WordHelper::declension(
+                      (int)$availableCount,
+                      [
+                          'магазина',
+                          'магазинов',
+                          'магазинов',
+                      ]
+                  ); ?>
+                  <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
+                      'DATE_FORMAT' => 'XX',
+                      'SHOW_TIME'   => $hasToday,
+                  ]);
+              } else { ?>
+                  <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
+                      'DATE_FORMAT' => 'XX',
+                      'SHOW_TIME'   => $hasToday,
+                  ]); ?> из <?= $availableCount . ' ' . WordHelper::declension(
+                      (int)$availableCount,
+                      [
+                          'магазина',
+                          'магазинов',
+                          'магазинов',
+                      ]
+                  );
+              }
+              if ($unavailableCount) { ?>
+                <br>
+                и из <?= $unavailableCount ?> <?= WordHelper::declension(
+                      (int)$unavailableCount,
+                      [
+                          'магазина',
+                          'магазинов',
+                          'магазинов',
+                      ]
+                  ); ?> позже
+                  <?php
+              }
+          } else { ?>
+            из <?= $totalCount . ' ' . WordHelper::declension(
+                  (int)$totalCount,
+                  [
+                      'магазина',
+                      'магазинов',
+                      'магазинов',
+                  ]
+              ); ?>
+              <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, [
+                  'DATE_FORMAT' => 'XX',
+                  'SHOW_TIME'   => false,
+              ]) ?>
+          <?php } ?>
+      <?php } else { ?>
+          <?= DeliveryTimeHelper::showByDate($pickup['DELIVERY_DATE'], 0, ['DATE_FORMAT' => 'XX']) ?>
+      <?php } ?>
+  </div>
 </li>
