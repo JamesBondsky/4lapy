ec5660b9edfa3ab27c9c0603410f11cacd35cd40
Daria <d.gritsaenko@articul.ru>
2019-05-27 14:56:37 +0300

    tm-33331 стрелки для алфавита брендов на десктопе

--- common/local/templates/.default/components/fourpaws/brands/fp.17.0/fourpaws/iblock.alphabetical.index/fp.17.0.default/template.php
+++ common/local/templates/.default/components/fourpaws/brands/fp.17.0/fourpaws/iblock.alphabetical.index/fp.17.0.default/template.php
@@ -1,4 +1,7 @@
-<?if (!defined('B_PROLOG_INCLUDED')||B_PROLOG_INCLUDED!==true) {
+<?
+use FourPaws\Decorators\SvgDecorator;
+
+if (!defined('B_PROLOG_INCLUDED')||B_PROLOG_INCLUDED!==true) {
     die();
 }
 /**
@@ -10,25 +13,34 @@
 $this->setFrameMode(true);
 
 ?><h2 class="b-title b-title--h1 b-title--catalog b-title--alphabet-brands"><?=\Bitrix\Main\Localization\Loc::getMessage('BRANDS_AI.TITLE')?></h2>
-<div class="b-link-list js-scroll-x">
-    <div class="b-link-list__wrapper"><?php
-        foreach ($arResult['PRINT'] as $arItem) {
-            $sTmpAddClass = '';
-            //if($arItem['SELECTED'] === 'Y') {
-            //    $sTmpAddClass .= ' active';
-            //}
-            if ($arItem['EXISTS'] === 'Y') {
-                $sTmpAddClass .= ' active';
+<div class="b-link-list__wrap b-link-list__wrap-scroll-arrows js-wrap-arrows-scroll-x">
+    <span class="b-icon b-icon--orange b-icon--brand-scroll-prev js-prev-brand-scroll">
+        <?= new SvgDecorator('icon-arrow-down', 10, 10) ?>
+    </span>
+    <span class="b-icon b-icon--orange b-icon--brand-scroll-next js-next-brand-scroll">
+        <?= new SvgDecorator('icon-arrow-down', 10, 10) ?>
+    </span>
+
+    <div class="b-link-list js-scroll-x js-arrows-scroll-x">
+        <div class="b-link-list__wrapper js-content-arrows-scroll-x"><?php
+            foreach ($arResult['PRINT'] as $arItem) {
+                $sTmpAddClass = '';
+                //if($arItem['SELECTED'] === 'Y') {
+                //    $sTmpAddClass .= ' active';
+                //}
+                if ($arItem['EXISTS'] === 'Y') {
+                    $sTmpAddClass .= ' active';
+                }
+                if ($arItem['EXISTS'] === 'Y') {
+                    ?><a class="b-link-list__link js-scroll-x<?=$sTmpAddClass?>"   href="javascript:void(0);" onclick="document.location.hash='<?=$arItem['ANCHOR']?>';" title="<?=$arItem['ANCHOR']?>"><?php
+                        echo $arItem['CAPTION'];
+                    ?></a><?php
+                } else {
+                    ?><span class="b-link-list__link js-scroll-x<?=$sTmpAddClass?>" title=""><?php
+                        echo $arItem['CAPTION'];
+                    ?></span><?php
+                }
             }
-            if ($arItem['EXISTS'] === 'Y') {
-                ?><a class="b-link-list__link js-scroll-x<?=$sTmpAddClass?>"   href="javascript:void(0);" onclick="document.location.hash='<?=$arItem['ANCHOR']?>';" title="<?=$arItem['ANCHOR']?>"><?php
-                    echo $arItem['CAPTION'];
-                ?></a><?php
-            } else {
-                ?><span class="b-link-list__link js-scroll-x<?=$sTmpAddClass?>" title=""><?php
-                    echo $arItem['CAPTION'];
-                ?></span><?php
-            }
-        }
-    ?></div>
+        ?></div>
+    </div>
 </div><?php
