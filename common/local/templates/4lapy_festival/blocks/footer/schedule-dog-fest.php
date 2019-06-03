ab2f15b9057c2bbfeaeb232cdeeaa44efb9645a3
Daria <d.gritsaenko@articul.ru>
2019-06-03 18:34:44 +0300

    tm-33562 расписание для dog-fest и лектория. Картинки для Hills на карте. Кликабельная ссылка в попапе регистрации с выводом информационного сообщения для лендинга фестиваля

---
+++ common/local/templates/4lapy_festival/blocks/footer/schedule-dog-fest.php
@@ -0,0 +1,33 @@
+<section class="popwrap popwrap_content js-popup-section" data-popup="schedule-dog-fest">
+    <div class="btn-close btn-close--form js-close-popup"></div>
+    <div class="popwrap__content popup-schedule">
+        <div class="popup-schedule__subtitle">Все активности ждут гостей <nobr>с 11:00 до 18:00</nobr></div>
+        <div class="popup-schedule__title popup-schedule__title--small">Участвуйте со&nbsp;своим любимцем под присмотром профессиональных кинологов!</div>
+        <ul class="popup-schedule__list">
+            <li>
+                <span class="time">11:00 -13:00</span>
+                <span class="event">Конкурс «Веселый подзыв»</span>
+            </li>
+            <li>
+                <span class="time">13:30-15:00</span>
+                <span class="event">Показательные выступления</span>
+            </li>
+            <li>
+                <span class="time">14:00-15:30</span>
+                <span class="event">Конкурс «Веселые старты»</span>
+            </li>
+            <li>
+                <span class="time">15:30-16:30</span>
+                <span class="event">Конкурс «Веселая апортировка»</span>
+            </li>
+            <li>
+                <span class="time">16:00-17:30</span>
+                <span class="event">Соревнование по «Аджилити»​</span>
+            </li>
+            <li>
+                <span class="time">16:30-17:30</span>
+                <span class="event">Конкурс «Узнай хозяина»</span>
+            </li>
+        </ul>
+    </div>
+</section>
