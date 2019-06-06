cb0024e0434f0b71b6e19983d60caeaaab2c4c56
Daria <d.gritsaenko@articul.ru>
2019-06-06 13:53:10 +0300

    tm-33641 Регистрация

--- common/local/templates/4paws/blocks/footer/start_kiosk.php
+++ common/local/templates/4paws/blocks/footer/start_kiosk.php
@@ -7,8 +7,8 @@
     </div>
     <div class="b-popup-start-kiosk__content">
         <div class="b-popup-start-kiosk__btns">
-            <a href="/catalog/" class="item">Начать покупки и перейти в каталог</a>
-            <a href="/?showScan=1&page=bonus" class="item item--orange">Посмотреть баллы отсканировав карту</a>
+            <a href="/catalog/" class="item">Отсканируй карту и&nbsp;посмотри персональные предложения и&nbsp;другие акции</a>
+            <a href="/?showScan=1&page=bonus" class="item item--orange">Или сразу переходи в&nbsp;каталог и&nbsp;выбирай<br/> уже полюбившиеся твоему питомцу товары</a>
         </div>
     </div>
 </section>
