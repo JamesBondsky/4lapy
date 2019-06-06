a788137b27177fab4aec9e52fb87f4a0b9d5fc67
Anton Ankilov <a.ankilov@articul.ru>
2019-06-06 12:40:12 +0000

    tm-33492 перенос методов

--- src/SocServ/CSocServVK2.php
+++ src/SocServ/CSocServVK2.php
@@ -2,6 +2,8 @@
 namespace FourPaws\SocServ;
 
 
+use CModule;
+
 class CSocServVK2 extends \CSocServVKontakte {
     const ID = "VK2";
 
@@ -87,5 +89,155 @@ class CSocServVK2 extends \CSocServVKontakte {
         }
 
         return $arFields;
+    }
+
+    public function getUrl($arParams)
+    {
+        global $APPLICATION;
+
+        $gAuth = $this->getEntityOAuth();
+
+        if (IsModuleInstalled('bitrix24') && defined('BX24_HOST_NAME'))
+        {
+            $redirect_uri = self::CONTROLLER_URL . "/redirect.php";
+            // error, but this code is not working at all
+            $state = \CHTTP::URN2URI("/bitrix/tools/oauth/liveid.php") . "?state=";
+            $backurl = urlencode($APPLICATION->GetCurPageParam('check_key=' . $_SESSION["UNIQUE_KEY"], array("logout", "auth_service_error", "auth_service_id", "backurl")));
+            $state .= urlencode(urlencode("backurl=" . $backurl));
+        }
+        else
+        {
+            //$redirect_uri = CSocServUtil::GetCurUrl('auth_service_id='.self::ID);
+            $redirect_uri = \CHTTP::URN2URI($APPLICATION->GetCurPage()) . '?auth_service_id=' . self::ID;
+
+            $backurl = $APPLICATION->GetCurPageParam(
+                'check_key=' . $_SESSION["UNIQUE_KEY"],
+                array("logout", "auth_service_error", "auth_service_id", "backurl")
+            );
+
+            $state = 'site_id=' . SITE_ID . '&backurl=' . urlencode($backurl) . (isset($arParams['BACKURL']) ? '&redirect_url=' . urlencode($arParams['BACKURL']) : '');
+        }
+
+        return $gAuth->GetAuthUrl($redirect_uri, $state);
+    }
+
+    public function Authorize()
+    {
+        $GLOBALS["APPLICATION"]->RestartBuffer();
+        $bSuccess = SOCSERV_AUTHORISATION_ERROR;
+
+        if ((isset($_REQUEST["code"]) && $_REQUEST["code"] <> '') && CSocServAuthManager::CheckUniqueKey())
+        {
+            if (IsModuleInstalled('bitrix24') && defined('BX24_HOST_NAME'))
+                $redirect_uri = self::CONTROLLER_URL . "/redirect.php";
+            else
+                $redirect_uri = \CHTTP::URN2URI($GLOBALS['APPLICATION']->GetCurPage()) . '?auth_service_id=' . self::ID;
+
+            $this->entityOAuth = $this->getEntityOAuth($_REQUEST['code']);
+            if ($this->entityOAuth->GetAccessToken($redirect_uri) !== false)
+            {
+                $arVkUser = $this->entityOAuth->GetCurrentUser();
+                if (is_array($arVkUser) && ($arVkUser['response']['0']['id'] <> ''))
+                {
+                    $arFields = $this->prepareUser($arVkUser);
+                    $bSuccess = $this->AuthorizeUser($arFields);
+                }
+            }
+        }
+
+        $url = ($GLOBALS["APPLICATION"]->GetCurDir() == "/login/") ? "" : $GLOBALS["APPLICATION"]->GetCurDir();
+        $aRemove = array("logout", "auth_service_error", "auth_service_id", "code", "error_reason", "error", "error_description", "check_key", "current_fieldset");
+
+
+        if (isset($_REQUEST['backurl']) || isset($_REQUEST['redirect_url']))
+        {
+            $parseUrl = parse_url(isset($_REQUEST['redirect_url']) ? $_REQUEST['redirect_url'] : $_REQUEST['backurl']);
+
+            $urlPath = $parseUrl["path"];
+            $arUrlQuery = explode('&', $parseUrl["query"]);
+
+            foreach ($arUrlQuery as $key => $value)
+            {
+                foreach ($aRemove as $param)
+                {
+                    if (strpos($value, $param . "=") === 0)
+                    {
+                        unset($arUrlQuery[$key]);
+                        break;
+                    }
+                }
+            }
+            $url = (!empty($arUrlQuery)) ? $urlPath . '?' . implode("&", $arUrlQuery) : $urlPath;
+        }
+
+        if ($bSuccess === SOCSERV_REGISTRATION_DENY)
+        {
+            $url = (preg_match("/\?/", $url)) ? $url . '&' : $url . '?';
+            $url .= 'auth_service_id=' . self::ID . '&auth_service_error=' . $bSuccess;
+        }
+        elseif ($bSuccess !== true)
+        {
+            $url = (isset($urlPath)) ? $urlPath . '?auth_service_id=' . self::ID . '&auth_service_error=' . $bSuccess : $GLOBALS['APPLICATION']->GetCurPageParam(('auth_service_id=' . self::ID . '&auth_service_error=' . $bSuccess), $aRemove);
+        }
+
+        if (CModule::IncludeModule("socialnetwork") && strpos($url, "current_fieldset=") === false)
+        {
+            $url = (preg_match("/\?/", $url)) ? $url . "&current_fieldset=SOCSERV" : $url . "?current_fieldset=SOCSERV";
+        }
+
+        echo '
+<script type="text/javascript">
+if(window.opener)
+{
+	window.opener.location = \'' . CUtil::JSEscape($url) . '\';
+}
+window.close();
+</script>
+';
+        die();
+    }
+
+    public function getFriendsList($limit, &$next)
+    {
+        if (IsModuleInstalled('bitrix24') && defined('BX24_HOST_NAME'))
+            $redirect_uri = self::CONTROLLER_URL . "/redirect.php";
+        else
+            $redirect_uri = \CHTTP::URN2URI($GLOBALS['APPLICATION']->GetCurPage()) . '?auth_service_id=' . self::ID;
+
+        $vk = $this->getEntityOAuth();
+        if ($vk->GetAccessToken($redirect_uri) !== false)
+        {
+            $res = $vk->getCurrentUserFriends($limit, $next);
+            if (is_array($res) && is_array($res['response']))
+            {
+                foreach ($res['response'] as $key => $contact)
+                {
+                    $res['response'][$key]['name'] = $contact["first_name"];
+                    $res['response'][$key]['url'] = "https://vk.com/id" . $contact["id"];
+                    $res['response'][$key]['picture'] = $contact['photo_200_orig'];
+                }
+
+                return $res['response'];
+            }
+        }
+
+        return false;
+    }
+
+    public function sendMessage($uid, $message)
+    {
+        $vk = $this->getEntityOAuth();
+
+        if (IsModuleInstalled('bitrix24') && defined('BX24_HOST_NAME'))
+            $redirect_uri = self::CONTROLLER_URL . "/redirect.php";
+        else
+            $redirect_uri = \CHTTP::URN2URI($GLOBALS['APPLICATION']->GetCurPage()) . '?auth_service_id=' . self::ID;
+
+        if ($vk->GetAccessToken($redirect_uri) !== false)
+        {
+            $res = $vk->sendMessage($uid, $message);
+        }
+
+        return $res;
     }
 }
