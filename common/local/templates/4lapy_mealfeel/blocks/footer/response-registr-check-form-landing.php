1ad92a66aec66d64e74b740d706d462058b16d22
Daria <d.gritsaenko@articul.ru>
2019-06-14 18:24:28 +0300

    tm-34022 картинки для Лендинга и забытые файлы

---
+++ common/local/templates/4lapy_mealfeel/blocks/footer/response-registr-check-form-landing.php
@@ -0,0 +1,19 @@
+<section class="popup-landing js-popup-section" data-popup="response-registr-check-landing">
+    <div class="popup-landing__container">
+        <div class="popup-landing__close js-close-popup"></div>
+        <div class="popup-landing__content">
+            <div class="popup-landing__ok"></div>
+            <div class="popup-registr-check-landing">
+                <div class="popup-registr-check-landing__title">
+                    Ваш чек зарегистрирован!
+                </div>
+                <div class="popup-registr-check-landing__descr">
+                    Проверяйте результаты розыгрыша каждую пятницу.<br />
+                    В&nbsp;розыгрыше 50 призов для правильного питания.<br />
+                    Главный приз&nbsp;&mdash; <nobr>SPA-weekend</nobr>, Роза Хутор Сочи. Разыгрывается 1 августа.
+                </div>
+                <div class="popup-registr-check-landing__primary">Удачи!</div>
+            </div>
+        </div>
+    </div>
+</section>
