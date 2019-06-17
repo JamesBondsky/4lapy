de71c9a80453089607c47bc7c4d011163edea169
mmasterkov <m.masterkov@articul.ru>
2019-06-17 12:12:00 +0300

    Миграция для почтового шаблона + изменённый запрос для получения записей без DATE_CHECK

--- src/PersonalBundle/Service/OrderSubscribeService.php
+++ src/PersonalBundle/Service/OrderSubscribeService.php
@@ -1596,9 +1596,15 @@ class OrderSubscribeService implements LoggerAwareInterface
         $params = [];
         $params['limit'] = $limit;
         $params['filter']['=UF_ACTIVE'] = 1;
-        $params['filter']['<=UF_DATE_CHECK'] = new DateTime();
+
         // TODO: временное решение, т.к. возникает непонятный баг с определнием зоны доставки DPD
         $params['filter']['!=UF_DEL_TYPE'] = [16, 17];
+
+        $params['filter'][] = [
+            'LOGIC' => 'OR',
+            ['<=UF_DATE_CHECK' => new DateTime()],
+            ['=UF_DATE_CHECK' => false],
+        ];
         $params['order'] = [
             'UF_NEXT_DEL' => 'ASC',
             'UF_DELIVERY_TIME' => 'ASC',
