44ee8cd5dae89f12f4327b601dfe88083be9a007
adutov <a.dutov@articul.ru>
2019-06-21 12:10:29 +0300

    base shop SHIPMENT_PLACE_CODE fix

--- src/SaleBundle/Service/OrderService.php
+++ src/SaleBundle/Service/OrderService.php
@@ -621,13 +621,22 @@ class OrderService implements LoggerAwareInterface
             (
                 ($this->deliveryService->isInnerDelivery($selectedDelivery) && $selectedDelivery->getSelectedStore()->isShop()) ||
                 $this->deliveryService->isInnerPickup($selectedDelivery)
-            ))
+            )) ||
+            mb_strpos($selectedDelivery->getDeliveryZone(), DeliveryService::ZONE_MOSCOW_DISTRICT_CODE_PATTERN) !== false
         ) {
+            /**
+             * Месье Костелье для районов Москвы (установка SHIPMENT_PLACE_CODE если есть базовый магазин для зоны)
+             */
             $shipmentResults = $selectedDelivery->getShipmentResults();
             $shipmentDays = [];
             /** @var BasketItem $item */
             foreach ($order->getBasket() as $item) {
-                $shipmentPlaceCode = self::STORE;
+                $selectedShop = $selectedDelivery->getSelectedStore();
+                if ($selectedShop instanceof Store) {
+                    $shipmentPlaceCode = $selectedShop->getXmlId();
+                } else {
+                    $shipmentPlaceCode = self::STORE;
+                }
                 /** @var DeliveryScheduleResult $deliveryResult */
                 if ($shipmentResults &&
                     ($deliveryResult = $shipmentResults->getByOfferId($item->getProductId()))
