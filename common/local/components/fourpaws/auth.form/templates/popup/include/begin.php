8880f042f416397f8e560693b396324fc5ff42da
mmasterkov <m.masterkov@articul.ru>
2019-06-26 18:00:30 +0300

    Revert "tm-31499 капча при регистрации 1 шаг и отправка смс, покупка в 1 клик, первый шаг авторизации"
    This reverts commit ebd4c631

--- common/local/components/fourpaws/auth.form/templates/popup/include/begin.php
+++ common/local/components/fourpaws/auth.form/templates/popup/include/begin.php
@@ -50,7 +50,6 @@ if ((isset($isAjax) && $isAjax) || $component->getMode() === FourPawsAuthFormCom
         </header>
         <form class="b-registration__form js-form-validation js-auth-2way"
               data-url="/ajax/user/auth/login-r/"
-              onsubmit="<?= $arResult['ON_SUBMIT'] ?>"
               method="post">
             <?= bitrix_sessid_post() ?>
             <input type="hidden" name="action" value="login" class="js-no-valid">
@@ -90,11 +89,16 @@ if ((isset($isAjax) && $isAjax) || $component->getMode() === FourPawsAuthFormCom
                     </div>
                 </div>
             </div>
-            <?
-            /** @var \FourPaws\ReCaptchaBundle\Service\ReCaptchaService $recaptchaService */
-            $recaptchaService = App::getInstance()->getContainer()->get(ReCaptchaInterface::class);
-            echo $recaptchaService->getCaptcha('', true, '', 'captchaAuthorize');
-            ?>
+            <?php
+            if ((int)$_SESSION['COUNT_AUTH_AUTHORIZE'] >= 3 && $arResult['IS_SHOW_CAPTCHA']) {
+                try {
+                    $recaptchaService = App::getInstance()
+                        ->getContainer()
+                        ->get(ReCaptchaInterface::class);
+                    echo $recaptchaService->getCaptcha('', true);
+                } catch (ApplicationCreateException $e) {
+                }
+            } ?>
             <div>
                 <span class="b-registration__auth-error">
                     <?= (int)$_SESSION['COUNT_AUTH_AUTHORIZE'] >= 3 ? 'Неверный логин или пароль' : '' ?>
