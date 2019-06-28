f01339a58a1978f85cda925ef881bd7706e8a765
Anton Ankilov <a.ankilov@articul.ru>
2019-06-28 13:29:15 +0000

    tm-34090 fix cache

--- src/SapBundle/Service/SapService.php
+++ src/SapBundle/Service/SapService.php
@@ -66,13 +66,17 @@ class SapService
         foreach ($this->pipelineRegistry->generator($pipelineCode) as $sourceMessage) {
             if ($this->consumerRegistry->consume($sourceMessage->getData())) {
                 $this->sourceRegistry->ack($sourceMessage);
-                $this->clearCacheProduct($sourceMessage->getData()->getItems());
+                if ($pipelineCode == 'catalog') {
+                    $this->clearCacheProduct($sourceMessage->getData()->getItems());
+                }
 
                 continue;
             }
 
             $this->sourceRegistry->noAck($sourceMessage);
-            $this->clearCacheProduct($sourceMessage->getData()->getItems());
+            if ($pipelineCode == 'catalog') {
+                $this->clearCacheProduct($sourceMessage->getData()->getItems());
+            }
         }
     }
 
