17aea3afa959b1fcaf3a61de378fd9ddba1e52e0
adutov <a.dutov@articul.ru>
2019-07-18 11:57:25 +0300

    shelter table and delivery result dobrolap class

---
+++ src/DeliveryBundle/Entity/CalculationResult/DobrolapDeliveryResult.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FourPaws\DeliveryBundle\Entity\CalculationResult;
+
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\SystemException;
+use FourPaws\App\Exceptions\ApplicationCreateException;
+use FourPaws\Catalog\Model\Offer;
+use FourPaws\DeliveryBundle\Entity\IntervalRule\TimeRuleInterface;
+use FourPaws\DeliveryBundle\Exception\NotFoundException;
+use FourPaws\DeliveryBundle\Helpers\DeliveryTimeHelper;
+use FourPaws\DeliveryBundle\Service\DeliveryService;
+use FourPaws\StoreBundle\Exception\NotFoundException as StoreNotFoundException;
+
+
+class DobrolapDeliveryResult extends BaseResult implements CalculationResultInterface
+{
+    /**
+     * @param bool $internalCall
+     * @return bool
+     * @throws ApplicationCreateException
+     * @throws ArgumentException
+     * @throws StoreNotFoundException
+     * @throws SystemException
+     */
+    public function isSuccess($internalCall = false)
+    {
+        return parent::isSuccess($internalCall);
+    }
+
+    /**
+     * @param Offer $offer
+     *
+     * @return bool
+     * @throws ApplicationCreateException
+     */
+    protected function checkIsDeliverable(Offer $offer): bool
+    {
+        return parent::checkIsDeliverable($offer) && $offer->getProduct()->isDeliveryAvailable();
+    }
+
+    protected function resetResult(): void
+    {
+        parent::resetResult();
+    }
+}
---
+++ src/SaleBundle/Repository/Table/AnimalShelterTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace FourPaws\SaleBundle\Repository\Table;
+
+use Bitrix\Main\Entity\DataManager;
+use Bitrix\Main\Entity\IntegerField;
+use Bitrix\Main\Entity\StringField;
+use Bitrix\Main\Entity\FloatField;
+
+class AnimalShelterTable extends DataManager
+{
+    /**
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return '4lapy_animal_shelters';
+    }
+
+    /**
+     * @return array
+     */
+    public static function getMap(): array
+    {
+        return [
+            new IntegerField('id', [
+                'primary'      => true,
+                'autocomplete' => true,
+            ]),
+            new StringField('name', [
+                'required' => true,
+            ]),
+            new StringField('description', [
+                'required' => true,
+            ]),
+            new StringField('address', [
+                'required' => true,
+            ]),
+            new StringField('city', [
+                'required' => true,
+            ]),
+            new FloatField('latitude', [
+                'required' => true,
+            ]),
+            new FloatField('longitude', [
+                'required' => true,
+            ])
+        ];
+    }
+}
