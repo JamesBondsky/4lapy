8929fed1831dc3ebe60038655c008ca11edf28dc
Daria <d.gritsaenko@articul.ru>
2019-07-19 17:00:56 +0300

    tm-35113 добролап попап в блок с попапами

---
+++ common/local/templates/4paws/blocks/footer/dobrolap-popup.php
@@ -0,0 +1,23 @@
+<section class="b-popup-pick-city b-popup-pick-city--authorization b-popup-pick-city--shelter js-popup-section" data-popup="shelter_popup">
+    <a class="b-popup-pick-city__close b-popup-pick-city__close--authorization js-close-popup" href="javascript:void(0);" title="Закрыть"></a>
+    <div class="shelter_wrap">
+        <div class="shelter_logo">
+            <img src="/dobrolap/images/shelter_logo/dino.png" alt="Дино">
+        </div>
+        <div class="shelter_tag">
+            <span>Благотворительный фонд</span>
+        </div>
+        <div class="shelter_about">
+            Дино
+        </div>
+
+        <div class="shelter_about_text">
+            Приют для животных города Волгограда, который помогает временно бездомным животных, существует с&nbsp;сентября 2011&nbsp;года. В&nbsp;приюте содержится более 300&nbsp;животных.
+        </div>
+        <div class="shelter_note">
+            <span>Волгоград</span>&nbsp;|&nbsp;
+            <span>300 питомцев</span>&nbsp;|&nbsp;
+            <span>8 лет</span>
+        </div>
+    </div>
+</section>
