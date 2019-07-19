258af84eb0181489c2c6a1dc0f9b6f3a8d343142
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-07-19 10:38:06 +0000

    TM-35111 API МП: Отключен апдейт юзера манзаны на каждом запросе

--- src/MobileApiBundle/Controller/BaseController.php
+++ src/MobileApiBundle/Controller/BaseController.php
@@ -15,28 +15,28 @@ use FourPaws\UserBundle\Service\UserService;
 
 class BaseController extends FOSRestController
 {
-    public function __destruct()
-    {
-        $currentDate = new DateTime();
-
-        $container = App::getInstance()->getContainer();
-
-        /** @var ManzanaService $manzanaService */
-        $manzanaService = $container->get('manzana.service');
-        $client = new Client();
-        try {
-            /** @var UserService $userCurrentUserService*/
-            $userCurrentUserService = App::getInstance()->getContainer()->get(CurrentUserProviderInterface::class);
-            $currentUser = $userCurrentUserService->getCurrentUser();
-
-            $client->phone = $currentUser->getPersonalPhone();
-            $client->haveMobileApp = true;
-            $client->lastDateUseMobileApp = $currentDate->format(\DateTime::ATOM);
-
-            if ($client instanceof Client) {
-                $manzanaService->updateContactAsync($client);
-            }
-        } catch(NotAuthorizedException $e){
-        }
-    }
+//    public function __destruct()
+//    {
+//        $currentDate = new DateTime();
+//
+//        $container = App::getInstance()->getContainer();
+//
+//        /** @var ManzanaService $manzanaService */
+//        $manzanaService = $container->get('manzana.service');
+//        $client = new Client();
+//        try {
+//            /** @var UserService $userCurrentUserService*/
+//            $userCurrentUserService = App::getInstance()->getContainer()->get(CurrentUserProviderInterface::class);
+//            $currentUser = $userCurrentUserService->getCurrentUser();
+//
+//            $client->phone = $currentUser->getPersonalPhone();
+//            $client->haveMobileApp = true;
+//            $client->lastDateUseMobileApp = $currentDate->format(\DateTime::ATOM);
+//
+//            if ($client instanceof Client) {
+//                $manzanaService->updateContactAsync($client);
+//            }
+//        } catch(NotAuthorizedException $e){
+//        }
+//    }
 }
