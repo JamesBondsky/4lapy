de086654a0677411513e3a665c08d5b3c5f0dd5a
Daria <d.gritsaenko@articul.ru>
2019-07-24 13:01:13 +0300

    tm-35064 правки по загрузке баннера мобильного приложения

--- common/local/components/articul/header.mobile.bunner/templates/.default/template.php
+++ common/local/components/articul/header.mobile.bunner/templates/.default/template.php
@@ -7,11 +7,13 @@ if ($arResult['SHOW_BANNER'] && (int)$sCancelMobileBannerCookie !== 1) { ?>
     <div class="b-mobile-app hidden js-banner-mobile-app">
         <div class="b-mobile-app__banner hidden js-banner-mobile-app-android">
             <a href="<?= $arResult['BANNER']['ANDROID_LINK'] ?>" class="b-mobile-app__link"
-               style="background-image: url(<?= $arResult['BANNER']['ANDROID_IMAGE'] ?>)" target="_blank"></a>
+               style="background-image: url(<?= $arResult['BANNER']['ANDROID_IMAGE'] ?>)" target="_blank"
+               data-img-banner-mobile-app="<?= $arResult['BANNER']['ANDROID_IMAGE'] ?>"></a>
         </div>
         <div class="b-mobile-app__banner hidden js-banner-mobile-app-ios">
             <a href="<?= $arResult['BANNER']['IOS_LINK'] ?>" class="b-mobile-app__link"
-               style="background-image: url(<?= $arResult['BANNER']['IOS_IMAGE'] ?>)" target="_blank"></a>
+               style="background-image: url(<?= $arResult['BANNER']['IOS_IMAGE'] ?>)" target="_blank"
+               data-img-banner-mobile-app="<?= $arResult['BANNER']['IOS_IMAGE'] ?>"></a>
         </div>
         <div class="b-mobile-app__close-wrap">
             <button type="button" class="b-mobile-app__close js-banner-mobile-app-close"></button>
