9b9b8905e5fffaa4f4115b4c2531577471253307
Daria <d.gritsaenko@articul.ru>
2019-08-06 18:10:59 +0300

    tm-35404 замена текста, плюс фикс попапа на 320 пикселей из блока "Добрые сюрпризы"

--- common/local/templates/4paws/blocks/footer/dobrolap-popup.php
+++ common/local/templates/4paws/blocks/footer/dobrolap-popup.php
@@ -1,23 +1,25 @@
 <section class="b-popup-pick-city b-popup-pick-city--authorization b-popup-pick-city--shelter js-popup-section" data-popup="shelter_popup">
 </section>
-<section class="b-popup-pick-city b-popup-pick-city--authorization b-popup-pick-city--shelter js-popup-section" data-popup="dobrolap_more_info_popup" style="max-width: 740px;padding: 35px 90px 35px 40px;">
+<section class="b-popup-pick-city b-popup-pick-city--authorization b-popup-pick-city--shelter popup-good-surprises-dobrolap js-popup-section" data-popup="dobrolap_more_info_popup">
     <a class="b-popup-pick-city__close b-popup-pick-city__close--authorization js-close-popup" href="javascript:void(0);" title="Закрыть"></a>
-    <h5>
+    <div class="popup-good-surprises-dobrolap__content">
         <p>Каждому участнику благотворительной инициативы «Добролап» мы говорим «спасибо» и дарим коллекционный магнит на память, а также «Добрый сюрприз» Добролап - купон с индивидуальным промокодом.</p>
-<p><strong>Что дает купон «Добрые сюрпризы» с индивидуальным промокодом?</strong></p>
-<p>Купон дает одну из привилегий:</p>
-<ul>
-<li>скидку до 30 % на аксессуары </li>
-<li>скидку до 20 % на лакомства </li>
-<li>купон на сумму 200 бонусов на покупку корма Grandin от 1000 рублей. </li>
-<li>возможность выиграть участие в фотосессии, футболку с художественным изображением питомца, стать автором рубрики/поста в социальных сетях «Четыре Лапы», стать ключевым имиджем сезонной рекламной кампании (питомец) «Твой питомец –звезда».</li>
-</ul>
-<p>Как воспользоваться купоном «Добрые сюрпризы»</p>
-<ul>
-<li>При покупке онлайн – примените промокод со скидкой при оформлении заказа. </li>
-<li>Воспользуйся скидкой, предъявив код на кассе в магазине.</li>
-<li>Зарегистрируй промокод фан-бонуса, чтобы принять участие в розыгрыше призов. </li>
-</ul>
-    </h5>
-    <p>Дополнительные условия уточняйте на сайте «Четыре Лапы» в разделе <a href="/shares/blagotvoritelnaya-aktsiya-dobrolap-dlya-zhivotnykh-ikh-priyutov2.html" target="_blank">«Акции»</a> и в магазинах сети.</p>
+        <p><strong>Что дает купон «Добрые сюрпризы» с индивидуальным промокодом?</strong></p>
+        <p>Купон дает одну из привилегий:</p>
+        <ul>
+            <li>скидку до 30 % на аксессуары </li>
+            <li>скидку до 20 % на лакомства </li>
+            <li>купон на сумму 200 бонусов на покупку корма Grandin от 1000 рублей. </li>
+            <li>возможность выиграть участие в фотосессии, футболку с художественным изображением питомца, стать автором рубрики/поста в социальных сетях «Четыре Лапы», стать ключевым имиджем сезонной рекламной кампании (питомец) «Твой питомец –звезда».</li>
+        </ul>
+        <p>Как воспользоваться купоном «Добрые сюрпризы»</p>
+        <ul>
+            <li>При покупке онлайн – примените промокод со скидкой при оформлении заказа. </li>
+            <li>Воспользуйся скидкой, предъявив код на кассе в магазине.</li>
+            <li>Зарегистрируй промокод фан-бонуса, чтобы принять участие в розыгрыше призов. </li>
+        </ul>
+    </div>
+    <div class="popup-good-surprises-dobrolap__primary">
+        Дополнительные условия уточняйте на сайте «Четыре Лапы» в разделе <a href="/shares/blagotvoritelnaya-aktsiya-dobrolap-dlya-zhivotnykh-ikh-priyutov2.html" target="_blank">«Акции»</a> и в магазинах сети.
+    </div>
 </section>
