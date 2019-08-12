91bb919808fa169f86ea9689a3a75c2e47c38cfe
Daria <d.gritsaenko@articul.ru>
2019-08-12 18:30:02 +0300

    tm-35674 правка по тексту. Добролап

--- web/dobrolap/index.php
+++ web/dobrolap/index.php
@@ -7,7 +7,7 @@ $APPLICATION->SetTitle("Добролап");
             <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse"
                     data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false"
                     aria-label="Toggle navigation">
-                <span class="oi oi-menu"></span> Навигация
+                <span class="oi oi-menu"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="ftco-nav">
