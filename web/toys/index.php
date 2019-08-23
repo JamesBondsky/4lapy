5dd9842e0561d9f7ff55b9b3626c93e7d73ef481
Gleb Tsyrulnikov <glebserferus@gmail.com>
2019-08-23 11:07:37 +0300

    tm-35820 верстка страницы лендинга

---
+++ web/toys/index.php
@@ -0,0 +1,372 @@
+<?php
+    require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+?>
+
+<style>
+
+</style>
+
+<div class="toys-landing">
+    <div class="toys-landing__header">
+        <img src="/upload/toys-landing/header.jpg" alt="" />
+    </div>
+
+    <div class="toys-landing__steps">
+        <div class="b-container toys-landing__steps-container">
+            <ul class="toys-landing__steps-list">
+                <li class="toys-landing__steps-item">
+                    <img src="/upload/toys-landing/step-1.svg" alt="" class="toys-landing__steps-image" />
+
+                    <div>
+                        <p class="toys-landing__steps-title">Копи марки</p>
+                        <p class="toys-landing__steps-description">1 марка за 1500 Р в чеке</p>
+                    </div>
+                </li>
+
+                <li class="toys-landing__steps-item">
+                    <img src="/upload/toys-landing/step-2.svg" alt="" class="toys-landing__steps-image" />
+
+                    <div>
+                        <p class="toys-landing__steps-title">Выбери игру</p>
+                        <p class="toys-landing__steps-description">из 4-х участвующих</p>
+                    </div>
+                </li>
+
+                <li class="toys-landing__steps-item">
+                    <img src="/upload/toys-landing/step-3.svg" alt="" class="toys-landing__steps-image" />
+
+                    <div>
+                        <p class="toys-landing__steps-title">Развивай</p>
+                        <p class="toys-landing__steps-description">своего питомца</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <section class="toys-landing__section">
+        <h2 class="toys-landing__section-title toys-landing__section-title--red">
+            <span class="toys-landing__section-title-text">Игры для кошек</span>
+        </h2>
+
+        <div class="b-container">
+            <div class="toys-landing-item toys-landing-item--1">
+                <div class="toys-landing-item__splash">
+                    <img src="/upload/toys-landing/1.png" alt="">
+
+                    <div class="toys-landing-item__splash-content">
+                        <h3 class="toys-landing-item__splash-title">Cолитёр</h3>
+                        <p class="toys-landing-item__splash-text">Подходит кошек любого возраста от 3х месяцев!</p>
+                    </div>
+                </div>
+
+                <div class="toys-landing-item__product toys-landing-product">
+                    <video class="toys-landing-product__video" poster="https://4lapy.ru/dobrolap/images/raise-preview2.jpg" controls>
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.mp4">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.ogv" type="video/webm">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.webm" type="video/ogg">
+                    </video>
+
+                    <div class="toys-landing-product__footer">
+                        <div class="toys-landing-product__footer-info">
+                            <p class="toys-landing-product__price">
+                                Обычная цена <strong><s>3 556</s> ₽</strong>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        7 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        1 587 ₽
+                    </span>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        12 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        1 287 ₽
+                    </span>
+                            </p>
+                        </div>
+
+                        <div class="toys-landing-product__footer-actions">
+                            <button class="toys-landing-product__a2c">
+                                В корзину
+                            </button>
+
+                            <a href="" class="b-link">Покупай за марки!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="toys-landing-item toys-landing-item--2">
+                <div class="toys-landing-item__product toys-landing-product">
+                    <video class="toys-landing-product__video" poster="https://4lapy.ru/dobrolap/images/raise-preview2.jpg" controls>
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.mp4">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.ogv" type="video/webm">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.webm" type="video/ogg">
+                    </video>
+
+                    <div class="toys-landing-product__footer">
+                        <div class="toys-landing-product__footer-info">
+                            <p class="toys-landing-product__price">
+                                Обычная цена <strong><s>3 556</s> ₽</strong>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        7 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        1 458 ₽
+                    </span>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        14 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        3 818 ₽
+                    </span>
+                            </p>
+                        </div>
+
+                        <div class="toys-landing-product__footer-actions">
+                            <button class="toys-landing-product__a2c">
+                                В корзину
+                            </button>
+
+                            <a href="" class="b-link">Покупай за марки!</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="toys-landing-item__splash">
+                    <img src="/upload/toys-landing/2.png" alt="">
+
+                    <div class="toys-landing-item__splash-content">
+                        <h3 class="toys-landing-item__splash-title">Кормушка-тоннель</h3>
+                        <p class="toys-landing-item__splash-text">Можно кормить как из миски
+                            каждый день!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="toys-landing__section wo-sep">
+        <h2 class="toys-landing__section-title toys-landing__section-title--blue">
+            <span class="toys-landing__section-title-text">
+                Игры для собак
+            </span>
+        </h2>
+
+        <div class="b-container">
+            <div class="toys-landing-item toys-landing-item--3">
+                <div class="toys-landing-item__splash">
+                    <img src="/upload/toys-landing/3.png" alt="">
+
+                    <div class="toys-landing-item__splash-content">
+                        <h3 class="toys-landing-item__splash-title">Flip&amp;Fun</h3>
+                        <p class="toys-landing-item__splash-text">Увлекательная игра
+                            на ловкость и смекалку.</p>
+                    </div>
+                </div>
+
+                <div class="toys-landing-item__product toys-landing-product">
+                    <video class="toys-landing-product__video" poster="https://4lapy.ru/dobrolap/images/raise-preview2.jpg" controls>
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.mp4">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.ogv" type="video/webm">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.webm" type="video/ogg">
+                    </video>
+
+                    <div class="toys-landing-product__footer">
+                        <div class="toys-landing-product__footer-info">
+                            <p class="toys-landing-product__price">
+                                Обычная цена <strong><s>3 556</s> ₽</strong>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        7 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        1 458 ₽
+                    </span>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                    <span class="toys-landing-product__price-highlighed">
+                        14 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                    </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                        3 818 ₽
+                    </span>
+                            </p>
+                        </div>
+
+                        <div class="toys-landing-product__footer-actions">
+                            <button class="toys-landing-product__a2c">
+                                В корзину
+                            </button>
+
+                            <a href="" class="b-link">Покупай за марки!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="toys-landing-item toys-landing-item--4">
+                <div class="toys-landing-item__product toys-landing-product">
+                    <video class="toys-landing-product__video" poster="https://4lapy.ru/dobrolap/images/raise-preview2.jpg" controls>
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.mp4">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.ogv" type="video/webm">
+                        <source src="https://4lapy.ru/upload/dobrolap/4lapi_priut_14_08_2.webm" type="video/ogg">
+                    </video>
+
+                    <div class="toys-landing-product__footer">
+                        <div class="toys-landing-product__footer-info">
+                            <p class="toys-landing-product__price">
+                                Обычная цена <strong><s>3 556</s> ₽</strong>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                                <span class="toys-landing-product__price-highlighed">
+                                    7 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                                </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                                    1 458 ₽
+                                </span>
+                            </p>
+
+                            <p class="toys-landing-product__price">
+                                <span class="toys-landing-product__price-highlighed">
+                                    14 <img src="/upload/toys-landing/logo.png" alt="" width="19" height="19">
+                                </span>
+
+                                +
+
+                                <span class="toys-landing-product__price-highlighed">
+                                    3 818 ₽
+                                </span>
+                            </p>
+                        </div>
+
+                        <div class="toys-landing-product__footer-actions">
+                            <button class="toys-landing-product__a2c">
+                                В корзину
+                            </button>
+
+                            <a href="" class="b-link">Покупай за марки!</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="toys-landing-item__splash">
+                    <img src="/upload/toys-landing/4.png" alt="">
+
+                    <div class="toys-landing-item__splash-content">
+                        <h3 class="toys-landing-item__splash-title">Poker Box</h3>
+                        <p class="toys-landing-item__splash-text">Мощный тренажер для
+                            развития интеллекта собаки</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="b-container">
+        <ul class="toys-landing-benefits">
+            <li class="toys-landing-benefits__item">
+                <img src="/upload/toys-landing/benefit-1.png" alt="" class="toys-landing-benefits__image" />
+
+                <div class="toys-landing-benefits__text">
+                    <strong>Развивают</strong><br />
+                    интеллект, внимание
+                    и&nbsp;ловкость
+                </div>
+            </li>
+            <li class="toys-landing-benefits__item">
+                <img src="/upload/toys-landing/benefit-2.png" alt="" class="toys-landing-benefits__image" />
+
+                <div class="toys-landing-benefits__text">
+                    <strong>Подходят</strong><br />
+                    для питомцев <br />
+                    от 3х месяцев
+                </div>
+            </li>
+
+            <li class="toys-landing-benefits__item">
+                <img src="/upload/toys-landing/benefit-3.png" alt="" class="toys-landing-benefits__image" />
+
+                <div class="toys-landing-benefits__text">
+                    <strong>Продлевают</strong><br />
+                    молодость и радует <br />
+                    питомца
+                </div>
+            </li>
+        </ul>
+
+        <h2 class="toys-landing__check-header">Проверяй марки где удобно!</h2>
+
+        <div class="toys-landing-check">
+            <img src="/upload/toys-landing/check.jpg" alt="" class="toys-landing-check__image" />
+
+            <div class="toys-landing-check__sidebar">
+                <p class="toys-landing-check__text">
+                    LOREM IPSUM Подходят
+                    для питомцев LOREM IPSUM
+                    от3х месяцев LOREM IPSUM
+                    LOREM IPSUM
+                </p>
+
+                <ul class="toys-landing-check__list">
+                    <li>
+                        <img src="/upload/toys-landing/check--1.png" alt="" />
+                        На чеке
+                    </li>
+
+                    <li>
+                        <img src="/upload/toys-landing/check--2.png" alt="" />
+                        В приложении
+                    </li>
+
+                    <li>
+                        <img src="/upload/toys-landing/check--3.png" alt="" />
+                        На сайте
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
+?>
