143895e26d54999b95668523440805cf79b8b3ba
 Vladislav Shavelsky <v.shavelsky@MacBook-Pro-Vladislav.local>
2019-08-29 18:49:58 +0300

    балланс марок и авторизация на лендинге

--- web/iqgames/index.php
+++ web/iqgames/index.php
@@ -3,34 +3,41 @@
 
 <div class="toys-landing">
     <div class="toys-landing__header">
-        <img src="/upload/toys-landing/header.jpg" alt=""/>
+        <img src="/upload/toys-landing/header.jpg" alt="" />
     </div>
 
     <div class="toys-landing__steps">
         <div class="b-container toys-landing__steps-container">
             <ul class="toys-landing__steps-list">
                 <li class="toys-landing__steps-item">
-                    <img src="/upload/toys-landing/step-1.svg" alt="" class="toys-landing__steps-image"/>
-                    <div>
-                        <p class="toys-landing__steps-title">Выбирай умные игрушки</p>
-                        <p class="toys-landing__steps-description">Покупай со скидкой до 50%</p>
-                    </div>
-                </li>
-                <li class="toys-landing__steps-item">
-                    <img src="/upload/toys-landing/step-2.svg" alt="" class="toys-landing__steps-image"/>
+                    <img src="/upload/toys-landing/step-1.svg" alt="" class="toys-landing__steps-image" />
+
                     <div>
                         <p class="toys-landing__steps-title">Копи марки</p>
                         <p class="toys-landing__steps-description">1 марка = 400 рублей</p>
                     </div>
                 </li>
+
                 <li class="toys-landing__steps-item">
-                    <img src="/upload/toys-landing/step-3.svg" alt="" class="toys-landing__steps-image"/>
+                    <img src="/upload/toys-landing/step-2.svg" alt="" class="toys-landing__steps-image" />
+
+                    <div>
+                        <p class="toys-landing__steps-title">Выбирай умные игрушки</p>
+                        <p class="toys-landing__steps-description">Покупай со скидкой до 50%</p>
+                    </div>
+                </li>
+
+                <li class="toys-landing__steps-item">
+                    <img src="/upload/toys-landing/step-3.svg" alt="" class="toys-landing__steps-image" />
+
                     <div>
                         <p class="toys-landing__steps-title">Занимайся с питомцем</p>
                         <p class="toys-landing__steps-description">Развивай любознательность, обучай и играй с удовольствием</p>
                     </div>
                 </li>
             </ul>
+
+            <? $APPLICATION->IncludeComponent('articul:stamps.balance', '') ?>
         </div>
     </div>
 
@@ -39,30 +46,30 @@
     <div class="b-container">
         <ul class="toys-landing-benefits">
             <li class="toys-landing-benefits__item">
-                <img src="/upload/toys-landing/benefit-1.png" alt="" class="toys-landing-benefits__image"/>
+                <img src="/upload/toys-landing/benefit-1.png" alt="" class="toys-landing-benefits__image" />
 
                 <div class="toys-landing-benefits__text">
-                    <strong>Развивают</strong><br/>
+                    <strong>Развивают</strong><br />
                     интеллект, внимание<br/>
                     и&nbsp;ловкость
                 </div>
             </li>
             <li class="toys-landing-benefits__item">
-                <img src="/upload/toys-landing/benefit-2.png" alt="" class="toys-landing-benefits__image"/>
+                <img src="/upload/toys-landing/benefit-2.png" alt="" class="toys-landing-benefits__image" />
 
                 <div class="toys-landing-benefits__text">
-                    <strong>Подходят</strong><br/>
-                    для питомцев <br/>
+                    <strong>Подходят</strong><br />
+                    для питомцев <br />
                     от 3-х месяцев
                 </div>
             </li>
 
             <li class="toys-landing-benefits__item">
-                <img src="/upload/toys-landing/benefit-3.png" alt="" class="toys-landing-benefits__image"/>
+                <img src="/upload/toys-landing/benefit-3.png" alt="" class="toys-landing-benefits__image" />
 
                 <div class="toys-landing-benefits__text">
-                    <strong>Продлевают</strong><br/>
-                    молодость и&nbsp;радуют <br/>
+                    <strong>Продлевают</strong><br />
+                    молодость и&nbsp;радуют <br />
                     питомца
                 </div>
             </li>
@@ -71,37 +78,47 @@
         <h2 class="toys-landing__check-header">Как отслеживать баланс марок</h2>
 
         <div class="toys-landing-check">
-            <img src="/upload/toys-landing/check.jpg" alt="" class="toys-landing-check__image"/>
+            <img src="/upload/toys-landing/check.jpg" alt="" class="toys-landing-check__image" />
 
             <div class="toys-landing-check__sidebar">
-                <p class="toys-landing-check__text">
-                    <strong style="text-transform: uppercase;">Копи марки как тебе удобно</strong> <br/> <br/>
-
-                    АКТИВИРУЙ КАРТУ, СОВЕРШАЙ ПОКУПКИ, ОТСЛЕЖИВАЙ БАЛАНС МАРОК НА ЧЕКЕ, В
-                    ЛИЧНОМ КАБИНЕТЕ НА САЙТЕ ИЛИ В МОБИЛЬНОМ ПРИЛОЖЕНИИ, ПОКУПАЙ УМНЫЕ
-                    ИГРУШКИ СО СКИДКОЙ ДО 50%.
-                </p>
-
-                <ul class="toys-landing-check__list">
+                <ul class="toys-landing-check__sidebar-list">
                     <li>
-                        <img src="/upload/toys-landing/check--1.png" alt=""/>
-                        На чеке
+                        <span style="text-transform: uppercase;">
+                            <b>
+                                делай покупки, получай марки: 1
+                                <img src="/upload/toys-landing/logo.png" width="19" height="19" style="transform: translateY(2px)" />
+                                = 400р
+                            </b>
+                        </span>
                     </li>
 
                     <li>
-                        <img src="/upload/toys-landing/check--2.png" alt=""/>
-                        В приложении
+                        <span style="text-transform: uppercase;">
+                            <b>отслеживай марки где удобно:</b> <br />
+                            на чеке, <u>в личном кабинете</u> на сайте
+                            и в приложении
+                        </span>
                     </li>
 
                     <li>
-                        <img src="/upload/toys-landing/check--3.png" alt=""/>
-                        На сайте
+                        <span style="text-transform: uppercase;">
+                            <b>На сайте и в приложении:</b><br />
+                            выбери игру и добавь в корзину
+                            нажми “списать марки” <br /><br />
+
+                            <b>при покупке офлайн</b> <br/>
+                            вам спишет марки кассир
+                        </span>
+                    </li>
+
+                    <li>
+                        <span style="text-transform: uppercase;">
+                            <b>Получи игру и развивай питомца</b>
+                        </span>
                     </li>
                 </ul>
             </div>
         </div>
-
-        <? $APPLICATION->IncludeComponent('articul:stamps.balance', '') ?>
     </div>
 </div>
 
