19eee17f5c931c17252b59cf298a3143e7ff0e83
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-09-03 15:50:40 +0000

    TM-35419 Костыль - доп.запросы в Manzana по балансу, если первый не прошел v2

--- src/PersonalBundle/Service/StampService.php
+++ src/PersonalBundle/Service/StampService.php
@@ -166,8 +166,21 @@ class StampService implements LoggerAwareInterface
                     $this->activeStampsCount = 0;
                     return $this->activeStampsCount;
                 } else {
+                    throw new ExecuteErrorException($e->getMessage(), $e->getCode());
+                }
+            } catch (\Exception $e) {
+                //FIXME Убрать! Временный (но плохой) костыль для быстрого решения проблемы - еще два дополнительных запроса, если не получен ответ от Manzana
 
-                    //FIXME Убрать! Временный (но плохой) костыль для быстрого решения проблемы - еще два дополнительных запроса, если не получен ответ от Manzana
+                try {
+                    $balanceResponse = $this->manzanaPosService->executeBalanceRequest((new BalanceRequest())->setCardByNumber($discountCardNumber));
+                } catch (ExecuteErrorException $e) {
+                    if ($e->getCode() == 80241) { // Карта не найдена
+                        $this->activeStampsCount = 0;
+                        return $this->activeStampsCount;
+                    } else {
+                        throw new ExecuteErrorException($e->getMessage(), $e->getCode());
+                    }
+                } catch (\Exception $e) {
                     try {
                         $balanceResponse = $this->manzanaPosService->executeBalanceRequest((new BalanceRequest())->setCardByNumber($discountCardNumber));
                     } catch (ExecuteErrorException $e) {
@@ -175,17 +188,10 @@ class StampService implements LoggerAwareInterface
                             $this->activeStampsCount = 0;
                             return $this->activeStampsCount;
                         } else {
-                            try {
-                                $balanceResponse = $this->manzanaPosService->executeBalanceRequest((new BalanceRequest())->setCardByNumber($discountCardNumber));
-                            } catch (ExecuteErrorException $e) {
-                                if ($e->getCode() == 80241) { // Карта не найдена
-                                    $this->activeStampsCount = 0;
-                                    return $this->activeStampsCount;
-                                } else {
-                                    throw new ExecuteErrorException($e->getMessage(), $e->getCode());
-                                }
-                            }
+                            throw new ExecuteErrorException($e->getMessage(), $e->getCode());
                         }
+                    } catch (\Exception $e) {
+                        throw new \Exception($e->getMessage(), $e->getCode());
                     }
                 }
             }
