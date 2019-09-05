991465b53dd5a5d2cdf98860364376394951dd16
mmasterkov <m.masterkov@articul.ru>
2019-09-05 18:05:50 +0300

    Пустая миграция

---
+++ common/local/php_interface/migrations/HlBlockOrderSubscribeUpgrade20190905173304.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class HlBlockOrderSubscribeUpgrade20190905173304 extends \Adv\Bitrixtools\Migration\SprintMigrationBase {
+
+    protected $description = "Создаёт новую таблицу \"Единичная подписка\" и добавляет новые интервалы периодичности";
+
+    public function up(){
+        $helper = new HelperManager();
+
+        //your code ...
+
+    }
+
+    public function down(){
+        $helper = new HelperManager();
+
+        //your code ...
+
+    }
+
+}
