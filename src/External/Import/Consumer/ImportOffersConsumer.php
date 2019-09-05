8362e2bb73f2905c463ab4c4d62557081043d5bb
Anton Ankilov <a.ankilov@articul.ru>
2019-09-05 10:54:26 +0000

    tm-35848 исправление ошибки

--- src/External/Import/Consumer/ImportOffersConsumer.php
+++ src/External/Import/Consumer/ImportOffersConsumer.php
@@ -25,7 +25,8 @@ class ImportOffersConsumer extends ImportConsumerBase
         /** @var ImportOffer $importOffer */
         $importOffer = $this->serializer->deserialize($message->getBody(), ImportOffer::class, 'json');
 
-        $currentDate = new \DateTime(new \DateTimeZone('Europe/Moscow'));
+        $currentDate = new \DateTime();
+        $currentDate->setTimezone(new \DateTimeZone('Europe/Moscow'));
         $dateActive = new \DateTime($importOffer->activeFrom);
 
         if ($dateActive > $currentDate) {
