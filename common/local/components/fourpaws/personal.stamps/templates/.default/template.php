e78d81bb8256c345bd6b4c5e265a67f115aac2a4
Daria <d.gritsaenko@articul.ru>
2019-09-10 11:02:43 +0300

    tm-36389 проставила количество отображаемых товаров в карусели

--- common/local/components/fourpaws/personal.stamps/templates/.default/template.php
+++ common/local/components/fourpaws/personal.stamps/templates/.default/template.php
@@ -51,6 +51,7 @@ $marksDeclension = new Declension('марку', 'марки', 'марок');
             'OFFER_FILTER' => [
                 '=XML_ID' => $arParams['PRODUCTS_XML_ID'],
             ],
+            'COUNT' => 500,
             'TITLE' => 'Товары, участвующие в акции',
 	        'CACHE_TIME' => 0,
 	        'ONLY_PRODUCTS_XML_ID' => $arParams['PRODUCTS_XML_ID'], // показывать только указанные ТП, а не все в родительских товарах
