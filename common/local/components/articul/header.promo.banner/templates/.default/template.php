e3ac0d5b6148e91128779d0782fd7dc8e95d2214
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2019-09-17 14:35:02 +0300

    task36522 решил конфликт с веткой tm-36473

--- common/local/components/articul/header.promo.banner/templates/.default/template.php
+++ common/local/components/articul/header.promo.banner/templates/.default/template.php
@@ -3,14 +3,15 @@
 <?php if ($arResult['ELEMENT']) { ?>
     <div class="b-promo-top-full b-promo-top-full--fashion js-promo-top-full hide">
         <div class="b-container b-promo-top-full__fashion-container">
-            <div>
-                <a href="<?= $arResult['ELEMENT']['LINK'] ?>" target="_blank">
+            <a href="<?= $arResult['ELEMENT']['LINK'] ?>" class="b-promo-top-full__fashion-inner">
+                <div class="b-promo-top-full__fashion-image-wrap">
                     <img src="<?= $arResult['IMAGES'][$arResult['ELEMENT']['PICTURE']] ?>" alt="<?= $arResult['NAME'] ?>" class="b-promo-top-full__fashion-image"/>
                     <img src="<?= $arResult['IMAGES'][$arResult['ELEMENT']['MOBILE_PICTURE']] ?>" alt="<?= $arResult['NAME'] ?>" class="b-promo-top-full__fashion-image b-promo-top-full__fashion-image--mobile"/>
-                </a>
-            </div>
-
-            <div class="b-promo-top-full__close js-close-promo-top-full"></div>
+                </div>
+                <div class="b-promo-top-full__close-wrap js-close-promo-top-full">
+                    <div class="b-promo-top-full__close"></div>
+                </div>
+            </a>
         </div>
     </div>
 <?php } ?>
