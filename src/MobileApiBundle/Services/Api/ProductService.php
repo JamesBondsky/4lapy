18dc2a776280355a93ddaaaeefdad2f55fa6a717
Anton Ankilov <a.ankilov@articul.ru>
2019-09-23 14:07:16 +0000

    tm-36587 исправление ошибки с получением цвета

--- src/MobileApiBundle/Services/Api/ProductService.php
+++ src/MobileApiBundle/Services/Api/ProductService.php
@@ -1111,8 +1111,10 @@ class ProductService
         foreach ($offers as $offer) {
             if ($offerImages = $offer->getResizeImages(static::DETAIL_PICTURE_WIDTH, static::DETAIL_PICTURE_HEIGHT)) {
                 foreach ($offerImages as $image) {
-                    if ($currentOffer->getColor()->getColorCode() != $offer->getColor()->getColorCode()) {
-                        $images[] = $image;
+                    if ($currentOffer->getColor()) {
+                        if ($currentOffer->getColor()->getColorCode() != $offer->getColor()->getColorCode()) {
+                            $images[] = $image;
+                        }
                     } else {
                         $addInStart[] = $image;
                     }
