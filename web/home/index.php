f85e1ac4738f1c9e619873c29cd436ee2a391ee3
Daria <d.gritsaenko@articul.ru>
2019-09-23 14:30:14 +0300

    tm-36732 лендинг Уютно жить, стартовые файлы

---
+++ web/home/index.php
@@ -0,0 +1,14 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+
+$APPLICATION->SetPageProperty('title', 'Уютно жить');
+$APPLICATION->SetPageProperty('description', '');
+$APPLICATION->SetTitle("Уютно жить");
+?>
+<div class="comfortable-living-page">
+    <section class="questions-comfortable-living">
+        <div class="title-comfortable-living"></div>
+    </section>
+</div>
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'; ?>
