89cdcb824b3d606e8bad4c2d00837e561c163660
Daria <d.gritsaenko@articul.ru>
2019-09-24 17:35:55 +0300

    tm-36732 верстка лендинга про Уютно жить

--- web/home/index.php
+++ web/home/index.php
@@ -4,23 +4,59 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 $APPLICATION->SetPageProperty('title', 'Уютно жить');
 $APPLICATION->SetPageProperty('description', '');
 $APPLICATION->SetTitle("Уютно жить");
-?>
+
+use FourPaws\Decorators\SvgDecorator;
+use FourPaws\Enum\IblockCode;
+use FourPaws\Enum\IblockType; ?>
+
 <div class="comfortable-living-page">
+
+    <section class="contest-comfortable-living">
+        <div class="b-container">
+            <h2 class="title-comfortable-living">Как накопить марки и купить домик, лежак или когтеточку со скидкой до - 30%</h2>
+        </div>
+    </section>
+
+    <section class="info-comfortable-living">
+        <div class="b-container">
+            <h2 class="title-comfortable-living">Как накопить марки и купить домик, лежак или когтеточку со скидкой до - 30%</h2>
+             <div class="info-comfortable-living__content">
+                <div class="info-comfortable-living__img-wrap">
+                    <div class="info-comfortable-living__img" style="background-image: url('/home/img/steps-info.jpg')"></div>
+                </div>
+                 <ol class="info-comfortable-living__steps">
+                     <li class="item">Совершай любые покупки, копи марки в&nbsp;буклете
+                         или Личном кабинете: 1&nbsp;<span class="b-icon b-icon--mark"><?= new SvgDecorator('icon-mark', 24, 24) ?></span>&nbsp;=&nbsp;500&nbsp;Р</li>
+                     <li class="item">Отслеживай баланс марок: на&nbsp;чеке, в&nbsp;буклете <a href="/personal/marki/" target="_blank">в&nbsp;личном&nbsp;кабинете</a> и&nbsp;в&nbsp;приложении</li>
+                     <li class="item">
+                         Покупай лежаки и&nbsp;когтеточки со&nbsp;скидкой до&nbsp;-30%
+
+                         <ul class="item__list">
+                             <li>&mdash;&nbsp;на&nbsp;сайте и&nbsp;в&nbsp;приложении: добавь товар в&nbsp;корзину, нажми &laquo;списать марки&raquo;</li>
+                             <li>&mdash;&nbsp;в&nbsp;магазине: предъяви буклет или сообщи кассиру номер телефона</li>
+                         </ul>
+                     </li>
+                 </ol>
+             </div>
+        </div>
+    </section>
+
     <section class="questions-comfortable-living">
         <div class="b-container">
-            <div class="title-comfortable-living">Вопросы и ответы</div>
+            <h2 class="title-comfortable-living title-comfortable-living_questions">Вопросы и ответы</h2>
             <div class="questions-comfortable-living__accordion">
                 <div class="item-accordion">
                     <div class="item-accordion__header js-toggle-accordion">
-                        <span class="item-accordion__header-inner">Как накопить марки</span>
+                        <span class="item-accordion__header-inner">Как накопить марки?</span>
                     </div>
                     <div class="item-accordion__block js-dropdown-block">
-                        «Четыре Лапы»&nbsp;— это крупнейшая сеть магазинов, насчитывает более 240&nbsp;магазинов, представленных в&nbsp;20 регионах России.
-                        Мы&nbsp;гарантируем качество всех товаров, так как работаем лишь с&nbsp;проверенными и&nbsp;надёжными поставщиками. Все товары в&nbsp;наших магазинах прошли тщательную проверку,
-                        сертифицированы и&nbsp;соответствуют стандартам европейского уровня. Мы&nbsp;поддерживаем доступные цены,
-                        ведь в&nbsp;сети «Четыре Лапы» регулярно проходят акции и&nbsp;сезонные распродажи зоотоваров, а&nbsp;так&nbsp;же действует накопительная бонусная программа.
-                        Компания сотрудничает с&nbsp;питомниками и&nbsp;приютами (в&nbsp;нашей сети Вы&nbsp;всегда сможете поучаствовать в&nbsp;оказании адресной помощи бездомным животным) на&nbsp;регулярной основе,
-                        но&nbsp;при этом развиваем и&nbsp;собственную благотворительную программу «Добролап».
+                        <div class="item-accordion__block-content">
+                            <div class="item-accordion__block-text">
+                                Покупай Taft в магазинах сети «Лента» с 1 по 30 сентября и получай
+                                гарантированно 30 баллов на карту лояльности, а также участвуй
+                                в розыгрыше Beauty Box.
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="item-accordion">
@@ -28,12 +64,16 @@ $APPLICATION->SetTitle("Уютно жить");
                         <span class="item-accordion__header-inner">Какие будут подарки</span>
                     </div>
                     <div class="item-accordion__block js-dropdown-block">
-                        «Четыре Лапы»&nbsp;— это крупнейшая сеть магазинов, насчитывает более 240&nbsp;магазинов, представленных в&nbsp;20 регионах России.
-                        Мы&nbsp;гарантируем качество всех товаров, так как работаем лишь с&nbsp;проверенными и&nbsp;надёжными поставщиками. Все товары в&nbsp;наших магазинах прошли тщательную проверку,
-                        сертифицированы и&nbsp;соответствуют стандартам европейского уровня. Мы&nbsp;поддерживаем доступные цены,
-                        ведь в&nbsp;сети «Четыре Лапы» регулярно проходят акции и&nbsp;сезонные распродажи зоотоваров, а&nbsp;так&nbsp;же действует накопительная бонусная программа.
-                        Компания сотрудничает с&nbsp;питомниками и&nbsp;приютами (в&nbsp;нашей сети Вы&nbsp;всегда сможете поучаствовать в&nbsp;оказании адресной помощи бездомным животным) на&nbsp;регулярной основе,
-                        но&nbsp;при этом развиваем и&nbsp;собственную благотворительную программу «Добролап».
+                        <div class="item-accordion__block-content">
+                            <div class="item-accordion__block-text">
+                                Покупай Taft в магазинах сети «Лента» с 1 по 30 сентября и получай
+                                гарантированно 30 баллов на карту лояльности, а также участвуй
+                                в розыгрыше Beauty Box.
+                            </div>
+                            <div class="item-accordion__block-img">
+                                <img src="/home/img/questions.png" alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="item-accordion">
@@ -41,12 +81,16 @@ $APPLICATION->SetTitle("Уютно жить");
                         <span class="item-accordion__header-inner">Как принять участие</span>
                     </div>
                     <div class="item-accordion__block js-dropdown-block">
-                        «Четыре Лапы»&nbsp;— это крупнейшая сеть магазинов, насчитывает более 240&nbsp;магазинов, представленных в&nbsp;20 регионах России.
-                        Мы&nbsp;гарантируем качество всех товаров, так как работаем лишь с&nbsp;проверенными и&nbsp;надёжными поставщиками. Все товары в&nbsp;наших магазинах прошли тщательную проверку,
-                        сертифицированы и&nbsp;соответствуют стандартам европейского уровня. Мы&nbsp;поддерживаем доступные цены,
-                        ведь в&nbsp;сети «Четыре Лапы» регулярно проходят акции и&nbsp;сезонные распродажи зоотоваров, а&nbsp;так&nbsp;же действует накопительная бонусная программа.
-                        Компания сотрудничает с&nbsp;питомниками и&nbsp;приютами (в&nbsp;нашей сети Вы&nbsp;всегда сможете поучаствовать в&nbsp;оказании адресной помощи бездомным животным) на&nbsp;регулярной основе,
-                        но&nbsp;при этом развиваем и&nbsp;собственную благотворительную программу «Добролап».
+                        <div class="item-accordion__block-content">
+                            <div class="item-accordion__block-text">
+                                Покупай Taft в магазинах сети «Лента» с 1 по 30 сентября и получай
+                                гарантированно 30 баллов на карту лояльности, а также участвуй
+                                в розыгрыше Beauty Box.
+                            </div>
+                            <div class="item-accordion__block-img">
+                                <img src="/home/img/questions.png" alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="item-accordion">
@@ -54,15 +98,25 @@ $APPLICATION->SetTitle("Уютно жить");
                         <span class="item-accordion__header-inner">Ещё какие-то вопросы</span>
                     </div>
                     <div class="item-accordion__block js-dropdown-block">
-                        «Четыре Лапы»&nbsp;— это крупнейшая сеть магазинов, насчитывает более 240&nbsp;магазинов, представленных в&nbsp;20 регионах России.
-                        Мы&nbsp;гарантируем качество всех товаров, так как работаем лишь с&nbsp;проверенными и&nbsp;надёжными поставщиками. Все товары в&nbsp;наших магазинах прошли тщательную проверку,
-                        сертифицированы и&nbsp;соответствуют стандартам европейского уровня. Мы&nbsp;поддерживаем доступные цены,
-                        ведь в&nbsp;сети «Четыре Лапы» регулярно проходят акции и&nbsp;сезонные распродажи зоотоваров, а&nbsp;так&nbsp;же действует накопительная бонусная программа.
-                        Компания сотрудничает с&nbsp;питомниками и&nbsp;приютами (в&nbsp;нашей сети Вы&nbsp;всегда сможете поучаствовать в&nbsp;оказании адресной помощи бездомным животным) на&nbsp;регулярной основе,
-                        но&nbsp;при этом развиваем и&nbsp;собственную благотворительную программу «Добролап».
+                        <div class="item-accordion__block-content">
+                            <div class="item-accordion__block-text">
+                                Покупай Taft в магазинах сети «Лента» с 1 по 30 сентября и получай
+                                гарантированно 30 баллов на карту лояльности, а также участвуй
+                                в розыгрыше Beauty Box.
+                            </div>
+                            <div class="item-accordion__block-img">
+                                <img src="/home/img/questions.png" alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <section class="fashion-interesting-clothes">
+        <div class="b-container">
+            <h2 class="title-comfortable-living">Полезные статьи</h2>
         </div>
     </section>
 </div>
