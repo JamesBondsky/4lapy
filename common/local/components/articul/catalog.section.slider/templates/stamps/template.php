7db13cf794bfc164bc363f96717da44e169b0ea5
Daria <d.gritsaenko@articul.ru>
2019-09-26 15:34:45 +0300

    tm-36732 привела слайдер на лендингах к общему классу item-category-landing

--- common/local/components/articul/catalog.section.slider/templates/stamps/template.php
+++ common/local/components/articul/catalog.section.slider/templates/stamps/template.php
@@ -9,17 +9,17 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 
 <div class="fashion-category-rungo">
     <? foreach($arResult['ELEMENTS'] as $i => $element) { ?>
-        <div class="item-category-fashion item-category-fashion--rungo active" data-item-filter-category-fashion="<?=$i?>" data-url="/ajax/catalog/product-info/">
+        <div class="item-category-landing item-category-landing--rungo active" data-item-filter-category-fashion="<?=$i?>" data-url="/ajax/catalog/product-info/">
             <div class="b-container">
-                <a href="<?= $component->getSectionLink($element['ID']) ?>" class="item-category-fashion__title" target="_blank"><?=$element['NAME']?></a>
-                <div class="item-category-fashion__content">
-                    <div class="item-category-fashion__img" style="background-image: url(<?=$arResult['IMAGES'][$element['PROPERTIES']['IMAGE']['VALUE']]?>)">
-                        <a href="<?= $component->getSectionLink($element['ID']) ?>" class="item-category-fashion__more" target="_blank">
+                <a href="<?= $component->getSectionLink($element['ID']) ?>" class="item-category-landing__title" target="_blank"><?=$element['NAME']?></a>
+                <div class="item-category-landing__content">
+                    <div class="item-category-landing__img" style="background-image: url(<?=$arResult['IMAGES'][$element['PROPERTIES']['IMAGE']['VALUE']]?>)">
+                        <a href="<?= $component->getSectionLink($element['ID']) ?>" class="item-category-landing__more" target="_blank">
                             В категорию
                         </a>
                     </div>
 
-                    <div class="item-category-fashion__slider" data-slider-category-fashion="true">
+                    <div class="item-category-landing__slider" data-slider-category-fashion="true">
                         <?php
                         $APPLICATION->IncludeComponent(
                             'fourpaws:catalog.snippet.list',
