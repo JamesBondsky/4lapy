baa0b0bb189587fa10794ee1e28cb55f6d074697
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2019-09-30 11:32:36 +0300

    tm-36460 перенес проверку даты в конец

--- common/local/components/fourpaws/order/class.php
+++ common/local/components/fourpaws/order/class.php
@@ -408,15 +408,6 @@ class FourPawsOrderComponent extends \CBitrixComponent
             $this->arResult['URL'][$key] = $route->getPath();
         }
 
-        if (!$this->orderStorageService->validateDeliveryDate($storage)) {
-            $storage = $this->orderStorageService->clearDeliveryDate($storage);
-            $this->orderStorageService->updateStorage($storage, OrderStorageEnum::NOVALIDATE_STEP);
-
-            if ($this->currentStep === OrderStorageEnum::PAYMENT_STEP) {
-                LocalRedirect($this->arResult['URL']['AUTH']);
-            }
-        }
-
         $realStep = $this->orderStorageService->validateStorage($storage, $this->currentStep);
         if ($realStep !== $this->currentStep) {
             if ($this->currentStep === OrderStorageEnum::PAYMENT_STEP && $_SESSION['ORDER_PAYMENT_URL']) {
@@ -571,6 +562,16 @@ class FourPawsOrderComponent extends \CBitrixComponent
             }
 
             $payments = $this->orderStorageService->getAvailablePayments($storage, true, true, $basket->getPrice());
+        }
+
+        // сравнение даты доставки с текущей, после всех остальных расчетов/проверок
+        if (!$this->orderStorageService->validateDeliveryDate($storage)) {
+            $storage = $this->orderStorageService->clearDeliveryDate($storage);
+            $this->orderStorageService->updateStorage($storage, OrderStorageEnum::NOVALIDATE_STEP);
+
+            if ($this->currentStep === OrderStorageEnum::PAYMENT_STEP) {
+                LocalRedirect($this->arResult['URL']['AUTH']);
+            }
         }
 
         $this->arResult['BASKET']             = $basket;
