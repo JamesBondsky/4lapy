0b86608875ed9878029e27a2ee044cc86cc8a708
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2019-10-03 12:31:58 +0300

    tm-37039 способ коммуникации "анализ адрес" в заказах по подписке

--- src/SaleBundle/Helper/OrderCopy.php
+++ src/SaleBundle/Helper/OrderCopy.php
@@ -41,6 +41,7 @@ use FourPaws\SaleBundle\Exception\OrderCopyBasketException;
 use FourPaws\SaleBundle\Exception\OrderCopyShipmentsException;
 use FourPaws\SaleBundle\Exception\OrderCreateException;
 use FourPaws\SaleBundle\Service\BasketService;
+use FourPaws\SaleBundle\Service\OrderPropertyService;
 use FourPaws\SaleBundle\Service\OrderService;
 use FourPaws\StoreBundle\Service\StoreService;
 use Bitrix\Sale\PaySystem\Manager as PaySystemManager;
@@ -1560,6 +1561,14 @@ class OrderCopy
             }
         }
         $orderService->setOrderAddress($order, $address);
+
+        if (($subscribe->getLocationId()) && ($address->getLocation() !== $subscribe->getLocationId())) {
+            $propValues = [
+                'COM_WAY' => OrderPropertyService::COMMUNICATION_ADDRESS_ANALYSIS,
+                'CITY_CODE' => $subscribe->getLocationId(),
+            ];
+            $orderService->setOrderPropertiesByCode($order, $propValues);
+        }
 
         // параметры доставки
         /** @var PropertyValue $propertyValue */
