60a8f527649033838ab03e9357e95d2711391020
mmasterkov <m.masterkov@articul.ru>
2019-10-30 14:26:30 +0300

    Фикс

--- common/local/components/fourpaws/personal.orders.subscribe.form/class.php
+++ common/local/components/fourpaws/personal.orders.subscribe.form/class.php
@@ -1178,8 +1178,10 @@ class FourPawsPersonalCabinetOrdersSubscribeFormComponent extends CBitrixCompone
                 $this->arResult['SELECTED_DATE'] = $orderSubscribe->getNearestDelivery();
                 $this->arResult['SELECTED_TIME'] = $orderSubscribe->getDeliveryTime();
                 if($orderSubscribeSingle){
-                    $this->arResult['SELECTED_DATE'] = $orderSubscribeSingle->getSubscribe()->getNextDate();
-                    $this->arResult['SELECTED_TIME'] = $orderSubscribeSingle->getSubscribe()->getDeliveryTime();
+                    $arOrderSubscribe = $orderSubscribeSingle->getSubscribe();
+                    $orderSubscribe = $this->arrayTransformer->fromArray($arOrderSubscribe, OrderSubscribe::class);
+                    $this->arResult['SELECTED_DATE'] = $orderSubscribe->getNextDate();
+                    $this->arResult['SELECTED_TIME'] = $orderSubscribe->getDeliveryTime();
                 }
 
                 // $this->arResult['MIN_DELIVERY_DATE'] = new \DateTime($this->getOrderSubscribeHistoryService()->getNearestDelivery($orderSubscribe));
