6faec2b39ce792919f16f9db7465a9724ae59d4f
Andrey Vorobyev <a.vorobyev@articul.ru>
2019-10-31 15:16:17 +0300

    TM-37695 анти-ddos

--- src/App/MainTemplate.php
+++ src/App/MainTemplate.php
@@ -491,7 +491,7 @@ class MainTemplate extends TemplateAbstract
      */
     public function hasUserAuth(): bool
     {
-        return $this->isDir('/ajax/user/auth/login-r') || $this->isDir('/api/user_login') || $this->isPartitionDirByFilePath('/personal') || $this->isPartitionDirByFilePath('/sale');
+        return $this->isDir('/ajax/user/auth/login-o') || $this->isDir('/api/user_login') || $this->isPartitionDirByFilePath('/personal') || $this->isPartitionDirByFilePath('/sale');
     }
 
     /**
