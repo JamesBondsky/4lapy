8039bdac7546ca1fe19d2df1813cdc40206a51ba
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2019-11-11 12:49:25 +0300

    баннер присылается неавторизованным пользователям

--- src/MobileApiBundle/Controller/v0/BannerController.php
+++ src/MobileApiBundle/Controller/v0/BannerController.php
@@ -7,14 +7,12 @@
 namespace FourPaws\MobileApiBundle\Controller\v0;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
 use FourPaws\MobileApiBundle\Controller\BaseController;
 use FourPaws\MobileApiBundle\Dto\Request\BannersRequest;
 use FourPaws\MobileApiBundle\Dto\Response\BannerListResponse;
 use FourPaws\MobileApiBundle\Services\Api\BannerService as ApiBannerService;
 use FourPaws\Helpers\TaggedCacheHelper;
 use Bitrix\Main\Application;
-use FourPaws\MobileApiBundle\Services\Api\UserService;
 
 class BannerController extends BaseController
 {
@@ -23,25 +21,16 @@ class BannerController extends BaseController
      */
     private $apiBannerService;
 
-    /**
-     * @var UserService
-     */
-    protected $apiUserService;
-
     private $cacheTime = 3600;
     private $cachePath = '/api/banners';
 
-    public function __construct(
-        ApiBannerService $apiBannerService,
-        UserService $apiUserService
-    )
+    public function __construct(ApiBannerService $apiBannerService)
     {
         $this->apiBannerService = $apiBannerService;
-        $this->apiUserService = $apiUserService;
     }
 
     /**
-     * @Rest\Get("/baner_list/")
+     * @Rest\Get("/ baner_list/")
      * @Rest\View()
      *
      * @param BannersRequest $bannersRequest
@@ -71,27 +60,6 @@ class BannerController extends BaseController
         } else {
             $apiResponse = $cache->getVars();
         }
-
-        /* для неавторизованных пользователей убираем баннер с квестом */
-        $bannerList = $apiResponse->getBannerList();
-        foreach ($bannerList as $key => $banner) {
-            if ($banner->getType() === 'quest') {
-                $deleteBanner = false;
-                try {
-                    if ($this->apiUserService->getCurrentApiUser() === null) {
-                        $deleteBanner = true;
-                    }
-                } catch (\Exception $e) {
-                    $deleteBanner = true;
-                }
-
-                if ($deleteBanner) {
-                    unset($bannerList[$key]);
-                }
-            }
-        }
-
-        $apiResponse->setBannerList($bannerList);
 
         return $apiResponse;
     }
