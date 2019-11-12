dbd7d2d46939eb3dedf3e3f3999582eedc8c6feb
Daria <d.gritsaenko@articul.ru>
2019-11-12 15:52:04 +0300

    tm-37803 новая логика в блоке тренировок

--- common/local/components/articul/flagman.training/templates/training/template.php
+++ common/local/components/articul/flagman.training/templates/training/template.php
@@ -1,51 +1,50 @@
-<section class="service-flagship-store" data-item-service-flagship-store="grooming">
+<section class="service-flagship-store" data-item-service-flagship-store="training">
     <div class="b-container">
-        <div class="service-flagship-store__header service-flagship-store__header_grooming">
+        <div class="service-flagship-store__header service-flagship-store__header_training">
             <div class="service-flagship-store__inner-header">
-                <div class="service-flagship-store__title">Груминг</div>
+                <div class="service-flagship-store__title">Тренировочный клуб</div>
                 <div class="service-flagship-store__btn active" data-toggle-service-flagship-store="true"></div>
             </div>
         </div>
         <div class="service-flagship-store__content" data-content-service-flagship-store="true" style="display: block">
             <div class="service-flagship-store__descr">
-                Груминг&nbsp;&mdash; это уход за&nbsp;внешностью животного, его кожей и&nbsp;шерстью, когтями и&nbsp;ушами.
-                Первоначально этот термин означал совокупность гигиенических процедур по&nbsp;уходу за&nbsp;домашними питомцами.
+                Запись на&nbsp;мастер класс по&nbsp;послушанию питомца. Вы&nbsp;сможете задать вопросы по&nbsp;правильному воспитанию вашей собаки опытному кинологу, а&nbsp;так&nbsp;же, разучить несколько команд.
             </div>
-            <form class="form-signup-grooming-flagship js-form-validation" data-form-signup-grooming-flagship="true">
-                <div class="form-signup-grooming-flagship__content">
-                    <div class="form-signup-grooming-flagship__top">
-                        <div class="b-input-line">
-                            <div class="b-input-line__label-wrapper">
-                                <span class="b-input-line__label">Дата</span>
-                            </div>
-                            <div class="b-select">
-                                <select class="b-select__block" data-date-grooming-flagship="true">
-                                    <option value="" disabled="disabled" selected="selected">выберите</option>
-                                    <?php foreach ($arResult['SCHEDULE'] as $key => $day) : ?>
-                                        <?php if ($day['end'] != 'Y') : ?>
-                                            <option value="<?=$key?>" data-url="/flagman/getschedule/grooming/<?=$key?>" data-date-option="<?=$day['day']?>"><?=$day['day']?></option>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-                                </select>
-                                <div class="b-error"><span class="js-message"></span></div>
-                            </div>
-                        </div>
+            <a class="link-walking-flagship-store" href="/events/Правила_тренировочного_клуба.pdf" target="_blank">Правила тренировочного клуба</a>
 
-                        <div class="b-input-line b-input-line--time">
-                            <div class="b-input-line__label-wrapper">
-                                <label class="b-input-line__label">Время</label>
-                            </div>
-                            <div class="b-select">
-                                <select class="b-select__block" data-time-grooming-flagship="true">
-                                    <option value="" disabled="disabled" selected="selected">выберите</option>
-                                </select>
-                                <div class="b-error"><span class="js-message"></span></div>
-                            </div>
+            <form class="form-signup-training-flagship js-form-validation" data-form-signup-training-flagship="true">
+                <div class="form-signup-training-flagship__content">
+                    <div class="b-input-line">
+                        <div class="b-input-line__label-wrapper">
+                            <span class="b-input-line__label">Дата</span>
+                        </div>
+                        <div class="b-select">
+                            <select class="b-select__block" data-date-training-flagship="true">
+                                <option value="" disabled="disabled" selected="selected">выберите</option>
+                                <?php foreach ($arResult['SCHEDULE'] as $key => $day) : ?>
+                                    <?php if ($day['end'] != 'Y') : ?>
+                                        <option value="<?=$key?>" data-url="/flagman/getschedule/training/<?=$key?>" data-date-option="<?=$day['day']?>"><?=$day['day']?></option>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </select>
+                            <div class="b-error"><span class="js-message"></span></div>
                         </div>
                     </div>
-                    
-                    <div class="form-signup-grooming-flagship__btn-wrap">
-                        <button type="submit" class="b-button" data-popup-id="grooming-flagship-store" data-btn-grooming-flagship-store="true">Записаться</button>
+
+                    <div class="b-input-line b-input-line--time">
+                        <div class="b-input-line__label-wrapper">
+                            <label class="b-input-line__label">Время</label>
+                        </div>
+                        <div class="b-select">
+                            <select class="b-select__block" data-time-training-flagship="true">
+                                <option value="" disabled="disabled" selected="selected">выберите</option>
+                            </select>
+                            <div class="b-error"><span class="js-message"></span></div>
+                        </div>
+                    </div>
+
+                    <div class="form-signup-training-flagship__btn-wrap">
+                        <button type="submit" class="b-button" data-popup-id="training-flagship-store" data-btn-training-flagship-store="true">Записаться</button>
                     </div>
                 </div>
             </form>
