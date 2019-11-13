b12bd298939b47b081283971f6ae3cdaff02a38d
Daria <d.gritsaenko@articul.ru>
2019-11-13 15:49:30 +0300

    tm-37803 дополнительное поле email и предзаполненная форма

--- common/local/templates/4paws/blocks/footer/recording-lecture-flagship-store.php
+++ common/local/templates/4paws/blocks/footer/recording-lecture-flagship-store.php
@@ -18,12 +18,13 @@ if ($USER->IsAuthorized()) {
                     <label class="b-input-line__label" for="data-first-name">Имя</label>
                 </div>
                 <div class="b-input">
-                    <input class="b-input__input-field b-input__input-field--registration-form js-small-input"
+                    <input class="b-input__input-field js-small-input"
                            type="text"
                            id="data-first-name"
                            name="name"
                            value="<?=($user['NAME']) ? $user['NAME'] : ''?>"
                            data-text="1"
+                           data-name-service-flagship-store-popup="true"
                            placeholder="" />
                     <div class="b-error"><span class="js-message"></span>
                     </div>
@@ -35,14 +36,31 @@ if ($USER->IsAuthorized()) {
                     <label class="b-input-line__label" for="edit-phone">Телефон</label>
                 </div>
                 <div class="b-input">
-                    <input class="b-input__input-field b-input__input-field--registration-form"
+                    <input class="b-input__input-field"
                            type="tel"
                            id="edit-phone"
                            name="phone"
                            value="<?=($user['PERSONAL_PHONE']) ? $user['PERSONAL_PHONE'] : ''?>"
+                           data-phone-service-flagship-store-popup="true"
                            placeholder="" />
                     <div class="b-error"><span class="js-message"></span>
                     </div>
+                </div>
+            </div>
+
+            <div class="b-input-line">
+                <div class="b-input-line__label-wrapper">
+                    <label class="b-input-line__label" for="data-email">Эл. почта</label>
+                </div>
+                <div class="b-input">
+                    <input class="b-input__input-field"
+                           type="email"
+                           id="data-email"
+                           name="email"
+                           value="<?=($user['EMAIL']) ? $user['EMAIL'] : ''?>"
+                           data-email-service-flagship-store-popup="true"
+                           placeholder="" />
+                    <div class="b-error"><span class="js-message"></span></div>
                 </div>
             </div>
 
