01e4f9533d7d96f2cca05dfe718fa519fff34aa0
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-11-22 12:17:42 +0300

    sp_38048 Черная Пятница

--- common/local/components/articul/blackfriday.action.users/templates/.default/template.php
+++ common/local/components/articul/blackfriday.action.users/templates/.default/template.php
@@ -1,17 +1,19 @@
-<section class="brands-blackfriday">
-    <div class="b-container">
-        <div class="title-blackfriday">Бренды со скидками до 50%</div>
-        <div class="b-common-section__content b-common-section__content--popular-brand">
-            <div class="b-popular-brand">
-                <?php foreach ($arResult['ITEMS'] as $item): ?>
-                    <div class="b-popular-brand-item">
-                        <a class="b-popular-brand-item__link" title="<?=$item['NAME'] ?>" href="<?=$item['LINK'] ?>">
-                            <img class="b-popular-brand-item__image js-image-wrapper js-lazy lazy-loaded" alt="<?=$item['NAME'] ?>" title="<?=$item['NAME'] ?>"
-                                 src="<?=$item['PREVIEW_PICTURE']?>" style="">
-                        </a>
-                    </div>
-                <?php endforeach; ?>
+<?php if ($arResult['ITEMS']) : ?>
+    <section class="brands-blackfriday">
+        <div class="b-container">
+            <div class="title-blackfriday">Бренды со скидками до 50%</div>
+            <div class="b-common-section__content b-common-section__content--popular-brand">
+                <div class="b-popular-brand">
+                    <?php foreach ($arResult['ITEMS'] as $item): ?>
+                        <div class="b-popular-brand-item">
+                            <a class="b-popular-brand-item__link" title="<?=$item['NAME']?>" href="<?=$item['LINK']?>">
+                                <img class="b-popular-brand-item__image js-image-wrapper js-lazy lazy-loaded" alt="<?=$item['NAME']?>" title="<?=$item['NAME']?>"
+                                     src="<?=$item['PREVIEW_PICTURE']?>" style="">
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
