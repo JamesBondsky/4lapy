593bed5ff6ead2096a8d87633b3aa75aa4d7cce7
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-11-25 11:55:18 +0300

    миниправка

--- common/local/components/articul/blackfriday.questions/templates/.default/template.php
+++ common/local/components/articul/blackfriday.questions/templates/.default/template.php
@@ -3,12 +3,12 @@
         <div class="b-container">
             <h2 class="questions-blackfriday__title">Вопросы и ответы</h2>
             <div class="questions-blackfriday__accordion">
-                <?php foreach ($arResult['ITEMS'] as $item): ?>
+                <?php foreach ($arResult['ITEMS'] as $key => $item): ?>
                     <div class="item-accordion">
-                        <div class="item-accordion__header js-toggle-accordion active">
+                        <div class="item-accordion__header js-toggle-accordion <?php if ($key == 0) : ?>active<?php endif; ?>">
                             <span class="item-accordion__header-inner"><?=$item['NAME']?></span>
                         </div>
-                        <div class="item-accordion__block js-dropdown-block" style="display: block;">
+                        <div class="item-accordion__block js-dropdown-block" <?php if ($key == 0) : ?>style="display: block;"<?php endif; ?>>
                             <div class="item-accordion__block-content">
                                 <div class="item-accordion__block-text">
                                     <?=$item['PREVIEW_TEXT']?>
