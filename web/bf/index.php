ff000bd9c7d9c7e5bbd1078504e58b073b41dc4e
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-11-27 19:37:32 +0300

    sp_38048 новые блоки для Черной Пятницы

--- web/bf/index.php
+++ web/bf/index.php
@@ -5,11 +5,18 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
     <section class="main-banner-blackfriday">
         <div class="b-container">
             <div class="main-banner-blackfriday__info">
-                <div class="main-banner-blackfriday__title main-banner-blackfriday__title_desktop">до&nbsp;-50% <nobr>на&nbsp;2&nbsp;500</nobr> товаров</div>
+                <div class="main-banner-blackfriday__title main-banner-blackfriday__title_desktop">до&nbsp;-50%
+                    <nobr>на&nbsp;2&nbsp;500</nobr>
+                                                                                                   товаров
+                </div>
                 <div class="main-banner-blackfriday__date">с&nbsp;28 ноября по&nbsp;1 декабря</div>
                 <div class="timer-blackfriday">
-                    <div class="timer-blackfriday__title">До&nbsp;начала <span class="hide-mobile">акции</span> осталось:</div>
-                    <?php /*<div class="timer-blackfriday__title">До&nbsp;завершения <span class="hide-mobile">акции</span> осталось:</div>*/ ?>
+                    <?php if (strtotime(date('d.m.Y')) < strtotime('29.11.2019')) : ?>
+                        <div class="timer-blackfriday__title">До&nbsp;начала <span class="hide-mobile">акции</span> осталось:</div>
+                    <?php else : ?>
+                        <div class="timer-blackfriday__title">До&nbsp;завершения <span class="hide-mobile">акции</span> осталось:</div>
+                    <?php endif; ?>
+                    <?php if (strtotime(date('d.m.Y')) < strtotime('29.11.2019')) : ?>
                     <div class="timer-blackfriday__time-wrap">
                         <div class="timer-blackfriday__time timer-blackfriday__time_desktop">
                             <script src="/bf/js/timer-bf.js" data-skip-moving="true"></script>
@@ -18,7 +25,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
                             <script src="/bf/js/timer-bf_mobile.js" data-skip-moving="true"></script>
                         </div>
                     </div>
-                    <?php /*
+                    <?php else : ?>
                     <div class="timer-blackfriday__time-wrap">
                         <div class="timer-blackfriday__time timer-blackfriday__time_desktop">
                             <script src="/bf/js/timer-bf_end.js" data-skip-moving="true"></script>
@@ -27,7 +34,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
                             <script src="/bf/js/timer-bf_end-mobile.js" data-skip-moving="true"></script>
                         </div>
                     </div>
-                    */ ?>
+                    <?php endif; ?>
                 </div>
                 <div class="main-banner-blackfriday__title main-banner-blackfriday__title_mobile">до&nbsp;-50% на&nbsp;2&nbsp;500 товаров</div>
             </div>
