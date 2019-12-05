f4cccf0e46d0d04bc640285e2a3d53e62a3812b1
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-12-05 18:53:54 +0300

    Лекции

--- src/LandingBundle/Controller/FlagmanController.php
+++ src/LandingBundle/Controller/FlagmanController.php
@@ -275,7 +275,7 @@ class FlagmanController extends Controller implements LoggerAwareInterface
             ]);
             
             if ($successAdding) {
-                \CIBlockElement::Update($request->get('id'), 0, ['FREE' => 'N']);
+                \CIBlockElement::SetPropertyValuesEx($request->get('id'), 0, ['FREE' => 'N']);
                 $sender = App::getInstance()->getContainer()->get('expertsender.service');
                 $sender->sendGroomingEmail($name, $phone, $email, $animal, $breed, $service, $clinic, $date);
                 
