c765f7311060d790008eb68e16b7d0354ea41e54
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-12-12 11:52:53 +0300

    sp_37574_local_grooming| ивенты: вывод в меню

--- common/local/components/articul/flagman.menu/class.php
+++ common/local/components/articul/flagman.menu/class.php
@@ -20,29 +20,106 @@ class FlagmanMenu extends \CBitrixComponent
     
     private function deactivateTrainingIfEmpty()
     {
-        Loader::includeModule('iblock');
-    
-        $iblockId = $this->getIblockId();
-    
-        $result = SectionTable::query()
-            ->setSelect(['ID', 'NAME'])
-            ->setFilter([
-                '=IBLOCK_ID' => $iblockId,
-                '=ACTIVE'    => 'Y',
-            ])
-            ->exec()
-            ->fetchAll();
-
-        if (empty($result)) {
-            $this->arParams['SHOW_TRAINING'] = 'N';
+        if ($this->StartResultCache(36000)) {
+            Loader::includeModule('iblock');
+            
+            $iblockTrainingId = $this->getIblockTrainingId();
+            $iblockLectionId = $this->getIblockLectionId();
+            $iblockGroomingId = $this->getIblockGroomingId();
+            
+            $groomingActivity = $this->setGroomingActivity($iblockGroomingId);
+            $trainingActivity = $this->setTrainingActivity($iblockTrainingId);
+            $lectionActivity = $this->setLectionActivity($iblockLectionId);
+            
+            if (!$groomingActivity && !$trainingActivity && !$lectionActivity) {
+                $this->abortResultCache();
+            }
         }
     }
     
-    private function getIblockId()
+    private function getIblockTrainingId()
     {
         return \CIBlock::GetList([], [
             'TYPE' => 'grandin',
             'CODE' => 'flagman_training',
         ])->Fetch()['ID'];
+    }
+    
+    private function getIblockLectionId()
+    {
+        return \CIBlock::GetList([], [
+            'TYPE' => 'grandin',
+            'CODE' => 'flagman_lections',
+        ])->Fetch()['ID'];
+    }
+    
+    private function getIblockGroomingId()
+    {
+        return \CIBlock::GetList([], [
+            'TYPE' => 'grandin',
+            'CODE' => 'flagman_grooming',
+        ])->Fetch()['ID'];
+    }
+    
+    private function setGroomingActivity($id)
+    {
+        $items = SectionTable::query()
+            ->setSelect(['ID', 'NAME'])
+            ->setFilter([
+                '=IBLOCK_ID' => $id,
+                '=ACTIVE'    => 'Y',
+            ])
+            ->exec()
+            ->fetchAll();
+    
+        foreach ($items as $item) {
+            preg_match('/^([0-9]{2,4}).([0-9]{2,4}).([0-9]{2,4})/', $item['NAME'], $matches);
+    
+            if (strtotime(date('d.m.Y')) < strtotime($matches[0])) {
+                $this->arParams['SHOW_GROOMING'] = 'Y';
+                
+                return true;
+            }
+        }
+    }
+    
+    private function setTrainingActivity($id)
+    {
+        $items = SectionTable::query()
+            ->setSelect(['ID', 'NAME'])
+            ->setFilter([
+                '=IBLOCK_ID' => $id,
+                '=ACTIVE'    => 'Y',
+            ])
+            ->exec()
+            ->fetchAll();
+
+        foreach ($items as $item) {
+            preg_match('/^([0-9]{2,4}).([0-9]{2,4}).([0-9]{2,4})/', $item['NAME'], $matches);
+            
+            if (strtotime(date('d.m.Y')) < strtotime($matches[0])) {
+                $this->arParams['SHOW_TRAINING'] = 'Y';
+                
+                return true;
+            }
+        }
+    }
+    
+    private function setLectionActivity($id)
+    {
+        $items = SectionTable::query()
+            ->setSelect(['ID', 'NAME'])
+            ->setFilter([
+                '=IBLOCK_ID' => $id,
+                '=ACTIVE'    => 'Y',
+                '=DEPTH_LEVEL' => 1
+            ])
+            ->exec()
+            ->fetchAll();
+        
+        if (!empty($items)) {
+            $this->arParams['SHOW_LECTION'] = 'Y';
+            return true;
+        }
     }
 }
