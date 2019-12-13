616772222c788c53108453e8cc2379dd5798874e
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2019-12-13 15:44:52 +0300

    sp_37788|марки скрыты

--- src/PersonalBundle/Service/StampService.php
+++ src/PersonalBundle/Service/StampService.php
@@ -36,7 +36,7 @@ class StampService implements LoggerAwareInterface
     public const MARK_RATE = 500;
     public const MARKS_PER_RATE = 1;
 
-    public const IS_STAMPS_OFFER_ACTIVE = true;
+    public const IS_STAMPS_OFFER_ACTIVE = false;
 
     public const DISCOUNT_TYPE_PERCENTAGE = 'P';
     public const DISCOUNT_TYPE_VALUE = 'V'; // скидка в рублях
