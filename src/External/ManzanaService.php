351a17f7495a308121ceb9e86e5c220db5f4d862
Anton Ankilov <a.ankilov@articul.ru>
2019-12-16 10:53:46 +0000

    tm-38677 fix events

--- src/External/ManzanaService.php
+++ src/External/ManzanaService.php
@@ -969,7 +969,8 @@ class ManzanaService implements LoggerAwareInterface, ManzanaServiceInterface
             /** @noinspection MissingService */
             /** @var Producer $producer */
             $producer = App::getInstance()->getContainer()->get('old_sound_rabbit_mq.manzana_orders_import_producer');
-            $producer->publish($this->serializer->serialize(clone $user, 'json'));
+//            $producer->publish($this->serializer->serialize($user, 'json'));
+            $producer->publish(json_encode($user));
 
             try
             {
