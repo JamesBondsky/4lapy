b7dd4dfff3df36261da99471e0f4c0cc4d9edf0b
Anton Ankilov <a.ankilov@articul.ru>
2019-12-25 12:47:46 +0000

    fix

--- src/MobileApiBundle/Controller/v0/ProductController.php
+++ src/MobileApiBundle/Controller/v0/ProductController.php
@@ -171,7 +171,6 @@ class ProductController extends BaseController
         $productsList = $this->apiProductService->getList($request, $categoryId, $sort, $count, $page, '', $stockId);
         /** @var \CIBlockResult $cdbResult */
         $cdbResult = $productsList->get('cdbResult');
-        die($productsList->get('time'));
         return (new Response\ProductListResponse())
             ->setProductList($productsList->get('products'))
             ->setTotalPages($cdbResult->NavPageCount ?: 0)
