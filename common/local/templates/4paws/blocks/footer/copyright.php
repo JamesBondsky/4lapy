64273ada06a69c77a55be48eca90ecad7712e251
Евгений Гарунович <egarunovich@4lapy.ru>
2020-01-16 07:55:52 +0000

    Коммит для тестирования релиза

--- common/local/templates/4paws/blocks/footer/copyright.php
+++ common/local/templates/4paws/blocks/footer/copyright.php
@@ -4,7 +4,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 } ?>
 <div class="b-copyright js-copyright-scroll">
-    <div class="b-copyright__copyright">
+    <div class="b-copyright__copyright" data-test="test">
         &copy; <?= (new DateTime())->format('Y') ?> Зоомагазин «Четыре Лапы»
     </div>
     <a class="b-copyright__link"
