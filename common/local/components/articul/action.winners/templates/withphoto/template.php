f6bd8eb3598213a8061eac1357e8f78c3e36cb04
Daria <d.gritsaenko@articul.ru>
2020-01-27 17:58:07 +0300

    tm-39473 заглушка для фото победителей на странице leto2020

--- common/local/components/articul/action.winners/templates/withphoto/template.php
+++ common/local/components/articul/action.winners/templates/withphoto/template.php
@@ -6,7 +6,7 @@
                 <?php foreach ($arResult['ITEMS'] as $item) : ?>
                     <div class="item">
                         <div class="item__top">
-                            <div class="item__photo" style="background-image: url('<?=$item['PREVIEW_PICTURE']?>')">
+                            <div class="item__photo" style="background-image: url('<?= ($item['PREVIEW_PICTURE']) ? ($item['PREVIEW_PICTURE']) : '/leto2020/img/avatar.jpg' ?>')">
                                 <img src="/leto2020/img/bg-photo.png" alt=""/>
                             </div>
                             <div class="item__prizes <?php if ($item['PROPERTY_ICON_VALUE']) : ?>item__prizes_<?=$item['PROPERTY_ICON_VALUE']?><?php endif; ?>"></div>
