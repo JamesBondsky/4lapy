13bf81bd044640704f90445e1144e4968412f5f0
Sergey Papastatopulos <spapastatopulos23@gmail.com>
2020-01-27 15:58:54 +0300

    fix

--- common/local/components/articul/action.winners/templates/withphoto/result_modifier.php
+++ common/local/components/articul/action.winners/templates/withphoto/result_modifier.php
@@ -1,6 +1,8 @@
 <?php
 foreach ($arResult as $element) {
     foreach ($element['WINNERS'] as $winner) {
-        $arResult['ITEMS'][] = $winner;
+        if ($winner['PROPERTY_SLIDER_VALUE']) {
+            $arResult['ITEMS'][] = $winner;
+        }
     }
 }
