6095cf2af7d738f26cbbf54b027e2d80936a088f
Anton Ankilov <a.ankilov@articul.ru>
2020-01-29 15:06:50 +0000

    tm-33492 исправление текста

--- web/personal/register/index.php
+++ web/personal/register/index.php
@@ -1,6 +1,10 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
-$title = 'Регистрация';
+if (isset($_SESSION['socServiceParams'])) {
+    $title = 'Регистрация через социальную сеть';
+} else {
+    $title = 'Регистрация';
+}
 if(\FourPaws\KioskBundle\Service\KioskService::isKioskMode()){
     $title = 'Мы Вас не узнали! Зарегистрируйтесь?';
 }
@@ -10,8 +14,11 @@ echo '<div class="b-registration b-registration--two-parts js-registration-conte
     <header class="b-registration__header">
         <h1 class="b-title b-title--h1 b-title--registration">';
 $APPLICATION->ShowTitle(false);
-echo '</h1>
-    </header>';
+echo '</h1>';
+if (isset($_SESSION['socServiceParams'])) {
+    echo '<div class="b-step-form" style="margin-top: 20px;">Для того, чтобы продолжить вход в личный кабинет, пожалуйста, укажите номер телефона для завершения процесса регистрации</div>';
+}
+    echo '</header>';
 ?>
 <?$APPLICATION->IncludeComponent(
     'fourpaws:register',
