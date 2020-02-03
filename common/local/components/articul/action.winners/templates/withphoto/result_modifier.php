78279f05ecb1c4752f69960d145080657bcc3014
Andrey Vorobyev <a.vorobyev@articul.ru>
2020-02-03 12:54:05 +0300

    Revert "Merge branch 'dg_task39473_photo' of 4Lapy/4lapy into dev"
    This reverts commit c4627c94a53d07c6dff4b0ff2601dae14c4e3d5b, reversing
    changes made to 3cf3cd4ec688573eaaf7a4799f6abbdebb3b918a.

--- common/local/components/articul/action.winners/templates/withphoto/result_modifier.php
+++
@@ -1,8 +0,0 @@
-<?php
-foreach ($arResult as $element) {
-    foreach ($element['WINNERS'] as $winner) {
-        if ($winner['PROPERTY_SLIDER_VALUE']) {
-            $arResult['ITEMS'][] = $winner;
-        }
-    }
-}
